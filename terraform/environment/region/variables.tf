0c73f4785c943f564109e1674afc781813f05f58
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-08-03 13:33:53 +0100

    pass execution role into region

--- terraform/environment/region/variables.tf
+++ terraform/environment/region/variables.tf
@@ -1,5 +1,5 @@
 locals {
-  name_prefix                   = "${data.aws_default_tags.current.tags.application}-${data.aws_default_tags.current.tags.environment-name}-${data.aws_region.current.name}"
+  name_prefix                   = "${data.aws_default_tags.current.tags.environment-name}-${data.aws_region.current.name}"
   dns_namespace_for_environment = var.account_name == "production" ? "" : "${data.aws_default_tags.current.tags.environment-name}."
   certificate_wildcard          = var.account_name == "production" ? "" : "*."
 }
@@ -7,4 +7,9 @@ locals {
 variable "account_name" {
   type        = string
   description = "Name of the target account for deployments"
+}
+
+variable "ecs_execution_role_arn" {
+  type        = string
+  description = "ARN of the task execution role that the Amazon ECS container agent and the Docker daemon can assume."
 }
