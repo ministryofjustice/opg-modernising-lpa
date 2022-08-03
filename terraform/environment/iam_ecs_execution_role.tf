b9c1f8bfb58d9afd540f1633f4f05f457d728b50
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-08-03 13:33:08 +0100

    create global ecs execution role

--- terraform/environment/iam_ecs_execution_role.tf
+++ terraform/environment/iam_ecs_execution_role.tf
@@ -16,3 +16,32 @@ data "aws_iam_policy_document" "execution_role_assume_policy" {
   }
   provider = aws.global
 }
+
+resource "aws_iam_role_policy" "execution_role" {
+  name     = "${local.environment_name}-execution-role"
+  policy   = data.aws_iam_policy_document.execution_role.json
+  role     = aws_iam_role.execution_role.arn
+  provider = aws.global
+}
+
+data "aws_iam_policy_document" "execution_role" {
+  statement {
+    effect    = "Allow"
+    resources = ["*"] #tfsec:ignore:aws-iam-no-policy-wildcards
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+    ]
+  }
+  statement {
+    effect    = "Allow"
+    resources = ["*"] #tfsec:ignore:aws-iam-no-policy-wildcards
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+  }
+  provider = aws.global
+}
