a7f3a1e6fb681311aea12b2f695a8be9dd8ac651
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-08-03 15:51:35 +0100

    create application log groups without encryption

--- terraform/environment/region/ecs.tf
+++ terraform/environment/region/ecs.tf
@@ -7,12 +7,21 @@ resource "aws_ecs_cluster" "main" {
   provider = aws.region
 }
 
+# module "application_logs" {
+#   source = "./modules/application_logs"
+#   providers = {
+#     aws.region = aws.region
+#   }
+# }
+
 module "app" {
   source                    = "./modules/app"
   account_name              = var.account_name
   ecs_cluster               = aws_ecs_cluster.main.id
   ecs_execution_role        = var.ecs_execution_role
+  ecs_task_role_arn         = var.ecs_task_role_arns.app
   ecs_service_desired_count = 0
+  # application_log_group_arn = module.application_logs.log_group_arn
   network = {
     vpc_id              = data.aws_vpc.main.id
     application_subnets = data.aws_subnet.application.*.id
