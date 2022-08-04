33237d8e88d187f3ea4ae3e49a25f60ff4aaa969
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-08-04 12:22:02 +0100

    MLPA-65 - Create certificate for app ALB (#14)
    * create new providers for management
    
    * pass management provider into region module
    
    * provision certificate for app

--- terraform/account/regions.tf
+++ terraform/account/regions.tf
@@ -2,7 +2,8 @@ module "eu_west_1" {
   source             = "./region"
   network_cidr_block = "10.162.0.0/16"
   providers = {
-    aws.region = aws.eu_west_1
+    aws.region     = aws.eu_west_1
+    aws.management = aws.management_eu_west_1
   }
 
 }
--- terraform/account/terraform.tf
+++ terraform/account/terraform.tf
@@ -50,6 +50,30 @@ provider "aws" {
 }
 
 provider "aws" {
+  alias  = "management_eu_west_1"
+  region = "eu-west-1"
+  default_tags {
+    tags = local.default_tags
+  }
+  assume_role {
+    role_arn     = "arn:aws:iam::311462405659:role/${var.default_role}"
+    session_name = "opg-modernising-lpa-terraform-session"
+  }
+}
+
+provider "aws" {
+  alias  = "management_eu_west_2"
+  region = "eu-west-2"
+  default_tags {
+    tags = local.default_tags
+  }
+  assume_role {
+    role_arn     = "arn:aws:iam::311462405659:role/${var.default_role}"
+    session_name = "opg-modernising-lpa-terraform-session"
+  }
+}
+
+provider "aws" {
   alias  = "global"
   region = "us-east-1"
   default_tags {
