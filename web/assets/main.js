ecae510834c7b2bd7eca5c626bd604ee68eeac79
Alex Saunders <acsauk@gmail.com>
2022-08-04 12:32:45 +0100

    Include MOJ frontend JS and CSS

--- web/assets/main.js
+++ web/assets/main.js
@@ -1,3 +1,8 @@
 import * as GOVUKFrontend from "govuk-frontend";
+import $ from 'jquery'
+import { initAll } from '@ministryofjustice/frontend'
+
+window.$ = $
+initAll()
 
 GOVUKFrontend.initAll();
