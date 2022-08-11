2a35a65ba3c5191e903bd0213f37b7f77d164e6c
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-08-11 15:25:03 +0100

    configure compose version

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,7 +11,20 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://app:5050',
+    setupNodeEvents(on, config) {
+      on('task', {
+        log(message) {
+          console.log(message)
+
+          return null
+        },
+      })
+    },
+    video: false
+  },
+});
