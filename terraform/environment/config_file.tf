dcd621f9d42114dd15036463b5e5e25885435b50
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-08-23 19:38:36 +0100

    fix name of file

--- terraform/environment/config_file.tf
+++ terraform/environment/config_file.tf
@@ -1,6 +1,6 @@
 resource "local_file" "environment_config" {
   content  = jsonencode(local.environment_config)
-  filename = "${path.module}/cluster_config.json"
+  filename = "${path.module}/environment_config.json"
 }
 
 locals {
