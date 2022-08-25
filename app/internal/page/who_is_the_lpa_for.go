0128835a696ded62396bfcc7c562ef4fcb9fc53d
Joshua Hawxwell <m@hawx.me>
2022-08-25 13:11:21 +0100

    MLPAB-92 Add who is the lpa for page (#46)

---
+++ app/internal/page/who_is_the_lpa_for.go
@@ -0,0 +1,60 @@
+package page
+
+import (
+	"net/http"
+
+	"github.com/ministryofjustice/opg-go-common/template"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/localize"
+)
+
+type whoIsTheLpaForData struct {
+	Page   string
+	L      localize.Localizer
+	Lang   Lang
+	Errors map[string]string
+}
+
+func WhoIsTheLpaFor(logger Logger, localizer localize.Localizer, lang Lang, tmpl template.Template, dataStore DataStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data := &whoIsTheLpaForData{
+			Page: whoIsTheLpaForPath,
+			L:    localizer,
+			Lang: lang,
+		}
+
+		if r.Method == http.MethodPost {
+			form := readWhoIsTheLpaForForm(r)
+			data.Errors = form.Validate()
+
+			if len(data.Errors) == 0 {
+				dataStore.Save(form.WhoFor)
+				lang.Redirect(w, r, "/next-page", http.StatusFound)
+				return
+			}
+		}
+
+		if err := tmpl(w, data); err != nil {
+			logger.Print(err)
+		}
+	}
+}
+
+type whoIsTheLpaForForm struct {
+	WhoFor string
+}
+
+func readWhoIsTheLpaForForm(r *http.Request) *whoIsTheLpaForForm {
+	return &whoIsTheLpaForForm{
+		WhoFor: postFormString(r, "who-for"),
+	}
+}
+
+func (f *whoIsTheLpaForForm) Validate() map[string]string {
+	errors := map[string]string{}
+
+	if f.WhoFor != "me" && f.WhoFor != "someone-else" {
+		errors["who-for"] = "selectWhoFor"
+	}
+
+	return errors
+}
--- cypress/e2e/donor-address.cy.js
+++ cypress/e2e/donor-address.cy.js
@@ -15,7 +15,7 @@ describe('Donor address', () => {
 
         cy.get('#f-select-address').select('123 Fake Street, Someville, NG1');
         cy.contains('button', 'Continue').click();
-        cy.url().should('contain', '/next-page');
+        cy.url().should('contain', '/who-is-the-lpa-for');
     });
 
     it('address can be entered manually', () => {
@@ -39,6 +39,6 @@ describe('Donor address', () => {
         cy.get('#f-address-postcode').type('NG1');
 
         cy.contains('button', 'Continue').click();
-        cy.url().should('contain', '/next-page');
+        cy.url().should('contain', '/who-is-the-lpa-for');
     });
 });
