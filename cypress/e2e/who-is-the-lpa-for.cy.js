19b8d649fc0b21c54beaabfeeef811808297b949
Joshua Hawxwell <m@hawx.me>
2022-08-25 11:23:38 +0100

    Add who is the lpa for page

---
+++ cypress/e2e/who-is-the-lpa-for.cy.js
@@ -0,0 +1,15 @@
+describe('Who is the lpa for', () => {
+    beforeEach(() => {
+        cy.visit('/who-is-the-lpa-for');
+        cy.injectAxe();
+    });
+
+    it('can be submitted', () => {
+        cy.get('#f-who-for').check('me');
+
+        cy.checkA11y(null, { rules: { region: { enabled: false } } });
+
+        cy.contains('button', 'Continue').click();
+        cy.url().should('contain', '/next-page');
+    });
+});
