ff92696be658ab3650ccbbcd7402ae58a0dc4e4b
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-08-26 10:43:43 +0100

    only need a single secret

--- terraform/account/secrets_manager_secret.tf
+++ terraform/account/secrets_manager_secret.tf
@@ -7,13 +7,3 @@ resource "aws_secretsmanager_secret" "private_jwt_key_base64" {
   }
   provider = aws.eu_west_1
 }
-
-resource "aws_secretsmanager_secret" "public_jwt_key_base64" {
-  name       = "public-jwt-key-base64"
-  kms_key_id = aws_kms_key.secrets_manager.key_id
-  replica {
-    kms_key_id = aws_kms_replica_key.secrets_manager_replica.key_id
-    region     = data.aws_region.eu_west_2.name
-  }
-  provider = aws.eu_west_1
-}
