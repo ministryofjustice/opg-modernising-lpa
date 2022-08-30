3e79eaef3da61f79990c740463a379c8258a4410
Joshua Hawxwell <m@hawx.me>
2022-08-30 07:29:23 +0100

    Use and verify random state when logging in

--- app/internal/page/auth_redirect.go
+++ app/internal/page/auth_redirect.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"net/url"
 
+	"github.com/gorilla/sessions"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/signin"
 )
 
@@ -12,8 +13,19 @@ type authRedirectClient interface {
 	UserInfo(string) (signin.UserInfo, error)
 }
 
-func AuthRedirect(logger Logger, c authRedirectClient) http.HandlerFunc {
+func AuthRedirect(logger Logger, c authRedirectClient, store sessions.Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		session, err := store.Get(r, "params")
+		if err != nil {
+			logger.Print(err)
+			return
+		}
+
+		if s, ok := session.Values["state"].(string); !ok || s != r.FormValue("state") {
+			logger.Print("state missing or incorrect")
+			return
+		}
+
 		jwt, err := c.Exchange(r.FormValue("code"))
 		if err != nil {
 			logger.Print(err)
