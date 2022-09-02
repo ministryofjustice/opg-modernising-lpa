020cfff186637ef24c18838848efb76c338eb83e
Joshua Hawxwell <m@hawx.me>
2022-09-02 09:19:06 +0100

    Store data in dynamodb

--- terraform/environment/config_file.tf
+++ terraform/environment/config_file.tf
@@ -8,5 +8,6 @@ locals {
     region                              = "eu-west-1"
     account_id                          = local.environment.account_id
     app_load_balancer_security_group_id = module.eu_west_1.app_load_balancer_security_group.id
+    lpas_table                          = aws_dynamodb_table.lpas_table.name
   }
 }
