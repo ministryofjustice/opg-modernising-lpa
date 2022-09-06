c0a00eb26632fd4f62d8c8f38fb56a45654aefa3
Joshua Hawxwell <m@hawx.me>
2022-09-06 07:19:48 +0100

    MLPAB-98 Add choose attorney form (#72)

---
+++ cypress/e2e/choose-attorneys.cy.js
@@ -0,0 +1,21 @@
+describe('Choose attorneys', () => {
+    beforeEach(() => {
+        cy.visit('/testing-start?redirect=/choose-attorneys');
+        cy.injectAxe();
+    });
+
+    it('can be submitted', () => {
+        cy.get('#f-first-names').type('John');
+        cy.get('#f-last-name').type('Doe');
+        cy.get('#f-email').type('what');
+        cy.get('#f-date-of-birth-day').type('1');
+        cy.get('#f-date-of-birth-month').type('2');
+        cy.get('#f-date-of-birth-year').type('1990');
+
+        cy.checkA11y(null, { rules: { region: { enabled: false } } });
+
+        cy.contains('button', 'Continue').click();
+        cy.url().should('contain', '/choose-attorneys-address');
+        cy.contains("John Doe's address");
+    });
+});
