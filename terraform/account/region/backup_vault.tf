8a16ff473bda3f5a9afcf79cb52e51cb90943907
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-09-06 13:32:23 +0100

    put the provider for this resource

--- terraform/account/region/backup_vault.tf
+++ terraform/account/region/backup_vault.tf
@@ -1,3 +1,4 @@
 resource "aws_backup_vault" "main" {
-  name = "${data.aws_region.current.name}-${data.aws_default_tags.current.tags.environment-name}-backup-vault"
+  name     = "${data.aws_region.current.name}-${data.aws_default_tags.current.tags.environment-name}-backup-vault"
+  provider = aws.region
 }
