21937931095158a7df3b8584c33c0ee5c05e173e
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-09-07 09:09:37 +0100

    consider backup vaults a global resource and create one for each region

---
+++ terraform/account/refactoring.tf
@@ -0,0 +1,4 @@
+moved {
+  from = module.eu_west_1.aws_backup_vault.main
+  to   = aws_backup_vault.eu_west_1
+}
--- terraform/account/region/backup_vault.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_backup_vault" "main" {
-  name     = "${data.aws_region.current.name}-${data.aws_default_tags.current.tags.environment-name}-backup-vault"
-  provider = aws.region
-}
