b3cc721c1fa8130b007e51c6439ccb6e55ea3f8d
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-09-07 15:35:45 +0100

    create environment resource groups for each provider

---
+++ terraform/environment/resource_group.tf
@@ -0,0 +1,46 @@
+resource "aws_resourcegroups_group" "environment_eu_west_1" {
+  name        = "${local.default_tags.environment-name}-environment-eu-west-1"
+  description = "Environment level eu-west-1 resources"
+
+  resource_query {
+    query = local.environment_resource_group_query
+    type  = "TAG_FILTERS_1_0"
+  }
+  provider = aws.eu_west_1
+}
+
+resource "aws_resourcegroups_group" "environment_eu_west_2" {
+  name        = "${local.default_tags.environment-name}-environment-eu-west-2"
+  description = "Environment level eu-west-2 resources"
+
+  resource_query {
+    query = local.environment_resource_group_query
+    type  = "TAG_FILTERS_1_0"
+  }
+  provider = aws.eu_west_2
+}
+
+resource "aws_resourcegroups_group" "environment_global" {
+  name        = "${local.default_tags.environment-name}-environment-global"
+  description = "Environment level global resources"
+
+  resource_query {
+    query = local.environment_resource_group_query
+    type  = "TAG_FILTERS_1_0"
+  }
+  provider = aws.global
+}
+
+locals {
+  environment_resource_group_query = jsonencode({
+    ResourceTypeFilters = [
+      "AWS::AllSupported"
+    ],
+    TagFilters = [
+      {
+        Key    = "environment-name",
+        Values = [local.default_tags.environment-name]
+      }
+    ]
+  })
+}
