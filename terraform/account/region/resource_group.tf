e62ac5c13bf8a32756fa7b15c1cfea2cdd20f291
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-09-07 15:15:20 +0100

    move resource group into region

--- terraform/account/region/resource_group.tf
+++ terraform/account/region/resource_group.tf
@@ -1,10 +1,12 @@
 resource "aws_resourcegroups_group" "account" {
-  name        = "account-${local.default_tags.account-name}"
-  description = "Account level resources"
+  name        = "${data.aws_default_tags.current.tags.account-name}-account-${data.aws_region.current.name}"
+  description = "Account level resources for ${data.aws_region.current.name}"
 
   resource_query {
     query = local.account_resource_group_query
+    type  = "TAG_FILTERS_1_0"
   }
+  provider = aws.region
 }
 
 locals {
@@ -15,7 +17,7 @@ locals {
     TagFilters = [
       {
         Key    = "account-name",
-        Values = [local.default_tags.account-name]
+        Values = [data.aws_default_tags.current.tags.account-name]
       }
     ]
   })
