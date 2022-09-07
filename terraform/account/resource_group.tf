93d4940c57079a6ad7cde841f567039e08f4287b
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-09-07 14:58:43 +0100

    add description for resource group

--- terraform/account/resource_group.tf
+++ terraform/account/resource_group.tf
@@ -1,5 +1,6 @@
 resource "aws_resourcegroups_group" "account" {
-  name = "account-${local.default_tags.account-name}"
+  name        = "account-${local.default_tags.account-name}"
+  description = "Account level resources"
 
   resource_query {
     query = local.account_resource_group_query
