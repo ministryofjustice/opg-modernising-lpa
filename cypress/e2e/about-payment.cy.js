b816a9a7a69c3e6ba3f695f9c3c3353910e5e4e7
Alex Saunders <acsauk@gmail.com>
2022-09-08 15:39:58 +0100

    Add first about payment tests

---
+++ cypress/e2e/about-payment.cy.js
@@ -0,0 +1,11 @@
+describe('About payment', () => {
+    it('has a title', () => {
+        cy.visit('/testing-start?redirect=/about-payment');
+        cy.injectAxe();
+        cy.get('h1').should('contain', 'About payment');
+    })
+
+    // it('has a continue button', () => {
+    //     cy.contains('a', 'Continue to payment');
+    // })
+})
