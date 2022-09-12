98f9ab1668918335cc5c6bf738a0eaea4e6bcd1a
Alex Saunders <acsauk@gmail.com>
2022-09-12 15:12:36 +0100

    Add check your lpa spec

---
+++ cypress/e2e/check-your-lpa.cy.js
@@ -0,0 +1,26 @@
+describe('Check the LPA', () => {
+    it("submits the completed LPA", () => {
+        cy.visit('/testing-start?redirect=/check-your-lpa');
+
+        cy.contains('h1', "Check your LPA")
+
+        cy.injectAxe();
+        cy.checkA11y(null, { rules: { region: { enabled: false } } });
+
+        cy.contains('h2', "LPA decisions")
+
+        cy.contains('h2', "People named on the LPA")
+        cy.contains('h3', "Donor")
+        cy.contains('h3', "Certificate provider")
+        cy.contains('h3', "Attorneys")
+        cy.contains('h3', "Replacement attorney")
+
+        cy.get('#f-checked').check()
+        cy.get('#f-happy').check()
+
+        cy.contains('button', 'Continue').click();
+
+        // for now - this will eventually lead to simple certificate provider journey
+        cy.url().should('contain', '/task-list');
+    })
+});
