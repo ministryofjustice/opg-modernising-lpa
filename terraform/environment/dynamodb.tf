af029f2295386acd8e0ca0538abb2bebd3097ea0
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-09-13 10:11:59 +0100

    MLPAB-158 - Use CMK for DynamoDB encryption (#104)
    - allow ECS to use DynamoDB encryption key
    
    - use CMK for DynamoDB server side encryption

--- terraform/environment/dynamodb.tf
+++ terraform/environment/dynamodb.tf
@@ -1,10 +1,16 @@
+data "aws_kms_alias" "dynamodb_encryption_key_eu_west_1" {
+  name     = "alias/${local.default_tags.application}_dynamodb_encryption"
+  provider = aws.eu_west_1
+}
+
 resource "aws_dynamodb_table" "lpas_table" {
   name         = "${local.environment_name}-Lpas"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "Id"
 
   server_side_encryption {
-    enabled = true
+    enabled     = true
+    kms_key_arn = data.aws_kms_alias.dynamodb_encryption_key_eu_west_1.target_key_arn
   }
 
   attribute {
