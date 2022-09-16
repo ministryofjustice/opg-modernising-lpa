1c3a47b0999c585edf3abec776c29a539ad5c3c5
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-09-16 16:03:50 +0100

    create variable to control deployments to a second region

--- terraform/environment/regions.tf
+++ terraform/environment/regions.tf
@@ -8,7 +8,8 @@ module "allow_list" {
 }
 
 module "eu_west_1" {
-  source             = "./region"
+  source = "./region"
+  # count              = contains(local.environment.regions, "eu-west-1") ? 1 : 0
   ecs_execution_role = aws_iam_role.execution_role
   ecs_task_roles = {
     app = aws_iam_role.app_task_role
@@ -25,3 +26,28 @@ module "eu_west_1" {
     aws.region = aws.eu_west_1
   }
 }
+
+module "eu_west_2" {
+  source             = "./region"
+  count              = contains(local.environment.regions, "eu-west-2") ? 1 : 0
+  ecs_execution_role = aws_iam_role.execution_role
+  ecs_task_roles = {
+    app = aws_iam_role.app_task_role
+  }
+  application_log_retention_days  = local.environment.cloudwatch_log_groups.application_log_retention_days
+  ecs_capacity_provider           = local.ecs_capacity_provider
+  app_service_repository_url      = data.aws_ecr_repository.app.repository_url
+  app_service_container_version   = var.container_version
+  ingress_allow_list_cidr         = module.allow_list.moj_sites
+  alb_deletion_protection_enabled = local.environment.application_load_balancer.deletion_protection_enabled
+  lpas_table                      = aws_dynamodb_table.lpas_table
+  app_env_vars                    = local.environment.app.env
+  providers = {
+    aws.region = aws.eu_west_2
+  }
+}
+
+# moved {
+#   from = module.eu_west_1
+#   to   = module.eu_west_1[0]
+# }
