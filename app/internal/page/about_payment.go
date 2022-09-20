a39584e57798007c48dd465e4bd918838d52a7f5
Alex Saunders <acsauk@gmail.com>
2022-09-20 17:01:34 +0100

    Revert debug

--- app/internal/page/about_payment.go
+++ app/internal/page/about_payment.go
@@ -1,6 +1,7 @@
 package page
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 	"time"
@@ -24,25 +25,24 @@ func AboutPayment(logger Logger, tmpl template.Template, sessionStore sessions.S
 		}
 
 		if r.Method == http.MethodPost {
-			//createPaymentBody := pay.CreatePaymentBody{
-			//	Amount:      0,
-			//	Reference:   "abc",
-			//	Description: "A payment",
-			//	ReturnUrl:   appPublicUrl + "/payment-confirmation",
-			//	Email:       "a@b.com",
-			//	Language:    "en",
-			//}
+			createPaymentBody := pay.CreatePaymentBody{
+				Amount:      0,
+				Reference:   "abc",
+				Description: "A payment",
+				ReturnUrl:   appPublicUrl + "/payment-confirmation",
+				Email:       "a@b.com",
+				Language:    "en",
+			}
 
-			//resp, err := payClient.CreatePayment(createPaymentBody)
+			resp, err := payClient.CreatePayment(createPaymentBody)
 
-			//if err != nil {
-			//	logger.Print(fmt.Sprintf("Error creating payment: %s", err.Error()))
-			//	return err
-			//}
+			if err != nil {
+				logger.Print(fmt.Sprintf("Error creating payment: %s", err.Error()))
+				return err
+			}
 
-			//nextUrl := resp.Links["next_url"].Href
+			nextUrl := resp.Links["next_url"].Href
 
-			nextUrl := "http://localhost:4010"
 			secureCookies := strings.HasPrefix(nextUrl, "https:")
 
 			cookieOptions := &sessions.Options{
@@ -57,13 +57,11 @@ func AboutPayment(logger Logger, tmpl template.Template, sessionStore sessions.S
 
 			session := sessions.NewSession(sessionStore, "pay")
 			session.Values = map[interface{}]interface{}{
-				//"paymentId": resp.PaymentId,
-				"paymentId": "abc123",
+				"paymentId": resp.PaymentId,
 			}
 			session.Options = cookieOptions
 
-			//session.Values = map[interface{}]interface{}{"paymentId": resp.PaymentId}
-			session.Values = map[interface{}]interface{}{"paymentId": "abc123"}
+			session.Values = map[interface{}]interface{}{"paymentId": resp.PaymentId}
 			if err := sessionStore.Save(r, w, session); err != nil {
 				return err
 			}
