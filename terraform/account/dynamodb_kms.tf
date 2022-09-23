f0267a4f5d12f961c02db2665ca9dcab8b685efb
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-09-23 14:27:05 +0100

    MLPAB-258 - Allow backup role to decrypt (#116)

--- terraform/account/dynamodb_kms.tf
+++ terraform/account/dynamodb_kms.tf
@@ -62,6 +62,7 @@ data "aws_iam_policy_document" "dynamodb_kms" {
       type = "AWS"
       identifiers = [
         local.account.account_name == "development" ? "arn:aws:iam::${data.aws_caller_identity.global.account_id}:root" : "arn:aws:iam::${data.aws_caller_identity.global.account_id}:role/${local.account.account_name}-app-task-role",
+        aws_iam_role.aws_backup_role.arn,
       ]
     }
     condition {
