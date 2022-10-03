3094252228588424b859a59ad13c1adc27be0e55
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-10-03 12:26:43 +0100

    allow List generally

--- terraform/account/sns_kms.tf
+++ terraform/account/sns_kms.tf
@@ -89,7 +89,7 @@ data "aws_iam_policy_document" "sns_kms" {
       "kms:DescribeKey",
       "kms:GetKeyPolicy",
       "kms:GetKeyRotationStatus",
-      "kms:ListResourceTags",
+      "kms:List*",
     ]
 
     principals {
