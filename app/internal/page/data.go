231d85bcb6619749c857e7d36bfd32dc18e91856
Joshua Hawxwell <m@hawx.me>
2022-10-06 09:09:28 +0100

    Move identity options to their own file

--- app/internal/page/data.go
+++ app/internal/page/data.go
@@ -2,12 +2,10 @@ package page
 
 import (
 	"encoding/json"
-	"sort"
 	"strings"
 	"time"
 
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/identity"
-	"golang.org/x/exp/slices"
 )
 
 const (
@@ -23,93 +21,6 @@ const (
 	TaskInProgress
 	TaskCompleted
 )
-
-type IdentityOption string
-
-func (o IdentityOption) String() string {
-	return string(o)
-}
-
-const (
-	IdentityOptionUnknown    = IdentityOption("")
-	Yoti                     = IdentityOption("yoti")
-	Passport                 = IdentityOption("passport")
-	DrivingLicence           = IdentityOption("driving licence")
-	GovernmentGatewayAccount = IdentityOption("government gateway account")
-	DwpAccount               = IdentityOption("dwp account")
-	OnlineBankAccount        = IdentityOption("online bank account")
-	UtilityBill              = IdentityOption("utility bill")
-	CouncilTaxBill           = IdentityOption("council tax bill")
-)
-
-func readIdentityOption(s string) IdentityOption {
-	switch s {
-	case "yoti":
-		return Yoti
-	case "passport":
-		return Passport
-	case "driving licence":
-		return DrivingLicence
-	case "government gateway account":
-		return GovernmentGatewayAccount
-	case "dwp account":
-		return DwpAccount
-	case "online bank account":
-		return OnlineBankAccount
-	case "utility bill":
-		return UtilityBill
-	case "council tax bill":
-		return CouncilTaxBill
-	default:
-		return IdentityOptionUnknown
-	}
-}
-
-func (o IdentityOption) ArticleLabel() string {
-	switch o {
-	case Yoti:
-		return "theYoti"
-	case Passport:
-		return "aPassport"
-	case DrivingLicence:
-		return "aDrivingLicence"
-	case GovernmentGatewayAccount:
-		return "aGovernmentGatewayAccount"
-	case DwpAccount:
-		return "aDwpAccount"
-	case OnlineBankAccount:
-		return "anOnlineBankAccount"
-	case UtilityBill:
-		return "aUtilityBill"
-	case CouncilTaxBill:
-		return "aCouncilTaxBill"
-	default:
-		return ""
-	}
-}
-
-func (o IdentityOption) Label() string {
-	switch o {
-	case Yoti:
-		return "yoti"
-	case Passport:
-		return "passport"
-	case DrivingLicence:
-		return "drivingLicence"
-	case GovernmentGatewayAccount:
-		return "governmentGatewayAccount"
-	case DwpAccount:
-		return "dwpAccount"
-	case OnlineBankAccount:
-		return "onlineBankAccount"
-	case UtilityBill:
-		return "utilityBill"
-	case CouncilTaxBill:
-		return "councilTaxBill"
-	default:
-		return ""
-	}
-}
 
 type Lpa struct {
 	You                      Person
@@ -130,35 +41,6 @@ type Lpa struct {
 	SignatureCode            string
 	IdentityOptions          IdentityOptions
 	YotiUserData             identity.UserData
-}
-
-type IdentityOptions struct {
-	Selected []IdentityOption
-	First    IdentityOption
-	Second   IdentityOption
-}
-
-func (o IdentityOptions) NextPath(current IdentityOption) string {
-	identityOptionPaths := map[IdentityOption]string{
-		Yoti:                     identityWithYotiPath,
-		Passport:                 identityWithPassportPath,
-		DrivingLicence:           identityWithDrivingLicencePath,
-		GovernmentGatewayAccount: identityWithGovernmentGatewayAccountPath,
-		DwpAccount:               identityWithDwpAccountPath,
-		OnlineBankAccount:        identityWithOnlineBankAccountPath,
-		UtilityBill:              identityWithUtilityBillPath,
-		CouncilTaxBill:           identityWithCouncilTaxBillPath,
-	}
-
-	if current == o.Second {
-		return whatHappensWhenSigningPath
-	}
-
-	if current == o.First {
-		return identityOptionPaths[o.Second]
-	}
-
-	return identityOptionPaths[o.First]
 }
 
 type PaymentDetails struct {
@@ -255,61 +137,4 @@ func readDate(t time.Time) Date {
 		Month: t.Format("1"),
 		Year:  t.Format("2006"),
 	}
-}
-
-type rankedItem struct {
-	item    IdentityOption
-	rank    int
-	subrank int
-}
-
-func identityOptionsRanked(options []IdentityOption) (firstChoice, secondChoice IdentityOption) {
-	if len(options) == 0 {
-		return IdentityOptionUnknown, IdentityOptionUnknown
-	}
-
-	table := map[IdentityOption]struct {
-		rank    int
-		subrank int
-		not     []IdentityOption
-	}{
-		Yoti:                     {rank: 0, subrank: 0, not: []IdentityOption{Passport, DrivingLicence}},
-		Passport:                 {rank: 1, subrank: 2, not: []IdentityOption{GovernmentGatewayAccount, OnlineBankAccount}},
-		DrivingLicence:           {rank: 2, subrank: 3, not: []IdentityOption{}},
-		DwpAccount:               {rank: 3, subrank: 5, not: []IdentityOption{GovernmentGatewayAccount}},
-		OnlineBankAccount:        {rank: 4, subrank: 6, not: []IdentityOption{Passport}},
-		GovernmentGatewayAccount: {rank: 6, subrank: 4, not: []IdentityOption{DwpAccount}},
-		UtilityBill:              {rank: 7, subrank: 7, not: []IdentityOption{CouncilTaxBill}},
-		CouncilTaxBill:           {rank: 8, subrank: 8, not: []IdentityOption{UtilityBill}},
-	}
-
-	rankedOptions := make([]rankedItem, len(options))
-	for i, option := range options {
-		rankedOptions[i] = rankedItem{item: option, rank: table[option].rank, subrank: table[option].subrank}
-	}
-
-	sort.Slice(rankedOptions, func(i, j int) bool {
-		return rankedOptions[i].rank < rankedOptions[j].rank
-	})
-
-	firstChoice = rankedOptions[0].item
-
-	var remainingOptions []rankedItem
-	for _, option := range rankedOptions {
-		if option.item != firstChoice && !slices.Contains(table[firstChoice].not, option.item) {
-			remainingOptions = append(remainingOptions, option)
-		}
-	}
-
-	sort.Slice(remainingOptions, func(i, j int) bool {
-		return remainingOptions[i].subrank < remainingOptions[j].subrank
-	})
-
-	if len(remainingOptions) == 0 {
-		return firstChoice, IdentityOptionUnknown
-	}
-
-	secondChoice = remainingOptions[0].item
-
-	return firstChoice, secondChoice
 }
