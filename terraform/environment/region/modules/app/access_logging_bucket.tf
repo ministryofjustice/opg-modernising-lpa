c162876fd5cbc92c481ea5a36866970f6253ce83
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-10-21 14:33:21 +0100

    select lb access loggin bucket for region

--- terraform/environment/region/modules/app/access_logging_bucket.tf
+++ terraform/environment/region/modules/app/access_logging_bucket.tf
@@ -1,4 +1,4 @@
 data "aws_s3_bucket" "access_log_bucket" {
-  bucket   = "s3-access-logs-opg-modernising-lpa-${data.aws_default_tags.current.tags.account-name}-${data.aws_region.current.name}"
+  bucket   = "${data.aws_default_tags.current.tags.application}-${data.aws_default_tags.current.tags.account-name}-lb-access-logs-${data.aws_region.current.name}"
   provider = aws.region
 }
