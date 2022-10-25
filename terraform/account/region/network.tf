0edc09913a35808898ff95f8fa5666e3d3f43a30
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-10-25 16:37:10 +0100

    upgrade network module

--- terraform/account/region/network.tf
+++ terraform/account/region/network.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source                         = "github.com/ministryofjustice/opg-terraform-aws-network?ref=v1.2.0-MLPAB-344.1"
+  source                         = "github.com/ministryofjustice/opg-terraform-aws-network?ref=v1.2.0"
   cidr                           = var.network_cidr_block
   default_security_group_ingress = []
   default_security_group_egress  = []
