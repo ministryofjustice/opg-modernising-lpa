cefaca3107c84f544fe285f256551181ac921c8e
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-10-25 13:15:32 +0100

    split rules by ip type

--- terraform/account/region/default_vpc.tf
+++ terraform/account/region/default_vpc.tf
@@ -47,25 +47,43 @@ resource "aws_default_network_acl" "default" {
     to_port    = 0
   }
   ingress {
+    action     = "deny"
+    cidr_block = "0.0.0.0/0" #tfsec:ignore:aws-ec2-no-public-ingress-acl
+    from_port  = 22
+    icmp_code  = 0
+    icmp_type  = 0
+    protocol   = "6"
+    rule_no    = 120
+    to_port    = 22
+  }
+  ingress {
+    action     = "deny"
+    cidr_block = "0.0.0.0/0" #tfsec:ignore:aws-ec2-no-public-ingress-acl
+    from_port  = 3389
+    icmp_code  = 0
+    icmp_type  = 0
+    protocol   = "6"
+    rule_no    = 130
+    to_port    = 3389
+  }
+  ingress {
     action          = "deny"
-    cidr_block      = "0.0.0.0/0" #tfsec:ignore:aws-ec2-no-public-ingress-acl
     ipv6_cidr_block = "::/0"
     from_port       = 22
     icmp_code       = 0
     icmp_type       = 0
     protocol        = "6"
-    rule_no         = 120
+    rule_no         = 125
     to_port         = 22
   }
   ingress {
     action          = "deny"
-    cidr_block      = "0.0.0.0/0" #tfsec:ignore:aws-ec2-no-public-ingress-acl
     ipv6_cidr_block = "::/0"
     from_port       = 3389
     icmp_code       = 0
     icmp_type       = 0
     protocol        = "6"
-    rule_no         = 130
+    rule_no         = 135
     to_port         = 3389
   }
   ingress {
