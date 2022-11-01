e381684ad949f8b908b806f3f6f03c62c40c6f95
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-11-01 16:24:59 +0000

    enable insights on fixed environments

--- terraform/environment/cloudwatch_application_insights.tf
+++ terraform/environment/cloudwatch_application_insights.tf
@@ -1,17 +1,23 @@
 resource "aws_applicationinsights_application" "environment_eu_west_1" {
+  count               = local.environment.cloudwatch_application_insights_enabled ? 1 : 0
   resource_group_name = "${local.default_tags.environment-name}-environment-eu-west-1"
   auto_config_enabled = true
+  cwe_monitor_enabled = true
   provider            = aws.eu_west_1
 }
 
 resource "aws_applicationinsights_application" "environment_eu_west_2" {
+  count               = local.environment.cloudwatch_application_insights_enabled ? 1 : 0
   resource_group_name = "${local.default_tags.environment-name}-environment-eu-west-2"
   auto_config_enabled = true
+  cwe_monitor_enabled = true
   provider            = aws.eu_west_2
 }
 
 resource "aws_applicationinsights_application" "environment_global" {
+  count               = local.environment.cloudwatch_application_insights_enabled ? 1 : 0
   resource_group_name = "${local.default_tags.environment-name}-environment-global"
   auto_config_enabled = true
+  cwe_monitor_enabled = true
   provider            = aws.global
 }
