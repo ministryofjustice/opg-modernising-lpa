fece6a78aeed710be107b741759d7b43139f901a
Alex Saunders <acsauk@gmail.com>
2022-11-08 14:29:41 +0000

    expand e2e

--- cypress/e2e/want-replacement-attorneys.cy.js
+++ cypress/e2e/want-replacement-attorneys.cy.js
@@ -3,7 +3,7 @@ describe('Do you want replacement attorneys', () => {
         cy.visit('/testing-start?redirect=/want-replacement-attorneys&howAttorneysAct=jointly');
         cy.injectAxe();
 
-        cy.contains('Replacement attorneys are an important backup when attorneys are appointed to act jointly.')
+        cy.get('div.govuk-warning-text').should('contain', 'Replacement attorneys are an important backup when attorneys are appointed to act jointly.')
 
         cy.get('#f-want').check()
 
@@ -17,7 +17,21 @@ describe('Do you want replacement attorneys', () => {
         cy.visit('/testing-start?redirect=/want-replacement-attorneys&howAttorneysAct=mixed');
         cy.injectAxe();
 
-        cy.contains('The donor appointed their attorneys to act jointly for some decisions, and jointly and severally for others.')
+        cy.get('div.govuk-warning-text').should('contain', 'The donor appointed their attorneys to act jointly for some decisions, and jointly and severally for others.')
+
+        cy.get('#f-want').check()
+
+        cy.checkA11y(null, { rules: { region: { enabled: false } } });
+
+        cy.contains('button', 'Continue').click();
+        cy.url().should('contain', '/choose-replacement-attorneys');
+    });
+
+    it('can be submitted - acting jointly and severally', () => {
+        cy.visit('/testing-start?redirect=/want-replacement-attorneys&howAttorneysAct=jointly-and-severally');
+        cy.injectAxe();
+
+        cy.get('div.govuk-warning-text').should('not.exist')
 
         cy.get('#f-want').check()
 
