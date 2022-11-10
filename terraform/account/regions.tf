88aa86dac4f8379dc7c3e4e7bc1d58bd4b0b3174
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-11-10 11:29:51 +0000

    pass kms alias in to use for encryption

--- terraform/account/regions.tf
+++ terraform/account/regions.tf
@@ -3,6 +3,7 @@ module "eu_west_1" {
   count                              = contains(local.account.regions, "eu-west-1") ? 1 : 0
   network_cidr_block                 = "10.162.0.0/16"
   cloudwatch_log_group_kms_key_alias = "alias/${local.default_tags.application}_cloudwatch_application_logs_encryption"
+  s3_kms_key_alias                   = "alias/${local.default_tags.application}_s3_encryption"
   sns_kms_key_alias                  = aws_kms_alias.sns_alias_eu_west_1.name
   providers = {
     aws.region     = aws.eu_west_1
@@ -16,6 +17,7 @@ module "eu_west_2" {
   count                              = contains(local.account.regions, "eu-west-2") ? 1 : 0
   network_cidr_block                 = "10.162.0.0/16"
   cloudwatch_log_group_kms_key_alias = "alias/${local.default_tags.application}_cloudwatch_application_logs_encryption"
+  s3_kms_key_alias                   = "alias/${local.default_tags.application}_s3_encryption"
   sns_kms_key_alias                  = aws_kms_alias.sns_alias_eu_west_2.name
   providers = {
     aws.region     = aws.eu_west_2
