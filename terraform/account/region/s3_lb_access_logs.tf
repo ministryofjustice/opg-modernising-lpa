868348f38f475a8b4168fa41373f67464abd9bfa
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-11-15 10:00:02 +0000

    neaten up bucket policy and trun off CMK encryption

--- terraform/account/region/s3_lb_access_logs.tf
+++ terraform/account/region/s3_lb_access_logs.tf
@@ -14,8 +14,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "access_log" {
   bucket   = aws_s3_bucket.access_log.id
   rule {
     apply_server_side_encryption_by_default {
-      kms_master_key_id = data.aws_kms_alias.s3_encryption.target_key_arn
-      sse_algorithm     = "aws:kms"
+      # kms_master_key_id = data.aws_kms_alias.s3_encryption.target_key_arn
+      sse_algorithm = "aws:kms"
     }
   }
 }
@@ -79,6 +79,7 @@ data "aws_iam_policy_document" "access_log" {
     resources = [
       aws_s3_bucket.access_log.arn,
       "${aws_s3_bucket.access_log.arn}/*",
+      "${aws_s3_bucket.access_log.arn}/*/AWSLogs/your-aws-account-id/*",
     ]
     effect  = "Allow"
     actions = ["s3:PutObject"]
