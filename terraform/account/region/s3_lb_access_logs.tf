a94808931c4a2c630913e3fa34a149cc8f84a31a
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-11-15 10:16:35 +0000

    remove CMK for S3

--- terraform/account/region/s3_lb_access_logs.tf
+++ terraform/account/region/s3_lb_access_logs.tf
@@ -14,8 +14,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "access_log" {
   bucket   = aws_s3_bucket.access_log.id
   rule {
     apply_server_side_encryption_by_default {
-      # kms_master_key_id = data.aws_kms_alias.s3_encryption.target_key_arn
-      sse_algorithm = "aws:kms"
+      # Access logging for ALBs only supports Amazon S3 Managed Encryption Keys (SSE-S3)
+      sse_algorithm = "aws:kms" #tfsec:ignore:aws-s3-encryption-customer-key
     }
   }
 }
