5578edb709f039ff1a62933a62870a975790f474
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-11-15 14:26:17 +0000

    only build rum resources on dev account

--- terraform/account/rum_cognito_identity_pool.tf
+++ terraform/account/rum_cognito_identity_pool.tf
@@ -1,10 +1,12 @@
 resource "aws_cognito_identity_pool" "rum_monitor" {
+  count                            = local.account.rum_enabled ? 1 : 0
   identity_pool_name               = "RUM-Monitor-${data.aws_region.eu_west_1.name}"
   allow_unauthenticated_identities = true
   provider                         = aws.eu_west_1
 }
 
 resource "aws_iam_role" "rum_monitor_unauthenticated" {
+  count              = local.account.rum_enabled ? 1 : 0
   name               = "RUM-Monitor-${data.aws_region.eu_west_1.name}"
   assume_role_policy = data.aws_iam_policy_document.rum_monitor_unauthenticated_role_assume_policy.json
   provider           = aws.eu_west_1
@@ -25,7 +27,7 @@ data "aws_iam_policy_document" "rum_monitor_unauthenticated_role_assume_policy" 
       variable = "cognito-identity.amazonaws.com:aud"
 
       values = [
-        aws_cognito_identity_pool.rum_monitor.id
+        aws_cognito_identity_pool.rum_monitor[0].id
       ]
     }
     condition {
@@ -34,6 +36,15 @@ data "aws_iam_policy_document" "rum_monitor_unauthenticated_role_assume_policy" 
 
       values = ["unauthenticated"]
     }
+  }
+  provider = aws.eu_west_1
+}
+
+resource "aws_cognito_identity_pool_roles_attachment" "rum_monitor" {
+  count            = local.account.rum_enabled ? 1 : 0
+  identity_pool_id = aws_cognito_identity_pool.rum_monitor[0].id
+  roles = {
+    unauthenticated = aws_iam_role.rum_monitor_unauthenticated[0].arn
   }
   provider = aws.eu_west_1
 }
