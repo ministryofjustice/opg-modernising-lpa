d681c2ec32aea7ddec6947c98c57fa6349241102
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-11-17 11:57:58 +0000

    import env from node:process

--- app/web/assets/main.js
+++ app/web/assets/main.js
@@ -2,6 +2,7 @@ import * as GOVUKFrontend from "govuk-frontend";
 import $ from 'jquery'
 import { initAll } from '@ministryofjustice/frontend'
 import { AwsRum, AwsRumConfig } from 'aws-rum-web';
+import { env } from 'node:process';
 
 window.$ = $
 initAll()
@@ -11,17 +12,17 @@ GOVUKFrontend.initAll();
 try {
   const config = {
     sessionSampleRate: 1,
-    guestRoleArn: process.env.AWS_RUM_GUEST_ROLE_ARN,
-    identityPoolId: process.env.AWS_RUM_IDENTITY_POOL_ID,
-    endpoint: process.env.AWS_RUM_ENDPOINT,
+    guestRoleArn: env.AWS_RUM_GUEST_ROLE_ARN,
+    identityPoolId: env.AWS_RUM_IDENTITY_POOL_ID,
+    endpoint: env.AWS_RUM_ENDPOINT,
     telemetries: ["http","errors","performance"],
     allowCookies: true,
     enableXRay: true
   };
 
-  const APPLICATION_ID = process.env.AWS_RUM_APPLICATION_ID;
+  const APPLICATION_ID = env.AWS_RUM_APPLICATION_ID;
   const APPLICATION_VERSION = '1.0.0';
-  const APPLICATION_REGION = process.env.AWS_RUM_APPLICATION_REGION;
+  const APPLICATION_REGION = env.AWS_RUM_APPLICATION_REGION;
 
   const awsRum = new AwsRum(
     APPLICATION_ID,
