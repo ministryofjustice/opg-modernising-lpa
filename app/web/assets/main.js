a5b7e8444a42d13e63e1b1568f71af7195bdde1d
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-11-17 10:32:47 +0000

    configure RUM monitoring

--- app/web/assets/main.js
+++ app/web/assets/main.js
@@ -7,3 +7,28 @@ window.$ = $
 initAll()
 
 GOVUKFrontend.initAll();
+
+try {
+  const config = {
+    sessionSampleRate: 1,
+    guestRoleArn: process.env.AWS_RUM_GUEST_ROLE_ARN,
+    identityPoolId: process.env.AWS_RUM_IDENTITY_POOL_ID,
+    endpoint: process.env.AWS_RUM_ENDPOINT,
+    telemetries: ["http","errors","performance"],
+    allowCookies: true,
+    enableXRay: true
+  };
+
+  const APPLICATION_ID = process.env.AWS_RUM_APPLICATION_ID;
+  const APPLICATION_VERSION = '1.0.0';
+  const APPLICATION_REGION = process.env.AWS_RUM_APPLICATION_REGION;
+
+  const awsRum = new AwsRum(
+    APPLICATION_ID,
+    APPLICATION_VERSION,
+    APPLICATION_REGION,
+    config
+  );
+} catch (error) {
+  // Ignore errors thrown during CloudWatch RUM web client initialization
+}
