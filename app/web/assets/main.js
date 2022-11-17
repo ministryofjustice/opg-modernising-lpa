4eaa9285f9cfec1c224d12edc86b45c2cd3034c0
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-11-17 12:13:47 +0000

    put the config directly for now

--- app/web/assets/main.js
+++ app/web/assets/main.js
@@ -2,7 +2,6 @@ import * as GOVUKFrontend from "govuk-frontend";
 import $ from 'jquery'
 import { initAll } from '@ministryofjustice/frontend'
 import { AwsRum, AwsRumConfig } from 'aws-rum-web';
-import { env } from 'node:process';
 
 window.$ = $
 initAll()
@@ -12,17 +11,17 @@ GOVUKFrontend.initAll();
 try {
   const config = {
     sessionSampleRate: 1,
-    guestRoleArn: env.AWS_RUM_GUEST_ROLE_ARN,
-    identityPoolId: env.AWS_RUM_IDENTITY_POOL_ID,
-    endpoint: env.AWS_RUM_ENDPOINT,
+    guestRoleArn: "arn:aws:iam::653761790766:role/RUM-Monitor-Unauthenticated",
+    identityPoolId: "eu-west-1:7c2ec7b2-7af5-4e45-81da-3c36f3961e1c",
+    endpoint: "https://dataplane.rum.eu-west-1.amazonaws.com",
     telemetries: ["http","errors","performance"],
     allowCookies: true,
     enableXRay: true
   };
 
-  const APPLICATION_ID = env.AWS_RUM_APPLICATION_ID;
+  const APPLICATION_ID = '3f435cdd-6dec-4cea-837b-33c6103d1562';
   const APPLICATION_VERSION = '1.0.0';
-  const APPLICATION_REGION = env.AWS_RUM_APPLICATION_REGION;
+  const APPLICATION_REGION = 'eu-west-1';
 
   const awsRum = new AwsRum(
     APPLICATION_ID,
