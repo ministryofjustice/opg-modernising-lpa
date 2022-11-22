e30646c22b6dc3fe2904d1ec20bdeff7b6d698a1
Joshua Hawxwell <m@hawx.me>
2022-11-22 12:10:00 +0000

    Pass RUM environment variables through to JS

--- app/web/assets/main.js
+++ app/web/assets/main.js
@@ -8,20 +8,24 @@ initAll()
 
 GOVUKFrontend.initAll();
 
+function metaContent(name) {
+    return document.querySelector(`meta[name=${name}]`).content;
+}
+
 try {
   const config = {
     sessionSampleRate: 1,
-    guestRoleArn: "arn:aws:iam::653761790766:role/RUM-Monitor-Unauthenticated",
-    identityPoolId: "eu-west-1:1b1631fd-d258-4677-a11d-51096cc876c1",
-    endpoint: "https://dataplane.rum.eu-west-1.amazonaws.com",
+    guestRoleArn: metaContent('rum-guest-role-arn'),
+    identityPoolId: metaContent('rum-identity-pool-id'),
+    endpoint: metaContent('rum-endpoint'),
     telemetries: ["http","errors","performance"],
     allowCookies: true,
     enableXRay: true
   };
 
-  const APPLICATION_ID = '81df3db5-de4f-46b1-869e-60cf0e4ac165';
+  const APPLICATION_ID = metaContent('rum-application-id');
   const APPLICATION_VERSION = '1.0.0';
-  const APPLICATION_REGION = 'eu-west-1';
+  const APPLICATION_REGION = metaContent('rum-application-region');
 
   const awsRum = new AwsRum(
     APPLICATION_ID,
