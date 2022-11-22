bc0e3f90b524a9c3d504d92477d366d026f39266
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-11-22 13:51:03 +0000

    refactor rum identity pool into region module

--- terraform/account/region/rum_cognito_identity_pool.tf
+++ terraform/account/region/rum_cognito_identity_pool.tf
@@ -1,20 +1,12 @@
-resource "aws_cognito_identity_pool" "rum_monitor" {
-  count                            = local.account.rum_enabled ? 1 : 0
-  identity_pool_name               = "RUM-Monitor-${data.aws_region.eu_west_1.name}"
-  allow_unauthenticated_identities = true
-  allow_classic_flow               = true
-  provider                         = aws.eu_west_1
-}
-
 resource "aws_iam_role" "rum_monitor_unauthenticated" {
-  count              = local.account.rum_enabled ? 1 : 0
-  name               = "RUM-Monitor-Unauthenticated"
+  count              = var.rum_enabled ? 1 : 0
+  name               = "RUM-Monitor-Unauthenticated-${data.aws_region.current.name}"
   assume_role_policy = data.aws_iam_policy_document.rum_monitor_unauthenticated_role_assume_policy[0].json
-  provider           = aws.eu_west_1
+  provider           = aws.global
 }
 
 data "aws_iam_policy_document" "rum_monitor_unauthenticated_role_assume_policy" {
-  count = local.account.rum_enabled ? 1 : 0
+  count = var.rum_enabled ? 1 : 0
   statement {
     effect  = "Allow"
     actions = ["sts:AssumeRoleWithWebIdentity"]
@@ -37,41 +29,37 @@ data "aws_iam_policy_document" "rum_monitor_unauthenticated_role_assume_policy" 
       values   = ["unauthenticated"]
     }
   }
-  provider = aws.eu_west_1
+  provider = aws.global
+}
+
+resource "aws_cognito_identity_pool" "rum_monitor" {
+  count                            = var.rum_enabled ? 1 : 0
+  identity_pool_name               = "RUM-Monitor-${data.aws_region.current.name}"
+  allow_unauthenticated_identities = true
+  allow_classic_flow               = true
+  provider                         = aws.region
 }
 
 resource "aws_cognito_identity_pool_roles_attachment" "rum_monitor" {
-  count            = local.account.rum_enabled ? 1 : 0
+  count            = var.rum_enabled ? 1 : 0
   identity_pool_id = aws_cognito_identity_pool.rum_monitor[0].id
   roles = {
     unauthenticated = aws_iam_role.rum_monitor_unauthenticated[0].arn
   }
-  provider = aws.eu_west_1
-}
-
-resource "aws_ssm_parameter" "rum_monitor_identity_pool_id" {
-  count    = local.account.rum_enabled ? 1 : 0
-  name     = "rum_monitor_identity_pool_id"
-  type     = "String"
-  value    = aws_cognito_identity_pool.rum_monitor[0].id
-  provider = aws.eu_west_1
+  provider = aws.region
 }
 
 resource "aws_secretsmanager_secret" "rum_monitor_identity_pool_id" {
-  count                   = local.account.rum_enabled ? 1 : 0
-  name                    = "rum-monitor-identity-pool-id"
-  kms_key_id              = aws_kms_key.secrets_manager.key_id
+  count                   = var.rum_enabled ? 1 : 0
+  name                    = "rum-monitor-identity-pool-id-${data.aws_region.current.name}"
+  kms_key_id              = data.aws_kms_alias.secrets_manager.target_key_id
   recovery_window_in_days = 0
-  replica {
-    kms_key_id = aws_kms_replica_key.secrets_manager_replica.key_id
-    region     = data.aws_region.eu_west_2.name
-  }
-  provider = aws.eu_west_1
+  provider                = aws.region
 }
 
 resource "aws_secretsmanager_secret_version" "rum_monitor_identity_pool_id" {
-  count         = local.account.rum_enabled ? 1 : 0
+  count         = var.rum_enabled ? 1 : 0
   secret_id     = aws_secretsmanager_secret.rum_monitor_identity_pool_id[0].id
   secret_string = aws_cognito_identity_pool.rum_monitor[0].id
-  provider      = aws.eu_west_1
+  provider      = aws.region
 }
