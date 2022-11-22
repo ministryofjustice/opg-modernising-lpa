4c05d83e4efc19cb2afec8159e516c0639474b27
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-11-22 10:46:50 +0000

    explicit config

--- app/web/assets/main.js
+++ app/web/assets/main.js
@@ -11,17 +11,17 @@ GOVUKFrontend.initAll();
 try {
   const config = {
     sessionSampleRate: 1,
-    guestRoleArn: process.env.AWS_RUM_GUEST_ROLE_ARN,
-    identityPoolId: process.env.AWS_RUM_IDENTITY_POOL_ID,
-    endpoint: process.env.AWS_RUM_ENDPOINT,
+    guestRoleArn: "arn:aws:iam::653761790766:role/RUM-Monitor-Unauthenticated",
+    identityPoolId: "eu-west-1:1b1631fd-d258-4677-a11d-51096cc876c1",
+    endpoint: "https://dataplane.rum.eu-west-1.amazonaws.com",
     telemetries: ["http","errors","performance"],
     allowCookies: true,
     enableXRay: true
   };
 
-  const APPLICATION_ID = process.env.AWS_RUM_APPLICATION_ID;
+  const APPLICATION_ID = '81df3db5-de4f-46b1-869e-60cf0e4ac165';
   const APPLICATION_VERSION = '1.0.0';
-  const APPLICATION_REGION = process.env.AWS_RUM_APPLICATION_REGION;
+  const APPLICATION_REGION = 'eu-west-1';
 
   const awsRum = new AwsRum(
     APPLICATION_ID,
@@ -30,6 +30,5 @@ try {
     config
   );
 } catch (error) {
-  console.log(error)
   // Ignore errors thrown during CloudWatch RUM web client initialization
 }
