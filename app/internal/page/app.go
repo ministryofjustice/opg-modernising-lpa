2223c3478274fed5ba2feb5f9ca2e27c78174ee6
John Nolan <john.nolan@digital.justice.gov.uk>
2022-11-29 15:48:53 +0000

    Remove code comment

--- app/internal/page/app.go
+++ app/internal/page/app.go
@@ -32,7 +32,7 @@ type Lang int
 
 func CacheControlWrapper(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Cache-Control", "max-age=2592000") // 30 days
+		w.Header().Set("Cache-Control", "max-age=2592000")
 		h.ServeHTTP(w, r)
 	})
 }
