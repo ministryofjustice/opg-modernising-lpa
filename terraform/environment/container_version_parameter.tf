0ea305f88c293a876ca1d4d5c39ae143eb1fc23a
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-12-13 10:13:59 +0000

    move to management global region

--- terraform/environment/container_version_parameter.tf
+++ terraform/environment/container_version_parameter.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "container_version" {
-  name     = "${local.environment_name}-container-version"
+  name     = "modernising-lpa-${local.environment_name}-container-version"
   type     = "String"
   value    = var.container_version
-  provider = aws.region
+  provider = aws.management_global
 }
