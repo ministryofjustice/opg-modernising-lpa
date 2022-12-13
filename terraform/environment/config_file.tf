dedb8d294cd49ddfc5243c9515aa4c2915ba0792
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-12-13 09:55:36 +0000

    write the container version string to parameter store for each env

--- terraform/environment/config_file.tf
+++ terraform/environment/config_file.tf
@@ -10,3 +10,9 @@ locals {
     app_load_balancer_security_group_id = module.eu_west_1[0].app_load_balancer_security_group.id
   }
 }
+
+resource "aws_ssm_parameter" "container_version" {
+  name  = "${local.environment_name}-container-version"
+  type  = "String"
+  value = var.container_version
+}
