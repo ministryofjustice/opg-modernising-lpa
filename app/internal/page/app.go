10b430daa5f99452e2e20141790d17865745e1f0
Alex Saunders <acsauk@gmail.com>
2022-12-20 18:00:53 +0000

    bump coverage

--- app/internal/page/app.go
+++ app/internal/page/app.go
@@ -243,6 +243,8 @@ func App(
 		ReadYourLpa(tmpls.Get("read_your_lpa.gohtml"), lpaStore))
 	handle(paths.SignYourLpa, RequireSession|CanGoBack,
 		SignYourLpa(tmpls.Get("sign_your_lpa.gohtml"), lpaStore))
+	handle(paths.WitnessingYourSignature, RequireSession|CanGoBack,
+		Guidance(tmpls.Get("witnessing_your_signature.gohtml"), "", lpaStore))
 	handle(paths.SigningConfirmation, RequireSession|CanGoBack,
 		Guidance(tmpls.Get("signing_confirmation.gohtml"), paths.TaskList, lpaStore))
 
