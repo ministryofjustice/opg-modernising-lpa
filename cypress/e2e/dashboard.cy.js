3593ca11e7f8321474966285e963a97a7fbb8857
Alex Saunders <acsauk@gmail.com>
2023-01-11 14:37:45 +0000

    drop -day selectors from e2e date tests

--- cypress/e2e/dashboard.cy.js
+++ cypress/e2e/dashboard.cy.js
@@ -3,7 +3,7 @@ describe('Dashboard', () => {
         cy.visit('/testing-start?redirect=/your-details');
         cy.get('#f-first-names').type('John');
         cy.get('#f-last-name').type('Doe');
-        cy.get('#f-date-of-birth-day').type('1');
+        cy.get('##f-date-of-birth').type('1');
         cy.get('#f-date-of-birth-month').type('2');
         cy.get('#f-date-of-birth-year').type('1990');
         cy.contains('button', 'Continue').click();
--- cypress/e2e/your-details.cy.js
+++ cypress/e2e/your-details.cy.js
@@ -7,7 +7,7 @@ describe('Donor details', () => {
     it('can be submitted', () => {
         cy.get('#f-first-names').type('John');
         cy.get('#f-last-name').type('Doe');
-        cy.get('#f-date-of-birth-day').type('1');
+        cy.get('##f-date-of-birth').type('1');
         cy.get('#f-date-of-birth-month').type('2');
         cy.get('#f-date-of-birth-year').type('1990');
 
