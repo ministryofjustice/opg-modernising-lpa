ba6ee353c0a4eb5883aca1c400d55a8cbe29cfd0
Alex Saunders <acsauk@gmail.com>
2023-01-16 12:43:34 +0000

    add prod smoke test

--- cypress/e2e/dev-features.cy.js
+++ cypress/e2e/dev-features.cy.js
@@ -2,10 +2,16 @@ describe('Dev features', () => {
 
     it('Show translation keys', () => {
         cy.visit('/testing-start?redirect=/when-can-the-lpa-be-used?showTransKeys=1&withAttorney=1');
-        cy.contains('h1', '{When can your attorneys use your LPA} [whenCanTheLpaBeUsed]');
+        cy.get('h1').should(
+            "have.text",
+            "{When can your attorneys use your LPA} [whenCanTheLpaBeUsed]"
+        );
         cy.contains('a', 'Toggle translation keys').click();
 
         cy.url().should('contain', '/when-can-the-lpa-be-used');
-        cy.contains('h1', 'When can your attorneys use your LPA');
+        cy.get('h1').should(
+            "have.text",
+            "When can your attorneys use your LPA"
+        );
     });
 });
