93e811278fc50c5caaa2311e20e603f3f69ca14c
Joshua Hawxwell <m@hawx.me>
2023-01-17 08:52:01 +0000

    Use new wording for how attorneys make decisions

--- cypress/e2e/how-should-attorneys-make-decisions.cy.js
+++ cypress/e2e/how-should-attorneys-make-decisions.cy.js
@@ -2,29 +2,23 @@ describe('How should attorneys make decisions', () => {
     beforeEach(() => {
         cy.visit('/testing-start?redirect=/how-should-attorneys-make-decisions?cookiesAccepted=1');
         cy.injectAxe();
-    });
-
-    it('can choose how attorneys act', () => {
-        cy.contains('h1', 'How do you want your attorneys to make decisions?');
 
         // see https://github.com/alphagov/govuk-frontend/issues/979
         cy.checkA11y(null, { rules: { region: { enabled: false }, 'aria-allowed-attr': { enabled: false } } });
+    });
+
+    it('can choose how attorneys act', () => {
+        cy.contains('h1', 'How should your attorneys make decisions?');
 
         cy.get('input[name="decision-type"]').check('jointly');
 
         cy.contains('button', 'Continue').click();
 
         cy.url().should('contain', '/do-you-want-replacement-attorneys');
-
-        cy.injectAxe();
-        cy.checkA11y(null, { rules: { region: { enabled: false }, 'aria-allowed-attr': { enabled: false } } });
-
     });
 
     it('can choose how attorneys act - Jointly for some decisions, and jointly and severally for other decisions', () => {
-        cy.contains('h1', 'How do you want your attorneys to make decisions?');
-
-        cy.checkA11y(null, { rules: { region: { enabled: false }, 'aria-allowed-attr': { enabled: false } } });
+        cy.contains('h1', 'How should your attorneys make decisions?');
 
         cy.get('input[name="decision-type"]').check('mixed');
         cy.get('#mixed-details').type('some details on attorneys');
@@ -32,8 +26,5 @@ describe('How should attorneys make decisions', () => {
         cy.contains('button', 'Continue').click();
 
         cy.url().should('contain', '/do-you-want-replacement-attorneys');
-
-        cy.injectAxe();
-        cy.checkA11y(null, { rules: { region: { enabled: false }, 'aria-allowed-attr': { enabled: false } } });
     });
 });
