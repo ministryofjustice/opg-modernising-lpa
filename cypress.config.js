8759997a838117c267430db3a563a55cf13515cc
Alex Saunders <acsauk@gmail.com>
2023-01-20 11:38:45 +0000

    fix pay cookie tests and bump cypress

--- cypress.config.js
+++ cypress.config.js
@@ -4,19 +4,16 @@ module.exports = defineConfig({
   projectId: "xxbft5",
   e2e: {
     baseUrl: 'http://localhost:5050',
-    experimentalSessionAndOrigin: true,
     defaultCommandTimeout: 2000,
     pageLoadTimeout: 3000,
     setupNodeEvents(on, config) {
       on('task', {
         log(message) {
           console.log(message)
-
           return null
         },
         table(message) {
           console.table(message)
-
           return null
         }
       })
