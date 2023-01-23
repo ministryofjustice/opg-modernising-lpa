accec3373665ac52f9e2e782587544c1f8844854
Joshua Hawxwell <m@hawx.me>
2023-01-23 14:49:24 +0000

    Allow creating multiple LPAs

--- cypress/e2e/dashboard.cy.js
+++ cypress/e2e/dashboard.cy.js
@@ -22,4 +22,26 @@ describe('Dashboard', () => {
 
         cy.url().should('contain', '/task-list');
     });
+
+    it('can create another', () => {
+        cy.visit('/dashboard');
+
+        cy.contains('button', 'Create another LPA').click();
+
+        cy.get('#f-first-names').type('Jane');
+        cy.get('#f-last-name').type('Doe');
+        cy.get('#f-date-of-birth').type('2');
+        cy.get('#f-date-of-birth-month').type('3');
+        cy.get('#f-date-of-birth-year').type('1990');
+        cy.contains('button', 'Continue').click();
+
+        cy.visitLpa('/lpa-type');
+        cy.get('#f-lpa-type-2').check();
+        cy.contains('button', 'Continue').click();
+
+        cy.visit('/dashboard');
+
+        cy.contains('Property and affairs: John Doe');
+        cy.contains('Personal welfare: Jane Doe');
+    });
 });
