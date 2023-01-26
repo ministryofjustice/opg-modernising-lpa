f064bac202f529893a808cf4a844c0a2ed2bdebc
Alex Saunders <acsauk@gmail.com>
2023-01-26 11:42:13 +0000

    covert dumb apostrophes to smart apostrophes

--- app/main_test.go
+++ app/main_test.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -30,5 +32,37 @@ func TestLanguageFilesMatch(t *testing.T) {
 			t.Fail()
 			t.Log("lang/en.json missing: ", k)
 		}
+	}
+}
+
+func TestApostrophesAreCurly(t *testing.T) {
+	loadTranslations := func(path string) map[string]string {
+		data, _ := os.ReadFile(path)
+		var v map[string]string
+		json.Unmarshal(data, &v)
+
+		return v
+	}
+
+	en := loadTranslations("lang/en.json")
+	cy := loadTranslations("lang/cy.json")
+
+	var failures []string
+
+	for k, v := range en {
+		if strings.Contains(v, "'") {
+			failures = append(failures, fmt.Sprintf("lang/en.json %s \n", k))
+		}
+	}
+
+	for k, v := range cy {
+		if strings.Contains(v, "'") {
+			failures = append(failures, fmt.Sprintf("lang/cy.json %s \n", k))
+		}
+	}
+
+	if len(failures) > 0 {
+		t.Log("non-curly apostrophe present in translations:\n", failures)
+		t.Fail()
 	}
 }
