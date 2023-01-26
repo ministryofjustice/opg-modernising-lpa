4481fb4c399a82b1afe82bcd40543c26d14e1f50
Alex Saunders <acsauk@gmail.com>
2023-01-26 15:08:36 +0000

    handle unfound postcodes

--- app/internal/page/your_address.go
+++ app/internal/page/your_address.go
@@ -1,6 +1,7 @@
 package page
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/ministryofjustice/opg-go-common/template"
@@ -53,7 +54,12 @@ func YourAddress(logger Logger, tmpl template.Template, addressClient AddressCli
 				addresses, err := addressClient.LookupPostcode(r.Context(), data.Form.LookupPostcode)
 				if err != nil {
 					logger.Print(err)
-					data.Errors["lookup-postcode"] = "couldNotLookupPostcode"
+
+					if errors.As(err, &place.NotFoundError{}) {
+						data.Errors["lookup-postcode"] = "enterUkPostCode"
+					} else {
+						data.Errors["lookup-postcode"] = "couldNotLookupPostcode"
+					}
 				}
 
 				data.Addresses = addresses
--- app/internal/page/your_address_test.go
+++ app/internal/page/your_address_test.go
@@ -518,6 +518,58 @@ func TestPostYourAddressLookupError(t *testing.T) {
 	mock.AssertExpectationsForObjects(t, addressClient, template, logger)
 }
 
+func TestPostYourAddressNotFoundError(t *testing.T) {
+	w := httptest.NewRecorder()
+	notFoundErr := place.NotFoundError{
+		Statuscode: 400,
+		Message:    "not found",
+	}
+
+	logger := &mockLogger{}
+	logger.
+		On("Print", notFoundErr)
+
+	lpaStore := &mockLpaStore{}
+	lpaStore.
+		On("Get", mock.Anything, "session-id").
+		Return(&Lpa{}, nil)
+
+	addressClient := &mockAddressClient{}
+	addressClient.
+		On("LookupPostcode", mock.Anything, "XYZ").
+		Return([]place.Address{}, notFoundErr)
+
+	template := &mockTemplate{}
+	template.
+		On("Func", w, &yourAddressData{
+			App: appData,
+			Form: &yourAddressForm{
+				Action:         "lookup",
+				LookupPostcode: "XYZ",
+			},
+			Addresses: []place.Address{},
+			Errors: map[string]string{
+				"lookup-postcode": "enterUkPostCode",
+			},
+		}).
+		Return(nil)
+
+	form := url.Values{
+		"action":          {"lookup"},
+		"lookup-postcode": {"XYZ"},
+	}
+
+	r, _ := http.NewRequest(http.MethodPost, "/", strings.NewReader(form.Encode()))
+	r.Header.Add("Content-Type", formUrlEncoded)
+
+	err := YourAddress(logger, template.Func, addressClient, lpaStore)(appData, w, r)
+	resp := w.Result()
+
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	mock.AssertExpectationsForObjects(t, addressClient, template, logger)
+}
+
 func TestPostYourAddressLookupWhenValidationError(t *testing.T) {
 	w := httptest.NewRecorder()
 
