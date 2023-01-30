e412f51f71c9dd3f525c92051458ffa4d43d859a
Alex Saunders <acsauk@gmail.com>
2023-01-30 14:31:15 +0000

    cover isLpaPath

--- app/internal/page/app_test.go
+++ app/internal/page/app_test.go
@@ -1364,3 +1364,29 @@ func TestQueryString(t *testing.T) {
 		})
 	}
 }
+
+func TestIsLpaPath(t *testing.T) {
+	testCases := map[string]struct {
+		url               string
+		expectedIsLpaPage bool
+	}{
+		"dashboard": {
+			url:               Paths.Dashboard + "?someQuery=5",
+			expectedIsLpaPage: false,
+		},
+		"start": {
+			url:               Paths.Start + "?someQuery=6",
+			expectedIsLpaPage: false,
+		},
+		"any other page": {
+			url:               "/other?someQuery=7",
+			expectedIsLpaPage: true,
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, tc.expectedIsLpaPage, IsLpaPath(tc.url))
+		})
+	}
+}
