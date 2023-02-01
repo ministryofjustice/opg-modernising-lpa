9b9e6e9983db0f8db6c8861c066419242e6c1681
Joshua Hawxwell <m@hawx.me>
2023-02-01 08:59:18 +0000

    Use same form for all address pages

--- app/internal/page/choose_attorneys_address.go
+++ app/internal/page/choose_attorneys_address.go
@@ -13,7 +13,7 @@ type chooseAttorneysAddressData struct {
 	Errors    validation.List
 	Attorney  Attorney
 	Addresses []place.Address
-	Form      *chooseAttorneysAddressForm
+	Form      *addressForm
 }
 
 func ChooseAttorneysAddress(logger Logger, tmpl template.Template, addressClient AddressClient, lpaStore LpaStore) Handler {
@@ -33,7 +33,7 @@ func ChooseAttorneysAddress(logger Logger, tmpl template.Template, addressClient
 		data := &chooseAttorneysAddressData{
 			App:      appData,
 			Attorney: attorney,
-			Form:     &chooseAttorneysAddressForm{},
+			Form:     &addressForm{},
 		}
 
 		if attorney.Address.Line1 != "" {
@@ -42,7 +42,7 @@ func ChooseAttorneysAddress(logger Logger, tmpl template.Template, addressClient
 		}
 
 		if r.Method == http.MethodPost {
-			data.Form = readChooseAttorneysAddressForm(r)
+			data.Form = readAddressForm(r)
 			data.Errors = data.Form.Validate()
 
 			if data.Form.Action == "manual" && data.Errors.None() {
@@ -96,65 +96,4 @@ func ChooseAttorneysAddress(logger Logger, tmpl template.Template, addressClient
 
 		return tmpl(w, data)
 	}
-}
-
-type chooseAttorneysAddressForm struct {
-	Action         string
-	LookupPostcode string
-	Address        *place.Address
-}
-
-func readChooseAttorneysAddressForm(r *http.Request) *chooseAttorneysAddressForm {
-	f := &chooseAttorneysAddressForm{}
-	f.Action = r.PostFormValue("action")
-
-	switch f.Action {
-	case "lookup":
-		f.LookupPostcode = postFormString(r, "lookup-postcode")
-
-	case "select":
-		f.LookupPostcode = postFormString(r, "lookup-postcode")
-		selectAddress := r.PostFormValue("select-address")
-		if selectAddress != "" {
-			f.Address = DecodeAddress(selectAddress)
-		}
-
-	case "manual":
-		f.Address = &place.Address{
-			Line1:      postFormString(r, "address-line-1"),
-			Line2:      postFormString(r, "address-line-2"),
-			Line3:      postFormString(r, "address-line-3"),
-			TownOrCity: postFormString(r, "address-town"),
-			Postcode:   postFormString(r, "address-postcode"),
-		}
-	}
-
-	return f
-}
-
-func (f *chooseAttorneysAddressForm) Validate() validation.List {
-	var errors validation.List
-
-	switch f.Action {
-	case "lookup":
-		errors.String("lookup-postcode", "postcode", f.LookupPostcode,
-			validation.Empty())
-
-	case "select":
-		errors.Address("select-address", "address", f.Address,
-			validation.Selected())
-
-	case "manual":
-		errors.String("address-line-1", "addressLine1", f.Address.Line1,
-			validation.Empty(),
-			validation.StringTooLong(50))
-		errors.String("address-line-2", "addressLine2Label", f.Address.Line2,
-			validation.StringTooLong(50))
-		errors.String("address-line-3", "addressLine3Label", f.Address.Line3,
-			validation.StringTooLong(50))
-		errors.String("address-town", "townOrCity", f.Address.TownOrCity,
-			validation.Empty())
-	}
-
-	return errors
 }
