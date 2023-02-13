1570ffd7438ac80ee60ab862a108367c1e95a8c7
Joshua Hawxwell <m@hawx.me>
2023-02-13 13:50:02 +0000

    Split page package by actor

---
+++ app/internal/page/donor/identity_with_todo_test.go
@@ -0,0 +1,45 @@
+package donor
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/identity"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/page"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestGetIdentityWithTodo(t *testing.T) {
+	w := httptest.NewRecorder()
+
+	template := &mockTemplate{}
+	template.
+		On("Func", w, &identityWithTodoData{
+			App:            appData,
+			IdentityOption: identity.Passport,
+		}).
+		Return(nil)
+
+	r, _ := http.NewRequest(http.MethodGet, "/", nil)
+
+	err := IdentityWithTodo(template.Func, identity.Passport)(appData, w, r)
+	resp := w.Result()
+
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	mock.AssertExpectationsForObjects(t, template)
+}
+
+func TestPostIdentityWithTodo(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodPost, "/", nil)
+
+	err := IdentityWithTodo(nil, identity.Passport)(appData, w, r)
+	resp := w.Result()
+
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusFound, resp.StatusCode)
+	assert.Equal(t, "/lpa/lpa-id"+page.Paths.ReadYourLpa, resp.Header.Get("Location"))
+}
---
+++ app/internal/page/donor/when_can_the_lpa_be_used.go
@@ -0,0 +1,75 @@
+package donor
+
+import (
+	"net/http"
+
+	"github.com/ministryofjustice/opg-go-common/template"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/page"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/validation"
+)
+
+type whenCanTheLpaBeUsedData struct {
+	App       page.AppData
+	Errors    validation.List
+	When      string
+	Completed bool
+	Lpa       *page.Lpa
+}
+
+func WhenCanTheLpaBeUsed(tmpl template.Template, lpaStore page.LpaStore) page.Handler {
+	return func(appData page.AppData, w http.ResponseWriter, r *http.Request) error {
+		lpa, err := lpaStore.Get(r.Context())
+		if err != nil {
+			return err
+		}
+
+		data := &whenCanTheLpaBeUsedData{
+			App:       appData,
+			When:      lpa.WhenCanTheLpaBeUsed,
+			Completed: lpa.Tasks.WhenCanTheLpaBeUsed.Completed(),
+			Lpa:       lpa,
+		}
+
+		if r.Method == http.MethodPost {
+			form := readWhenCanTheLpaBeUsedForm(r)
+			data.Errors = form.Validate()
+
+			if data.Errors.None() || form.AnswerLater {
+				if form.AnswerLater {
+					lpa.Tasks.WhenCanTheLpaBeUsed = page.TaskInProgress
+				} else {
+					lpa.WhenCanTheLpaBeUsed = form.When
+					lpa.Tasks.WhenCanTheLpaBeUsed = page.TaskCompleted
+				}
+				if err := lpaStore.Put(r.Context(), lpa); err != nil {
+					return err
+				}
+
+				return appData.Redirect(w, r, lpa, page.Paths.Restrictions)
+			}
+		}
+
+		return tmpl(w, data)
+	}
+}
+
+type whenCanTheLpaBeUsedForm struct {
+	AnswerLater bool
+	When        string
+}
+
+func readWhenCanTheLpaBeUsedForm(r *http.Request) *whenCanTheLpaBeUsedForm {
+	return &whenCanTheLpaBeUsedForm{
+		AnswerLater: page.PostFormString(r, "answer-later") == "1",
+		When:        page.PostFormString(r, "when"),
+	}
+}
+
+func (f *whenCanTheLpaBeUsedForm) Validate() validation.List {
+	var errors validation.List
+
+	errors.String("when", "whenYourAttorneysCanUseYourLpa", f.When,
+		validation.Select(page.UsedWhenRegistered, page.UsedWhenCapacityLost))
+
+	return errors
+}
