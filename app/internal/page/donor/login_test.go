48bc10259bda051f88ae28611d56250a8fc18b35
Joshua Hawxwell <m@hawx.me>
2023-02-24 07:39:05 +0000

    Refactor donor login handlers to be like certificateprovider

--- app/internal/page/donor/login_test.go
+++ app/internal/page/donor/login_test.go
@@ -6,79 +6,55 @@ import (
 	"testing"
 
 	"github.com/gorilla/sessions"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/localize"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/page"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/sesh"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
 func TestLogin(t *testing.T) {
-	w := httptest.NewRecorder()
-	r, _ := http.NewRequest(http.MethodGet, "/?locale=cy", nil)
-
-	client := newMockOneLoginClient(t)
-	client.
-		On("AuthCodeURL", "i am random", "i am random", "cy", false).
-		Return("http://auth")
-
-	sessionStore := newMockSessionStore(t)
-
-	session := sessions.NewSession(sessionStore, "params")
-
-	session.Options = &sessions.Options{
-		Path:     "/",
-		MaxAge:   600,
-		SameSite: http.SameSiteLaxMode,
-		HttpOnly: true,
-		Secure:   true,
-	}
-	session.Values = map[any]any{
-		"one-login": &sesh.OneLoginSession{State: "i am random", Nonce: "i am random", Locale: "cy"},
+	testcases := map[localize.Lang]string{
+		localize.En: "en",
+		localize.Cy: "cy",
 	}
 
-	sessionStore.
-		On("Save", r, w, session).
-		Return(nil)
+	for lang, str := range testcases {
+		t.Run(str, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			r, _ := http.NewRequest(http.MethodGet, "/", nil)
 
-	Login(nil, client, sessionStore, func(int) string { return "i am random" })(w, r)
-	resp := w.Result()
+			client := newMockOneLoginClient(t)
+			client.
+				On("AuthCodeURL", "i am random", "i am random", str, false).
+				Return("http://auth")
 
-	assert.Equal(t, http.StatusFound, resp.StatusCode)
-	assert.Equal(t, "http://auth", resp.Header.Get("Location"))
-}
+			sessionStore := newMockSessionStore(t)
 
-func TestLoginDefaultLocale(t *testing.T) {
-	w := httptest.NewRecorder()
-	r, _ := http.NewRequest(http.MethodGet, "/", nil)
+			session := sessions.NewSession(sessionStore, "params")
 
-	client := newMockOneLoginClient(t)
-	client.
-		On("AuthCodeURL", "i am random", "i am random", "en", false).
-		Return("http://auth")
+			session.Options = &sessions.Options{
+				Path:     "/",
+				MaxAge:   600,
+				SameSite: http.SameSiteLaxMode,
+				HttpOnly: true,
+				Secure:   true,
+			}
+			session.Values = map[any]any{
+				"one-login": &sesh.OneLoginSession{State: "i am random", Nonce: "i am random", Locale: str},
+			}
 
-	sessionStore := newMockSessionStore(t)
+			sessionStore.
+				On("Save", r, w, session).
+				Return(nil)
 
-	session := sessions.NewSession(sessionStore, "params")
+			Login(nil, client, sessionStore, func(int) string { return "i am random" })(page.AppData{Lang: lang}, w, r)
+			resp := w.Result()
 
-	session.Options = &sessions.Options{
-		Path:     "/",
-		MaxAge:   600,
-		SameSite: http.SameSiteLaxMode,
-		HttpOnly: true,
-		Secure:   true,
+			assert.Equal(t, http.StatusFound, resp.StatusCode)
+			assert.Equal(t, "http://auth", resp.Header.Get("Location"))
+		})
 	}
-	session.Values = map[any]any{
-		"one-login": &sesh.OneLoginSession{State: "i am random", Nonce: "i am random", Locale: "en"},
-	}
-
-	sessionStore.
-		On("Save", r, w, session).
-		Return(nil)
-
-	Login(nil, client, sessionStore, func(int) string { return "i am random" })(w, r)
-	resp := w.Result()
-
-	assert.Equal(t, http.StatusFound, resp.StatusCode)
-	assert.Equal(t, "http://auth", resp.Header.Get("Location"))
 }
 
 func TestLoginWhenStoreSaveError(t *testing.T) {
@@ -99,7 +75,7 @@ func TestLoginWhenStoreSaveError(t *testing.T) {
 		On("Save", r, w, mock.Anything).
 		Return(expectedError)
 
-	Login(logger, client, sessionStore, func(int) string { return "i am random" })(w, r)
+	Login(logger, client, sessionStore, func(int) string { return "i am random" })(testAppData, w, r)
 	resp := w.Result()
 
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
