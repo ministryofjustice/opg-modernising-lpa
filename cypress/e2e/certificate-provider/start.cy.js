50db75ec73e54dec03f30cf60030596bf1003285
Alex Saunders <acsauk@gmail.com>
2023-02-27 11:47:48 +0000

    test fix

--- cypress/e2e/certificate-provider/start.cy.js
+++ cypress/e2e/certificate-provider/start.cy.js
@@ -1,7 +1,6 @@
 describe('Start', () => {
     beforeEach(() => {
-        cy.visit('/testing-start?redirect=/your-details&withIncompleteAttorneys=1&withCP=1&paymentComplete=1');
-        cy.visitLpa('/task-list');
+        cy.visit('/testing-start?startCpFlowWithoutId=1');
     });
 
     it('can be completed', () => {
