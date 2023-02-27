ea918f44a79a00246fa0968f4a003a8546617674
Alex Saunders <acsauk@gmail.com>
2023-02-27 15:13:41 +0000

    try then

--- cypress/e2e/certificate-provider/enter-reference-code.cy.js
+++ cypress/e2e/certificate-provider/enter-reference-code.cy.js
@@ -10,7 +10,9 @@ describe('Enter reference code', () => {
         cy.get('#f-reference-code').type('abcdef123456');
         cy.contains('Continue').click();
 
-        cy.location('pathname').should('be.oneOf', ['/certificate-provider-login-callback', '/prove-identity-welcome'])
+        cy.then(() => {
+            cy.location('pathname').should('be.oneOf', ['/certificate-provider-login-callback', '/prove-identity-welcome'])
+        });
     });
 
     it('errors when empty code', () => {
