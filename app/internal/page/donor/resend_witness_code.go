bcd1a9740613c8dd8ac5c054a76ff5af6c2f12be
Joshua Hawxwell <m@hawx.me>
2023-03-06 12:30:07 +0000

    MLPAB-549 Add page to resend the witness code (#375)

--- app/internal/page/donor/resend_witness_code.go
+++ app/internal/page/donor/resend_witness_code.go
@@ -2,36 +2,40 @@ package donor
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/ministryofjustice/opg-go-common/template"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/page"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/validation"
 )
 
-type witnessingYourSignatureData struct {
+type resendWitnessCodeData struct {
 	App    page.AppData
 	Errors validation.List
-	Lpa    *page.Lpa
 }
 
-func WitnessingYourSignature(tmpl template.Template, lpaStore LpaStore, witnessCodeSender WitnessCodeSender) page.Handler {
+func ResendWitnessCode(tmpl template.Template, lpaStore LpaStore, witnessCodeSender WitnessCodeSender, now func() time.Time) page.Handler {
 	return func(appData page.AppData, w http.ResponseWriter, r *http.Request) error {
 		lpa, err := lpaStore.Get(r.Context())
 		if err != nil {
 			return err
 		}
 
+		data := &resendWitnessCodeData{
+			App: appData,
+		}
+
 		if r.Method == http.MethodPost {
+			if !lpa.WitnessCodes.CanRequest(now()) {
+				data.Errors.Add("request", validation.CustomError{Label: "pleaseWaitOneMinute"})
+				return tmpl(w, data)
+			}
+
 			if err := witnessCodeSender.Send(r.Context(), lpa); err != nil {
 				return err
 			}
 
 			return appData.Redirect(w, r, lpa, page.Paths.WitnessingAsCertificateProvider)
-		}
-
-		data := &witnessingYourSignatureData{
-			App: appData,
-			Lpa: lpa,
 		}
 
 		return tmpl(w, data)
