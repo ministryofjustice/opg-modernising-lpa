65acc72f5820d1f62626b2807b74df418487a062
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-03-07 12:16:42 +0000

    clean up

--- terraform/environment/healthcheck.tf
+++ terraform/environment/healthcheck.tf
@@ -1,3 +1,19 @@
+resource "aws_route53_health_check" "health_check" {
+  fqdn              = aws_route53_record.app.fqdn
+  reference_name    = "${substr(local.environment_name, 0, 20)}-health-check"
+  port              = 443
+  type              = "HTTPS"
+  failure_threshold = 1
+  request_interval  = 30
+  resource_path     = "/health-check"
+  measure_latency   = true
+  regions           = ["us-east-1", "eu-west-1", "ap-southeast-1"]
+  provider          = aws.global
+  tags = {
+    Name = "${local.environment_name}-health-check"
+  }
+}
+
 resource "aws_cloudwatch_metric_alarm" "health_check" {
   alarm_description   = "${local.environment_name} health check"
   alarm_name          = "${local.environment_name}-healthcheck-alarm"
@@ -15,20 +31,4 @@ resource "aws_cloudwatch_metric_alarm" "health_check" {
   }
 
   provider = aws.global
-}
-
-resource "aws_route53_health_check" "health_check" {
-  fqdn              = aws_route53_record.app.fqdn
-  reference_name    = "${substr(local.environment_name, 0, 20)}-health-check"
-  port              = 443
-  type              = "HTTPS"
-  failure_threshold = 1
-  request_interval  = 30
-  resource_path     = "/health-check"
-  measure_latency   = true
-  regions           = ["us-east-1", "eu-west-1", "ap-southeast-1"]
-  provider          = aws.global
-  tags = {
-    Name = "${local.environment_name}-health-check"
-  }
 }
