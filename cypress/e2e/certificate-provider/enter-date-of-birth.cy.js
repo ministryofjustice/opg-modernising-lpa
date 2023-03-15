210c2ae2cb01ec71c684516d4a124dcb70f4e7ed
Alex Saunders <acsauk@gmail.com>
2023-03-15 13:46:34 +0000

    fix block for over 100s

--- cypress/e2e/certificate-provider/enter-date-of-birth.cy.js
+++ cypress/e2e/certificate-provider/enter-date-of-birth.cy.js
@@ -13,8 +13,25 @@ describe('Enter date of birth', () => {
         cy.contains('button', 'Continue').click();
 
         cy.url().should('contain', '/certificate-provider-enter-mobile-number');
+    });
+
+    it('can be over 100', () => {
+        cy.checkA11yApp();
+
+        cy.get('#f-date-of-birth').type('1');
+        cy.get('#f-date-of-birth-month').type('2');
+        cy.get('#f-date-of-birth-year').type('1900');
+
+        cy.contains('button', 'Continue').click();
+
+        cy.url().should('contain', '/certificate-provider-enter-date-of-birth');
+        cy.contains('By saving this section, you confirm that the person is more than 100 years old')
 
         cy.checkA11yApp();
+
+        cy.contains('button', 'Continue').click();
+
+        cy.url().should('contain', '/certificate-provider-enter-mobile-number');
     });
 
     it('errors when empty', () => {
