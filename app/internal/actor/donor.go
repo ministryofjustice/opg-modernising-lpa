19a2d39dc2234993f736262af2c9e8e57e1ac1b1
Alex Saunders <acsauk@gmail.com>
2023-03-16 11:12:15 +0000

    update witness code personalisations and notify template IDs

--- app/internal/actor/donor.go
+++ app/internal/actor/donor.go
@@ -16,6 +16,16 @@ type Donor struct {
 	Address     place.Address
 }
 
-func (p Donor) FullName() string {
-	return fmt.Sprintf("%s %s", p.FirstNames, p.LastName)
+func (d Donor) FullName() string {
+	return fmt.Sprintf("%s %s", d.FirstNames, d.LastName)
+}
+
+func (d Donor) PossessiveFullName() string {
+	format := "%s %s’s"
+
+	if d.LastName[len(d.LastName)-1:] == "s" {
+		format = "%s %s’"
+	}
+
+	return fmt.Sprintf(format, d.FirstNames, d.LastName)
 }
