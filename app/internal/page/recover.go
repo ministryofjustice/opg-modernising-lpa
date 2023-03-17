6b99080a2b15c874bcafca46049e403bc0a504cd
Joshua Hawxwell <m@hawx.me>
2023-03-17 08:44:19 +0000

    Add a recover middleware

---
+++ app/internal/page/recover.go
@@ -0,0 +1,46 @@
+package page
+
+import (
+	"fmt"
+	"net/http"
+	"runtime/debug"
+	"strings"
+
+	"github.com/ministryofjustice/opg-go-common/template"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/localize"
+)
+
+type recoverError struct {
+	err   any
+	stack []byte
+}
+
+func (e recoverError) Error() string { return "recover error" }
+func (e recoverError) Title() string { return fmt.Sprint(e.err) }
+func (e recoverError) Data() any     { return string(e.stack) }
+
+func Recover(tmpl template.Template, logger Logger, bundle Bundle, next http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				logger.Request(r, recoverError{err: err, stack: debug.Stack()})
+				w.WriteHeader(http.StatusInternalServerError)
+
+				appData := AppData{CookieConsentSet: true, Paths: Paths}
+				if strings.HasPrefix(r.URL.Path, "/cy/") {
+					appData.Lang = localize.Cy
+				} else {
+					appData.Lang = localize.En
+				}
+				appData.Localizer = bundle.For(appData.Lang.String())
+
+				if terr := tmpl(w, &errorData{App: appData}); terr != nil {
+					logger.Print(fmt.Sprintf("Error rendering page: %s", terr.Error()))
+					http.Error(w, "Encountered an error", http.StatusInternalServerError)
+				}
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	}
+}
