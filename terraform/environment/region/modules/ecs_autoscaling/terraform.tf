2e0690c2f4926647458d7dd48c0d4c0a5ff70320
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-04-17 08:45:40 +0100

    MLPAB-812 - Set up ECS autoscaling for app service (#440)

---
+++ terraform/environment/region/modules/ecs_autoscaling/terraform.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.2.2"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      configuration_aliases = [
+        aws.region,
+      ]
+    }
+  }
+}
+
+data "aws_region" "current" {
+  provider = aws.region
+}
+
+data "aws_caller_identity" "current" {
+  provider = aws.region
+}
+
+data "aws_default_tags" "current" {
+  provider = aws.region
+}
