2559e126b78cadeafc50faaff49fb1dca305b040
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-04-20 15:47:05 +0100

    add redirect to welsh maintenance

--- terraform/environment/region/modules/app/alb.tf
+++ terraform/environment/region/modules/app/alb.tf
@@ -81,7 +81,7 @@ resource "aws_lb_listener_rule" "app_maintenance" {
 
     redirect {
       host        = "maintenance.opg.service.justice.gov.uk"
-      path        = "/en-gb/make-a-lasting-power-of-attorney"
+      path        = "/en-gb/make-a-lasting-power-of-attorney" # temporarily the english make a lasting power of attorney maintenance page
       query       = ""
       port        = "443"
       protocol    = "HTTPS"
@@ -91,6 +91,36 @@ resource "aws_lb_listener_rule" "app_maintenance" {
   condition {
     path_pattern {
       values = ["/maintenance"]
+    }
+  }
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to the condition as this is modified by a script
+      # when putting the service into maintenance mode.
+      condition,
+    ]
+  }
+  provider = aws.region
+}
+
+resource "aws_lb_listener_rule" "app_maintenance_welsh" {
+  listener_arn = aws_lb_listener.app_loadbalancer.arn
+  priority     = 100 # Specifically set so that maintenance mode scripts can locate the correct rule to modify
+  action {
+    type = "redirect"
+
+    redirect {
+      host        = "maintenance.opg.service.justice.gov.uk"
+      path        = "/cy/defnyddio-atwrneiaeth-arhosol" # temporarily the welsh use a lasting power of attorney maintenance page
+      query       = ""
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_302"
+    }
+  }
+  condition {
+    path_pattern {
+      values = ["/cy/maintenance"]
     }
   }
   lifecycle {
