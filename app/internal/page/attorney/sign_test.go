df16c7efc1b5db43abf29c7d2ae8aa39f682ced5
Joshua Hawxwell <m@hawx.me>
2023-04-24 11:58:14 +0100

    Add sign as attorney page

---
+++ app/internal/page/attorney/sign_test.go
@@ -0,0 +1,334 @@
+package attorney
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
+
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/actor"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/page"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/validation"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestGetSign(t *testing.T) {
+	testcases := map[string]struct {
+		appData            page.AppData
+		lpa                *page.Lpa
+		isReplacement      bool
+		usedWhenRegistered bool
+	}{
+		"attorney use when registered": {
+			appData: testAppData,
+			lpa: &page.Lpa{
+				WhenCanTheLpaBeUsed: page.UsedWhenRegistered,
+				Attorneys: actor.Attorneys{
+					{ID: "attorney-id", FirstNames: "Bob", LastName: "Smith"},
+					{ID: "other", FirstNames: "Dave", LastName: "Smith"},
+				},
+			},
+			usedWhenRegistered: true,
+		},
+		"attorney use when capacity lost": {
+			appData: testAppData,
+			lpa: &page.Lpa{
+				WhenCanTheLpaBeUsed: page.UsedWhenCapacityLost,
+				Attorneys: actor.Attorneys{
+					{ID: "attorney-id", FirstNames: "Bob", LastName: "Smith"},
+					{ID: "other", FirstNames: "Dave", LastName: "Smith"},
+				},
+			},
+		},
+		"replacement attorney use when registered": {
+			appData: testReplacementAppData,
+			lpa: &page.Lpa{
+				WhenCanTheLpaBeUsed: page.UsedWhenRegistered,
+				ReplacementAttorneys: actor.Attorneys{
+					{ID: "attorney-id", FirstNames: "Bob", LastName: "Smith"},
+					{ID: "other", FirstNames: "Dave", LastName: "Smith"},
+				},
+			},
+			isReplacement:      true,
+			usedWhenRegistered: true,
+		},
+		"replacement attorney use when capacity lost": {
+			appData: testReplacementAppData,
+			lpa: &page.Lpa{
+				WhenCanTheLpaBeUsed: page.UsedWhenCapacityLost,
+				ReplacementAttorneys: actor.Attorneys{
+					{ID: "attorney-id", FirstNames: "Bob", LastName: "Smith"},
+					{ID: "other", FirstNames: "Dave", LastName: "Smith"},
+				},
+			},
+			isReplacement: true,
+		},
+	}
+
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			r, _ := http.NewRequest(http.MethodGet, "/", nil)
+			w := httptest.NewRecorder()
+
+			template := newMockTemplate(t)
+			template.
+				On("Execute", w, &signData{
+					App:                        tc.appData,
+					Form:                       &signForm{},
+					Attorney:                   actor.Attorney{ID: "attorney-id", FirstNames: "Bob", LastName: "Smith"},
+					IsReplacement:              tc.isReplacement,
+					LpaCanBeUsedWhenRegistered: tc.usedWhenRegistered,
+				}).
+				Return(nil)
+
+			lpaStore := newMockLpaStore(t)
+			lpaStore.
+				On("Get", r.Context()).
+				Return(tc.lpa, nil)
+
+			err := Sign(template.Execute, lpaStore)(tc.appData, w, r)
+			resp := w.Result()
+
+			assert.Nil(t, err)
+			assert.Equal(t, http.StatusOK, resp.StatusCode)
+		})
+	}
+}
+
+func TestGetSignWhenAttorneyDoesNotExist(t *testing.T) {
+	testcases := map[string]struct {
+		appData page.AppData
+		lpa     *page.Lpa
+	}{
+		"attorney": {
+			appData: testAppData,
+			lpa: &page.Lpa{
+				ReplacementAttorneys: actor.Attorneys{
+					{ID: "attorney-id", FirstNames: "Bob", LastName: "Smith"},
+				},
+			},
+		},
+		"replacement attorney": {
+			appData: testReplacementAppData,
+			lpa: &page.Lpa{
+				Attorneys: actor.Attorneys{
+					{ID: "attorney-id", FirstNames: "Bob", LastName: "Smith"},
+				},
+			},
+		},
+	}
+
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			r, _ := http.NewRequest(http.MethodGet, "/", nil)
+			w := httptest.NewRecorder()
+
+			lpaStore := newMockLpaStore(t)
+			lpaStore.
+				On("Get", r.Context()).
+				Return(tc.lpa, nil)
+
+			err := Sign(nil, lpaStore)(tc.appData, w, r)
+			resp := w.Result()
+
+			assert.Nil(t, err)
+			assert.Equal(t, http.StatusFound, resp.StatusCode)
+			assert.Equal(t, page.Paths.Attorney.Start, resp.Header.Get("Location"))
+		})
+	}
+}
+
+func TestGetSignOnStoreError(t *testing.T) {
+	r, _ := http.NewRequest(http.MethodGet, "/", nil)
+	w := httptest.NewRecorder()
+
+	template := newMockTemplate(t)
+
+	lpaStore := newMockLpaStore(t)
+	lpaStore.
+		On("Get", r.Context()).
+		Return(&page.Lpa{}, expectedError)
+
+	err := Sign(template.Execute, lpaStore)(testAppData, w, r)
+	resp := w.Result()
+
+	assert.Equal(t, expectedError, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
+
+func TestGetSignOnTemplateError(t *testing.T) {
+	r, _ := http.NewRequest(http.MethodGet, "/", nil)
+	w := httptest.NewRecorder()
+
+	template := newMockTemplate(t)
+	template.
+		On("Execute", w, mock.Anything).
+		Return(expectedError)
+
+	lpaStore := newMockLpaStore(t)
+	lpaStore.
+		On("Get", r.Context()).
+		Return(&page.Lpa{
+			Attorneys: actor.Attorneys{{ID: "attorney-id"}},
+		}, nil)
+
+	err := Sign(template.Execute, lpaStore)(testAppData, w, r)
+	resp := w.Result()
+
+	assert.Equal(t, expectedError, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
+
+func TestPostSign(t *testing.T) {
+	testcases := map[string]struct {
+		appData    page.AppData
+		lpa        *page.Lpa
+		updatedLpa *page.Lpa
+	}{
+		"attorney": {
+			appData: testAppData,
+			lpa: &page.Lpa{
+				Attorneys: actor.Attorneys{{ID: "attorney-id", FirstNames: "Bob", LastName: "Smith"}},
+			},
+			updatedLpa: &page.Lpa{
+				Attorneys:               actor.Attorneys{{ID: "attorney-id", FirstNames: "Bob", LastName: "Smith"}},
+				AttorneyProvidedDetails: actor.Attorneys{{ID: "attorney-id", Confirmed: true}},
+			},
+		},
+		"replacement attorney": {
+			appData: testReplacementAppData,
+			lpa: &page.Lpa{
+				ReplacementAttorneys: actor.Attorneys{{ID: "attorney-id", FirstNames: "Bob", LastName: "Smith"}},
+			},
+			updatedLpa: &page.Lpa{
+				ReplacementAttorneys:               actor.Attorneys{{ID: "attorney-id", FirstNames: "Bob", LastName: "Smith"}},
+				ReplacementAttorneyProvidedDetails: actor.Attorneys{{ID: "attorney-id", Confirmed: true}},
+			},
+		},
+	}
+
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			form := url.Values{
+				"confirm": {"1"},
+			}
+
+			r, _ := http.NewRequest(http.MethodPost, "/", strings.NewReader(form.Encode()))
+			r.Header.Add("Content-Type", page.FormUrlEncoded)
+
+			w := httptest.NewRecorder()
+
+			lpaStore := newMockLpaStore(t)
+			lpaStore.
+				On("Get", r.Context()).
+				Return(tc.lpa, nil)
+			lpaStore.
+				On("Put", r.Context(), tc.updatedLpa).
+				Return(nil)
+
+			err := Sign(nil, lpaStore)(tc.appData, w, r)
+			resp := w.Result()
+
+			assert.Nil(t, err)
+			assert.Equal(t, http.StatusFound, resp.StatusCode)
+			assert.Equal(t, page.Paths.Attorney.NextPage, resp.Header.Get("Location"))
+		})
+	}
+}
+
+func TestPostSignWhenStoreError(t *testing.T) {
+	form := url.Values{
+		"confirm": {"1"},
+	}
+
+	r, _ := http.NewRequest(http.MethodPost, "/", strings.NewReader(form.Encode()))
+	r.Header.Add("Content-Type", page.FormUrlEncoded)
+
+	w := httptest.NewRecorder()
+
+	lpaStore := newMockLpaStore(t)
+	lpaStore.
+		On("Get", r.Context()).
+		Return(&page.Lpa{
+			Attorneys: actor.Attorneys{{ID: "attorney-id", FirstNames: "Bob", LastName: "Smith"}},
+		}, nil)
+	lpaStore.
+		On("Put", r.Context(), mock.Anything).
+		Return(expectedError)
+
+	err := Sign(nil, lpaStore)(testAppData, w, r)
+	resp := w.Result()
+
+	assert.Equal(t, expectedError, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
+
+func TestPostSignOnValidationError(t *testing.T) {
+	form := url.Values{}
+
+	lpa := &page.Lpa{
+		Attorneys: actor.Attorneys{{ID: "attorney-id", FirstNames: "Bob", LastName: "Smith"}},
+	}
+
+	r, _ := http.NewRequest(http.MethodPost, "/", strings.NewReader(form.Encode()))
+	r.Header.Add("Content-Type", page.FormUrlEncoded)
+
+	w := httptest.NewRecorder()
+
+	lpaStore := newMockLpaStore(t)
+	lpaStore.
+		On("Get", r.Context()).
+		Return(lpa, nil)
+
+	template := newMockTemplate(t)
+	template.
+		On("Execute", w, &signData{
+			App:      testAppData,
+			Form:     &signForm{},
+			Attorney: actor.Attorney{ID: "attorney-id", FirstNames: "Bob", LastName: "Smith"},
+			Errors:   validation.With("confirm", validation.SelectError{Label: "confirm"}),
+		}).
+		Return(nil)
+
+	err := Sign(template.Execute, lpaStore)(testAppData, w, r)
+	resp := w.Result()
+
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
+
+func TestReadSignForm(t *testing.T) {
+	form := url.Values{
+		"confirm": {"1"},
+	}
+
+	r, _ := http.NewRequest(http.MethodPost, "/", strings.NewReader(form.Encode()))
+	r.Header.Add("Content-Type", page.FormUrlEncoded)
+
+	assert.Equal(t, &signForm{Confirm: true}, readSignForm(r))
+}
+
+func TestValidateSignForm(t *testing.T) {
+	testCases := map[string]struct {
+		form   signForm
+		errors validation.List
+	}{
+		"true": {
+			form: signForm{
+				Confirm: true,
+			},
+		},
+		"false": {
+			form:   signForm{},
+			errors: validation.With("confirm", validation.SelectError{Label: "confirm"}),
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, tc.errors, tc.form.Validate())
+		})
+	}
+}
