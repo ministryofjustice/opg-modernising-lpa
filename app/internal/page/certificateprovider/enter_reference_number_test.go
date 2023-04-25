1293d6ffb8c9afa96d0d29709b463069d583ee42
Joshua Hawxwell <m@hawx.me>
2023-04-25 13:04:56 +0100

    Strip dashes and spaces in reference numbers, and update content

--- app/internal/page/certificateprovider/enter_reference_number_test.go
+++ app/internal/page/certificateprovider/enter_reference_number_test.go
@@ -88,7 +88,7 @@ func TestPostEnterReferenceNumber(t *testing.T) {
 	for name, tc := range testCases {
 		t.Run(name, func(t *testing.T) {
 			form := url.Values{
-				"reference-number": {"aRefNumber12"},
+				"reference-number": {"a Ref-Number12"},
 			}
 
 			w := httptest.NewRecorder()
@@ -144,7 +144,7 @@ func TestPostEnterReferenceNumberOnDataStoreError(t *testing.T) {
 
 func TestPostEnterReferenceNumberOnDataStoreNotFoundError(t *testing.T) {
 	form := url.Values{
-		"reference-number": {"aRefNumber12"},
+		"reference-number": {"a Ref-Number12"},
 	}
 
 	w := httptest.NewRecorder()
@@ -153,7 +153,7 @@ func TestPostEnterReferenceNumberOnDataStoreNotFoundError(t *testing.T) {
 
 	data := enterReferenceNumberData{
 		App:    testAppData,
-		Form:   &enterReferenceNumberForm{ReferenceNumber: "aRefNumber12"},
+		Form:   &enterReferenceNumberForm{ReferenceNumber: "aRefNumber12", ReferenceNumberRaw: "a Ref-Number12"},
 		Errors: validation.With("reference-number", validation.CustomError{Label: "incorrectReferenceNumber"}),
 	}
 
@@ -177,7 +177,7 @@ func TestPostEnterReferenceNumberOnDataStoreNotFoundError(t *testing.T) {
 
 func TestPostEnterReferenceNumberOnLpaStoreError(t *testing.T) {
 	form := url.Values{
-		"reference-number": {"aRefNumber12"},
+		"reference-number": {"a Ref-Number12"},
 	}
 
 	w := httptest.NewRecorder()
