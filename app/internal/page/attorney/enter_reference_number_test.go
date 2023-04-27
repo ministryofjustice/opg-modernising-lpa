4fa22fbdf7be6a2da59399215417a843be63c288
Joshua Hawxwell <m@hawx.me>
2023-04-27 13:51:06 +0100

    MLPAB-727 Add task list for attorneys (#459)

--- app/internal/page/attorney/enter_reference_number_test.go
+++ app/internal/page/attorney/enter_reference_number_test.go
@@ -155,7 +155,7 @@ func TestPostEnterReferenceNumber(t *testing.T) {
 
 			assert.Nil(t, err)
 			assert.Equal(t, http.StatusFound, resp.StatusCode)
-			assert.Equal(t, page.Paths.Attorney.CheckYourName, resp.Header.Get("Location"))
+			assert.Equal(t, page.Paths.Attorney.TaskList, resp.Header.Get("Location"))
 		})
 	}
 }
