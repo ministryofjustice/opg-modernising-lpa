71ef7e8da56234b2a66150c2b5963c1739b87c2e
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-05-02 11:20:09 +0100

    use parameter store for persistent and changable variable

---
+++ terraform/environment/parameters.tf
@@ -0,0 +1,24 @@
+resource "aws_ssm_parameter" "container_version" {
+  name     = "/modernising-lpa/container-version/${local.environment_name}"
+  type     = "String"
+  value    = var.container_version
+  provider = aws.management_global
+}
+
+resource "aws_ssm_parameter" "dns_target_region" {
+  name     = "/modernising-lpa/dns-target-region/${local.environment_name}"
+  type     = "String"
+  value    = "eu-west-1"
+  provider = aws.management_global
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+data "aws_ssm_parameter" "dns_target_region" {
+  name = aws_ssm_parameter.dns_target_region.name
+}
+
+output "region_target" {
+  value = data.aws_ssm_parameter.dns_target_region.value == "eu-west-1" ? "region one" : "region two"
+}
