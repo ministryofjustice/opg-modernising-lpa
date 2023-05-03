5b2d1839d615837ed0d4ab21569fd7143a240a3a
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-05-03 09:28:14 +0100

    MLPAB-857 - Add job to switch between deployed regions (#463)

---
+++ terraform/environment/parameters.tf
@@ -0,0 +1,21 @@
+resource "aws_ssm_parameter" "container_version" {
+  name     = "/modernising-lpa/container-version/${local.environment_name}"
+  type     = "String"
+  value    = var.container_version
+  provider = aws.management_global
+}
+
+resource "aws_ssm_parameter" "dns_target_region" {
+  provider = aws.management_global
+  name     = "/modernising-lpa/dns-target-region/${local.environment_name}"
+  type     = "String"
+  value    = "eu-west-1"
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+data "aws_ssm_parameter" "dns_target_region" {
+  provider = aws.management_global
+  name     = aws_ssm_parameter.dns_target_region.name
+}
