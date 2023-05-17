a878b00879d2da25455e1c4fed9a98800dfb5ca2
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-05-17 11:29:50 +0100

    remove second region account resources

--- terraform/account/regions.tf
+++ terraform/account/regions.tf
@@ -12,19 +12,19 @@ module "eu_west_1" {
   }
 }
 
-module "eu_west_2" {
-  source                             = "./region"
-  count                              = contains(local.account.regions, "eu-west-2") ? 1 : 0
-  network_cidr_block                 = "10.162.0.0/16"
-  cloudwatch_log_group_kms_key_alias = aws_kms_alias.cloudwatch_alias_eu_west_2.name
-  sns_kms_key_alias                  = aws_kms_alias.sns_alias_eu_west_2.name
-  secrets_manager_kms_key_alias      = aws_kms_alias.secrets_manager_alias_eu_west_2.name
-  providers = {
-    aws.region     = aws.eu_west_2
-    aws.management = aws.management_eu_west_2
-    aws.global     = aws.global
-  }
-}
+# module "eu_west_2" {
+#   source                             = "./region"
+#   count                              = contains(local.account.regions, "eu-west-2") ? 1 : 0
+#   network_cidr_block                 = "10.162.0.0/16"
+#   cloudwatch_log_group_kms_key_alias = aws_kms_alias.cloudwatch_alias_eu_west_2.name
+#   sns_kms_key_alias                  = aws_kms_alias.sns_alias_eu_west_2.name
+#   secrets_manager_kms_key_alias      = aws_kms_alias.secrets_manager_alias_eu_west_2.name
+#   providers = {
+#     aws.region     = aws.eu_west_2
+#     aws.management = aws.management_eu_west_2
+#     aws.global     = aws.global
+#   }
+# }
 
 moved {
   from = module.eu_west_1
