b3fa0d43f44adceaffe846ed16a3fe5c8f20ba78
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-06-08 15:04:01 +0100

    fix reference to region module

--- terraform/account/pagerduty.tf
+++ terraform/account/pagerduty.tf
@@ -12,14 +12,10 @@ resource "pagerduty_service_integration" "main" {
   vendor  = data.pagerduty_vendor.cloudwatch.id
 }
 
-# resource "aws_sns_topic_subscription" "main" {
-#   topic_arn              = module.eu_west_1.ecs_autoscaling_alarm_sns_topic.arn
-#   protocol               = "https"
-#   endpoint_auto_confirms = true
-#   endpoint               = "https://events.pagerduty.com/integration/${pagerduty_service_integration.main.integration_key}/enqueue"
-#   provider               = aws.eu_west_1
-# }
-
-output "ecs_autoscaling_alarm_sns_topic" {
-  value = module.eu_west_1.ecs_autoscaling_alarm_sns_topic
+resource "aws_sns_topic_subscription" "main" {
+  topic_arn              = module.eu_west_1[0].ecs_autoscaling_alarm_sns_topic.arn
+  protocol               = "https"
+  endpoint_auto_confirms = true
+  endpoint               = "https://events.pagerduty.com/integration/${pagerduty_service_integration.main.integration_key}/enqueue"
+  provider               = aws.eu_west_1
 }
