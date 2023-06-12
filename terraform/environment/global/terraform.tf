9eb8760169bd57e6311524e17376fc8b093444a8
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-06-12 20:25:25 +0100

    create intance of global module

--- terraform/environment/global/terraform.tf
+++ terraform/environment/global/terraform.tf
@@ -5,7 +5,7 @@ terraform {
     aws = {
       source = "hashicorp/aws"
       configuration_aliases = [
-        aws.region,
+        # aws.region,
         aws.global,
       ]
     }
--- terraform/environment/refactor.tf
+++ terraform/environment/refactor.tf
@@ -2,3 +2,13 @@ moved {
   from = aws_secretsmanager_secret.rum_monitor_application_id_eu_west_1
   to   = module.eu_west_1[0].aws_secretsmanager_secret.rum_monitor_application_id
 }
+
+moved {
+  from = aws_resourcegroups_group.environment-eu-west-1
+  to   = module.eu_west_1[0].aws_resourcegroups_group.environment
+}
+
+moved {
+  from = aws_resourcegroups_group.environment_global
+  to   = module.global.aws_resourcegroups_group.environment
+}
