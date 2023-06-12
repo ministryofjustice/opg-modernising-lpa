8d987b03e696c1f52e19877fdc608daf6afd6436
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-06-12 18:04:54 +0100

    new global module

---
+++ terraform/environment/global/terraform.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_version = ">= 1.2.2"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      configuration_aliases = [
+        aws.region,
+        aws.global,
+      ]
+    }
+    pagerduty = {
+      source  = "PagerDuty/pagerduty"
+      version = "~> 2.14.0"
+    }
+  }
+}
+
+data "aws_region" "current" {
+  provider = aws.region
+}
+
+data "aws_caller_identity" "current" {
+  provider = aws.region
+}
+
+data "aws_default_tags" "current" {
+  provider = aws.region
+}
+
+data "aws_caller_identity" "global" {
+  provider = aws.global
+}
+
+data "aws_region" "global" {
+  provider = aws.global
+}
