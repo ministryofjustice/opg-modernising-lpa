98c923b243c321619d980ccd444cd1f972eed01a
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-06-12 20:17:53 +0100

    use resource group name for insights

--- terraform/environment/region/cloudwatch_application_insights.tf
+++ terraform/environment/region/cloudwatch_application_insights.tf
@@ -1,5 +1,5 @@
 resource "aws_applicationinsights_application" "environment" {
-  resource_group_name = "${data.aws_default_tags.current.tags.environment-name}-environment-${data.aws_region.current.name}"
+  resource_group_name = aws_resourcegroups_group.environment.name
   auto_config_enabled = true
   cwe_monitor_enabled = true
   provider            = aws.region
