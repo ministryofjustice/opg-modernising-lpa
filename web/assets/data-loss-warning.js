f1fcec0c9caac24ce6dda465bccffeb3d7cb60c5
Alex Saunders <acsauk@gmail.com>
2023-06-20 17:55:02 +0100

    check module has 2 children that are anchor or button

--- web/assets/data-loss-warning.js
+++ web/assets/data-loss-warning.js
@@ -1,3 +1,15 @@
-export default function returnToTaskListAndSaveButtonsVisible() {
+export default function hasReturnToTaskListAndSaveButtons() {
+    const saveOrReturn = document.querySelector('[data-module="app-save-or-return"]')
+
+    if (saveOrReturn?.children.length !== 2) {
+        return false
+    }
+
+    for (let element of saveOrReturn.children) {
+        if (!['A', 'BUTTON'].includes(element.tagName)) {
+            return false
+        }
+    }
+
     return true
 }
--- web/assets/data-loss-warning.test.js
+++ web/assets/data-loss-warning.test.js
@@ -1,17 +1,45 @@
-import returnToTaskListAndSaveButtonsVisible from './data-loss-warning'
+import hasReturnToTaskListAndSaveButtons from './data-loss-warning'
 
 describe('data loss warning', () => {
-    it('knows when a return to task list and save and continue button are part of a form', () => {
-        document.body.innerHTML = `
-<form>
-    <div class="govuk-button-group" id="save-or-return">
-        <button class="govuk-button" data-module="govuk-button">Some button text</button>
-        <a href="/" class="govuk-button govuk-button--secondary">More button text</a>
-    </div>
-</form>`
-
-        expect(returnToTaskListAndSaveButtonsVisible()).toBeTruthy()
+    it.each([
+        {
+            body: `<form>
+                <div class="govuk-button-group" data-module="app-save-or-return">
+                    <button class="govuk-button" data-module="govuk-button">Some button text</button>
+                    <a href="/" class="govuk-button govuk-button--secondary">More button text</a>
+                </div>
+            </form>`,
+            expected: true,
+        },
+        {
+            body: `<form>
+                <div class="govuk-button-group" data-module="app-save-or-return">
+                    <button class="govuk-button" data-module="govuk-button">Some button text</button>
+                </div>
+            </form>`,
+            expected: false,
+        },
+        {
+            body: `<form>
+                <div class="govuk-button-group" data-module="not-app-save-or-return">
+                    <button class="govuk-button" data-module="govuk-button">Some button text</button>
+                    <a href="/" class="govuk-button govuk-button--secondary">More button text</a>
+                </div>
+            </form>`,
+            expected: false,
+        },
+        {
+            body: `<form>
+                <div class="govuk-button-group" data-module="app-save-or-return">
+                    <div class="govuk-button" data-module="govuk-button">Some button text</div>
+                    <p class="govuk-button govuk-button--secondary">More button text</p>
+                </div>
+            </form>`,
+            expected: false,
+        },
+    ])('knows when a save or return button group has two buttons with body %s', ({body, expected}) => {
+        document.body.innerHTML = body
+        expect(hasReturnToTaskListAndSaveButtons()).toBe(expected)
     })
-
 
 })
