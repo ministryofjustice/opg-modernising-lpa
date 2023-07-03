097b8410317a7301f4bf6d4706022896c3a2c6e1
Alex Saunders <acsauk@gmail.com>
2023-07-03 18:09:48 +0100

    use Health in dependency smoke test

--- app/internal/page/dependency_health_check.go
+++ app/internal/page/dependency_health_check.go
@@ -2,24 +2,19 @@ package page
 
 import (
 	"net/http"
-
-	"github.com/ministryofjustice/opg-modernising-lpa/app/internal/date"
-	"github.com/ministryofjustice/opg-modernising-lpa/app/internal/uid"
 )
 
 func DependencyHealthCheck(logger Logger, uidClient UidClient) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		_, err := uidClient.CreateCase(r.Context(), &uid.CreateCaseRequestBody{
-			Type: LpaTypePropertyFinance.String(),
-			Donor: uid.DonorDetails{
-				Name:     "Jane Smith",
-				Dob:      uid.ISODate{Time: date.New("2000", "1", "2").Time()},
-				Postcode: "B147ED",
-			},
-		})
+		resp, err := uidClient.Health(r.Context())
 
 		if err != nil {
 			logger.Print(err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			logger.Print("UID service not available")
 			w.WriteHeader(http.StatusInternalServerError)
 		}
 	}
