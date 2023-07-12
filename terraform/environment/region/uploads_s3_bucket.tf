039992e427241f31ac0116c98c512c051fba8781
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-07-12 14:09:03 +0100

    name bucket after function

--- terraform/environment/region/uploads_s3_bucket.tf
+++ terraform/environment/region/uploads_s3_bucket.tf
@@ -1,7 +1,7 @@
 module "uploads_s3_bucket" {
   source = "./modules/uploads_s3_bucket"
 
-  bucket_name   = "${data.aws_default_tags.current.tags.application}-${data.aws_default_tags.current.tags.environment-name}-${data.aws_region.current.name}"
+  bucket_name   = "uploads-${data.aws_default_tags.current.tags.application}-${data.aws_default_tags.current.tags.environment-name}-${data.aws_region.current.name}"
   force_destroy = data.aws_default_tags.current.tags.environment-name != "production" ? true : false
   providers = {
     aws.region = aws.region
