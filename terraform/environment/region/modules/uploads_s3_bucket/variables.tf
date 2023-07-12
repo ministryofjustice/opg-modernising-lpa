ecb73308df03a11592f1fb1a6cb575cc9fc17227
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-07-12 14:34:51 +0100

    fix var description

--- terraform/environment/region/modules/uploads_s3_bucket/variables.tf
+++ terraform/environment/region/modules/uploads_s3_bucket/variables.tf
@@ -3,6 +3,6 @@ variable "bucket_name" {
 }
 
 variable "force_destroy" {
-  description = " A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error. These objects are not recoverable."
+  description = "A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error. These objects are not recoverable."
   default     = false
 }
