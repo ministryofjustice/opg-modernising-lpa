f353a028c45f5b2ec6313b889010812a593a3c8c
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-07-12 11:44:27 +0100

    fix condition

--- terraform/environment/region/uploads_s3_bucket.tf
+++ terraform/environment/region/uploads_s3_bucket.tf
@@ -2,7 +2,7 @@ module "uploads_s3_bucket" {
   source = "./modules/uploads_s3_bucket"
 
   bucket_name   = "${data.aws_default_tags.current.tags.application}-${data.aws_default_tags.current.tags.environment-name}-${data.aws_region.current.name}"
-  force_destroy = data.aws_default_tags.current.tags.environment-name ? true : false
+  force_destroy = data.aws_default_tags.current.tags.environment-name == "production" ? true : false
   providers = {
     aws.region = aws.region
   }
