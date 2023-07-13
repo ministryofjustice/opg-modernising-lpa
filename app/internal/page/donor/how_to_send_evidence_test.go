1c75a123e64764fb13a679455b935c405cf179bb
Joshua Hawxwell <m@hawx.me>
2023-07-13 13:24:02 +0100

    MLPAB-1019 Add page on how to send evidence that goes to payment (#580)

---
+++ app/internal/page/donor/how_to_send_evidence_test.go
@@ -0,0 +1,72 @@
+package donor
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ministryofjustice/opg-modernising-lpa/app/internal/actor"
+	"github.com/ministryofjustice/opg-modernising-lpa/app/internal/page"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestGetHowToSendEvidence(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodGet, "/about-payment", nil)
+
+	template := newMockTemplate(t)
+	template.
+		On("Execute", w, &howToSendEvidenceData{App: testAppData}).
+		Return(nil)
+
+	err := HowToSendEvidence(template.Execute, nil)(testAppData, w, r, &page.Lpa{})
+	resp := w.Result()
+
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
+
+func TestGetHowToSendEvidenceWhenTemplateErrors(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodGet, "/about-payment", nil)
+
+	template := newMockTemplate(t)
+	template.
+		On("Execute", w, &howToSendEvidenceData{App: testAppData}).
+		Return(expectedError)
+
+	err := HowToSendEvidence(template.Execute, nil)(testAppData, w, r, &page.Lpa{})
+	resp := w.Result()
+
+	assert.Equal(t, expectedError, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
+
+func TestPostHowToSendEvidence(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodPost, "/about-payment", nil)
+
+	lpa := &page.Lpa{ID: "lpa-id", Donor: actor.Donor{Email: "a@b.com"}}
+
+	payer := newMockPayer(t)
+	payer.
+		On("Pay", testAppData, w, r, lpa).
+		Return(nil)
+
+	err := HowToSendEvidence(nil, payer)(testAppData, w, r, lpa)
+	assert.Nil(t, err)
+}
+
+func TestPostHowToSendEvidenceWhenPayerErrors(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodPost, "/about-payment", nil)
+
+	payer := newMockPayer(t)
+	payer.
+		On("Pay", testAppData, w, r, mock.Anything).
+		Return(expectedError)
+
+	err := HowToSendEvidence(nil, payer)(testAppData, w, r, &page.Lpa{})
+	assert.Equal(t, expectedError, err)
+}
--- app/internal/page/donor/register_test.go
+++ app/internal/page/donor/register_test.go
@@ -561,3 +561,127 @@ func TestMakeLpaHandleErrors(t *testing.T) {
 
 	mux.ServeHTTP(w, r)
 }
+
+func TestPayHelperPay(t *testing.T) {
+	testcases := map[string]struct {
+		nextURL  string
+		redirect string
+	}{
+		"real": {
+			nextURL:  "https://www.payments.service.gov.uk/path-from/response",
+			redirect: "https://www.payments.service.gov.uk/path-from/response",
+		},
+		"fake": {
+			nextURL:  "/lpa/lpa-id/something-else",
+			redirect: page.Paths.PaymentConfirmation.Format("lpa-id"),
+		},
+	}
+
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			r, _ := http.NewRequest(http.MethodPost, "/about-payment", nil)
+
+			sessionStore := newMockSessionStore(t)
+
+			session := sessions.NewSession(sessionStore, "pay")
+			session.Options = &sessions.Options{
+				Path:     "/",
+				MaxAge:   5400,
+				SameSite: http.SameSiteLaxMode,
+				HttpOnly: true,
+				Secure:   true,
+			}
+			session.Values = map[any]any{"payment": &sesh.PaymentSession{PaymentID: "a-fake-id"}}
+
+			sessionStore.
+				On("Save", r, w, session).
+				Return(nil)
+
+			payClient := newMockPayClient(t)
+			payClient.
+				On("CreatePayment", pay.CreatePaymentBody{
+					Amount:      8200,
+					Reference:   "123456789012",
+					Description: "Property and Finance LPA",
+					ReturnUrl:   "http://example.org/lpa/lpa-id/payment-confirmation",
+					Email:       "a@b.com",
+					Language:    "en",
+				}).
+				Return(pay.CreatePaymentResponse{
+					PaymentId: "a-fake-id",
+					Links: map[string]pay.Link{
+						"next_url": {
+							Href: tc.nextURL,
+						},
+					},
+				}, nil)
+
+			err := (&payHelper{
+				sessionStore: sessionStore,
+				payClient:    payClient,
+				appPublicURL: "http://example.org",
+				randomString: func(int) string { return "123456789012" },
+			}).Pay(testAppData, w, r, &page.Lpa{ID: "lpa-id", Donor: actor.Donor{Email: "a@b.com"}})
+			resp := w.Result()
+
+			assert.Nil(t, err)
+			assert.Equal(t, http.StatusFound, resp.StatusCode)
+			assert.Equal(t, tc.redirect, resp.Header.Get("Location"))
+		})
+	}
+}
+
+func TestPayHelperPayWhenCreatePaymentErrors(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodPost, "/about-payment", nil)
+
+	logger := newMockLogger(t)
+	logger.
+		On("Print", "Error creating payment: err")
+
+	payClient := newMockPayClient(t)
+	payClient.
+		On("CreatePayment", mock.Anything).
+		Return(pay.CreatePaymentResponse{}, expectedError)
+
+	err := (&payHelper{
+		logger:       logger,
+		payClient:    payClient,
+		appPublicURL: "http://example.org",
+		randomString: func(int) string { return "123456789012" },
+	}).Pay(testAppData, w, r, &page.Lpa{})
+
+	assert.Equal(t, expectedError, err)
+}
+
+func TestPayHelperPayWhenSessionErrors(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodPost, "/about-payment", nil)
+
+	sessionStore := newMockSessionStore(t)
+	sessionStore.
+		On("Save", r, w, mock.Anything).
+		Return(expectedError)
+
+	payClient := newMockPayClient(t)
+	payClient.
+		On("CreatePayment", mock.Anything).
+		Return(pay.CreatePaymentResponse{
+			PaymentId: "a-fake-id",
+			Links: map[string]pay.Link{
+				"next_url": {
+					Href: "http://somewhere",
+				},
+			},
+		}, nil)
+
+	err := (&payHelper{
+		sessionStore: sessionStore,
+		payClient:    payClient,
+		appPublicURL: "http://example.org",
+		randomString: func(int) string { return "123456789012" },
+	}).Pay(testAppData, w, r, &page.Lpa{})
+
+	assert.Equal(t, expectedError, err)
+}
