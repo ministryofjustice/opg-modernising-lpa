fcfe457d8802a0a645c44936d2e0755cd6f90f73
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-07-19 21:30:43 +0100

    output bucket

--- terraform/environment/region/modules/uploads_s3_bucket/outputs.tf
+++ terraform/environment/region/modules/uploads_s3_bucket/outputs.tf
@@ -1,13 +1,3 @@
-output "id" {
-  description = "The name of the bucket."
-  value       = aws_s3_bucket.bucket.id
-}
-
-output "arn" {
-  description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname."
-  value       = aws_s3_bucket.bucket.arn
-}
-
 output "bucket" {
   description = "S3 uploads bucket."
   value       = aws_s3_bucket.bucket
