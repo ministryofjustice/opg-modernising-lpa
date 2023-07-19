0cd3005010d0260b03d03ce984351f22f1ca70d4
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-07-19 22:25:11 +0100

    use arn instead of id

--- terraform/environment/region/modules/s3_antivirus/main.tf
+++ terraform/environment/region/modules/s3_antivirus/main.tf
@@ -5,7 +5,7 @@ data "aws_kms_alias" "cloudwatch_application_logs_encryption" {
 
 resource "aws_cloudwatch_log_group" "lambda" {
   name              = "/aws/lambda/s3-antivirus-${data.aws_default_tags.current.tags.environment-name}"
-  kms_key_id        = data.aws_kms_alias.cloudwatch_application_logs_encryption.target_key_id
+  kms_key_id        = data.aws_kms_alias.cloudwatch_application_logs_encryption.target_key_arn
   retention_in_days = 30
   provider          = aws.region
 }
