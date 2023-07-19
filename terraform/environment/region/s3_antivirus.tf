37f2125c3509000d898fcfd2c282e51a38060701
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-07-19 21:18:08 +0100

    implement module

---
+++ terraform/environment/region/s3_antivirus.tf
@@ -0,0 +1,41 @@
+data "aws_ecr_repository" "s3_antivirus" {
+  name     = "s3-antivirus"
+  provider = aws.management
+}
+
+data "aws_ecr_image" "s3_antivirus" {
+  repository_name = data.aws_ecr_repository.s3_antivirus.name
+  image_tag       = "latest"
+  provider        = aws.management
+}
+
+data "aws_s3_bucket" "antivirus_definitions" {
+  bucket   = "virus-definitions-${data.aws_default_tags.current.tags.application}-${data.aws_default_tags.current.tags.account-name}-${data.aws_region.current.name}"
+  provider = aws.region
+}
+
+module "s3_antivirus" {
+  source              = "./modules/s3_antivirus"
+  alarm_sns_topic_arn = data.aws_sns_topic.custom_cloudwatch_alarms.arn
+  aws_subnet_ids      = data.aws_subnet.application.*.id
+  data_store_bucket   = module.uploads_s3_bucket.id
+  definition_bucket   = data.aws_s3_bucket.antivirus_definitions
+  ecr_image_uri       = "${data.aws_ecr_repository.s3_antivirus.repository_url}@${data.aws_ecr_image.s3_antivirus.image_digest}"
+  enable_autoscan     = true
+  lambda_task_role    = var.iam_roles.s3_antivirus
+
+  environment_variables = {
+    ANTIVIRUS_DEFINITIONS_BUCKET = data.aws_s3_bucket.antivirus_definitions.id
+    ANTIVIRUS_TAG_KEY            = "virus-scan-status"
+    ANTIVIRUS_TAG_VALUE_PASS     = "ok"
+    ANTIVIRUS_TAG_VALUE_FAIL     = "infected"
+  }
+  providers = {
+    aws.region = aws.region
+  }
+}
+
+data "aws_sns_topic" "custom_cloudwatch_alarms" {
+  name     = "custom_cloudwatch_alarms"
+  provider = aws.region
+}
