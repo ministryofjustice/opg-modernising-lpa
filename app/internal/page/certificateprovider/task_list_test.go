c396c4af1d4bb0d9a0b46f4681d8918b360b8f19
Joshua Hawxwell <m@hawx.me>
2023-07-21 10:43:35 +0100

    MLPAB-1121 Add task list for certificate provider (#593)

---
+++ app/internal/page/certificateprovider/task_list_test.go
@@ -0,0 +1,165 @@
+package certificateprovider
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ministryofjustice/opg-modernising-lpa/app/internal/actor"
+	"github.com/ministryofjustice/opg-modernising-lpa/app/internal/page"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestGetTaskList(t *testing.T) {
+	testCases := map[string]struct {
+		lpa                 *page.Lpa
+		certificateProvider *actor.CertificateProviderProvidedDetails
+		appData             page.AppData
+		expected            func([]taskListItem) []taskListItem
+	}{
+		"empty": {
+			lpa:                 &page.Lpa{ID: "lpa-id"},
+			certificateProvider: &actor.CertificateProviderProvidedDetails{},
+			appData:             testAppData,
+			expected: func(items []taskListItem) []taskListItem {
+				items[2].Disabled = true
+
+				return items
+			},
+		},
+		"required tasks completed": {
+			lpa: &page.Lpa{
+				ID: "lpa-id",
+			},
+			certificateProvider: &actor.CertificateProviderProvidedDetails{
+				Tasks: actor.CertificateProviderTasks{
+					ConfirmYourIdentity: actor.TaskCompleted,
+					ReadTheLpa:          actor.TaskCompleted,
+				},
+			},
+			appData: testAppData,
+			expected: func(items []taskListItem) []taskListItem {
+				items[0].State = actor.TaskCompleted
+				items[1].State = actor.TaskCompleted
+
+				return items
+			},
+		},
+		"all completed": {
+			lpa: &page.Lpa{
+				ID: "lpa-id",
+			},
+			certificateProvider: &actor.CertificateProviderProvidedDetails{
+				Tasks: actor.CertificateProviderTasks{
+					ConfirmYourIdentity:   actor.TaskCompleted,
+					ReadTheLpa:            actor.TaskCompleted,
+					ProvideTheCertificate: actor.TaskCompleted,
+				},
+			},
+			appData: testAppData,
+			expected: func(items []taskListItem) []taskListItem {
+				items[0].State = actor.TaskCompleted
+				items[1].State = actor.TaskCompleted
+				items[2].State = actor.TaskCompleted
+
+				return items
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			r, _ := http.NewRequest(http.MethodGet, "/", nil)
+
+			donorStore := newMockDonorStore(t)
+			donorStore.
+				On("GetAny", r.Context()).
+				Return(tc.lpa, nil)
+
+			certificateProviderStore := newMockCertificateProviderStore(t)
+			certificateProviderStore.
+				On("Get", r.Context()).
+				Return(tc.certificateProvider, nil)
+
+			template := newMockTemplate(t)
+			template.
+				On("Execute", w, &taskListData{
+					App: tc.appData,
+					Lpa: tc.lpa,
+					Items: tc.expected([]taskListItem{
+						{Name: "confirmYourIdentity", Path: page.Paths.CertificateProvider.EnterDateOfBirth.Format("lpa-id")},
+						{Name: "readTheLpa", Path: page.Paths.CertificateProvider.ReadTheLpa.Format("lpa-id")},
+						{Name: "provideTheCertificateForThisLpa", Path: page.Paths.CertificateProvider.ProvideCertificate.Format("lpa-id")},
+					}),
+				}).
+				Return(nil)
+
+			err := TaskList(template.Execute, donorStore, certificateProviderStore)(tc.appData, w, r)
+			resp := w.Result()
+
+			assert.Nil(t, err)
+			assert.Equal(t, http.StatusOK, resp.StatusCode)
+		})
+	}
+}
+
+func TestGetTaskListWhenDonorStoreErrors(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodGet, "/", nil)
+
+	donorStore := newMockDonorStore(t)
+	donorStore.
+		On("GetAny", r.Context()).
+		Return(&page.Lpa{}, expectedError)
+
+	err := TaskList(nil, donorStore, nil)(testAppData, w, r)
+
+	assert.Equal(t, expectedError, err)
+}
+
+func TestGetTaskListWhenCertificateProviderStoreErrors(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodGet, "/", nil)
+
+	donorStore := newMockDonorStore(t)
+	donorStore.
+		On("GetAny", r.Context()).
+		Return(&page.Lpa{ID: "lpa-id"}, nil)
+
+	certificateProviderStore := newMockCertificateProviderStore(t)
+	certificateProviderStore.
+		On("Get", mock.Anything).
+		Return(nil, expectedError)
+
+	err := TaskList(nil, donorStore, certificateProviderStore)(testAppData, w, r)
+
+	assert.Equal(t, expectedError, err)
+}
+
+func TestGetTaskListWhenTemplateErrors(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodGet, "/", nil)
+
+	donorStore := newMockDonorStore(t)
+	donorStore.
+		On("GetAny", r.Context()).
+		Return(&page.Lpa{ID: "lpa-id"}, nil)
+
+	certificateProviderStore := newMockCertificateProviderStore(t)
+	certificateProviderStore.
+		On("Get", r.Context()).
+		Return(&actor.CertificateProviderProvidedDetails{}, nil)
+
+	template := newMockTemplate(t)
+	template.
+		On("Execute", w, mock.Anything).
+		Return(expectedError)
+
+	err := TaskList(template.Execute, donorStore, certificateProviderStore)(testAppData, w, r)
+	resp := w.Result()
+
+	assert.Equal(t, expectedError, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
---
+++ cypress/e2e/certificate-provider/task-list.cy.js
@@ -0,0 +1,13 @@
+describe('Task list', () => {
+    beforeEach(() => {
+        cy.visit('/testing-start?redirect=/task-list&lpa.complete=1&certificateProviderProvided=1&loginAs=certificate-provider');
+    });
+
+    it('shows tasks', () => {
+        cy.checkA11yApp();
+
+        cy.contains('li', 'Confirm your identity').should('contain', 'Not started');
+        cy.contains('li', 'Read the LPA').should('contain', 'Not started');
+        cy.contains('li', 'Provide the certificate for this LPA').should('contain', 'Cannot start yet');
+    });
+});
