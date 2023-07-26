fd9ca0ba0a8959a03a9780ad498fe9c087211b18
Alex Saunders <acsauk@gmail.com>
2023-07-26 12:24:55 +0100

    MLPAB-1149: Update /check-your-lpa and /lpa-details-saved variable template logic  (#608)

---
+++ app/internal/page/donor/lpa_details_saved_test.go
@@ -0,0 +1,56 @@
+package donor
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ministryofjustice/opg-modernising-lpa/app/internal/page"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestGetLpaDetailsSaved(t *testing.T) {
+	testCases := map[string]bool{
+		"/?firstCheck=1": true,
+		"/":              false,
+	}
+
+	for url, expectedIsFirstCheck := range testCases {
+		t.Run(url, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			r, _ := http.NewRequest(http.MethodGet, url, nil)
+
+			template := newMockTemplate(t)
+			template.
+				On("Execute", w, LpaDetailsSavedData{
+					App:          testAppData,
+					IsFirstCheck: expectedIsFirstCheck,
+					Lpa:          &page.Lpa{},
+				}).
+				Return(nil)
+
+			err := LpaDetailsSaved(template.Execute)(testAppData, w, r, &page.Lpa{})
+			resp := w.Result()
+
+			assert.Nil(t, err)
+			assert.Equal(t, http.StatusOK, resp.StatusCode)
+		})
+	}
+}
+
+func TestGetLpaDetailsSavedOnTemplateError(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodGet, "/", nil)
+
+	template := newMockTemplate(t)
+	template.
+		On("Execute", mock.Anything, mock.Anything).
+		Return(expectedError)
+
+	err := LpaDetailsSaved(template.Execute)(testAppData, w, r, &page.Lpa{})
+	resp := w.Result()
+
+	assert.Equal(t, expectedError, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
