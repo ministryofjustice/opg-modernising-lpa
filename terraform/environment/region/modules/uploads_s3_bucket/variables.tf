140881aa2a000905b7e9b8f453d2c61bd6ea3802
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-08-04 17:10:33 +0100

    fix up bools

--- terraform/environment/region/modules/uploads_s3_bucket/variables.tf
+++ terraform/environment/region/modules/uploads_s3_bucket/variables.tf
@@ -6,6 +6,7 @@ variable "bucket_name" {
 variable "force_destroy" {
   description = "A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error. These objects are not recoverable."
   default     = false
+  type        = bool
 }
 
 variable "s3_replication_target_bucket_arn" {
