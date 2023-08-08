7bdb8478a363423eea6d6079b562ea0eda045f4f
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-08-08 11:55:52 +0100

    allow batch operations to assume replication role

--- terraform/account/s3_replication_role.tf
+++ terraform/account/s3_replication_role.tf
@@ -1,10 +1,13 @@
-data "aws_iam_policy_document" "assume_role" {
+data "aws_iam_policy_document" "assume_replication_role" {
   statement {
     effect = "Allow"
 
     principals {
-      type        = "Service"
-      identifiers = ["s3.amazonaws.com"]
+      type = "Service"
+      identifiers = [
+        "s3.amazonaws.com",
+        "batchoperations.s3.amazonaws.com"
+      ]
     }
 
     actions = ["sts:AssumeRole"]
@@ -14,6 +17,6 @@ data "aws_iam_policy_document" "assume_role" {
 
 resource "aws_iam_role" "replication" {
   name               = "reduced-fees-uploads-replication"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  assume_role_policy = data.aws_iam_policy_document.assume_replication_role.json
   provider           = aws.global
 }
