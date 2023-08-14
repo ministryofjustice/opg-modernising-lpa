dcd94b318e1220f57302ab1c17aabfd281bf88e6
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-08-14 11:21:53 +0100

    stringlist

--- terraform/account/parameters.tf
+++ terraform/account/parameters.tf
@@ -1,7 +1,7 @@
 resource "aws_ssm_parameter" "additional_allowed_ingress" {
   name  = "/modernising-lpa/additional-allowed-ingress-cidrs/${data.aws_default_tags.global.tags.account-name}"
   type  = "StringList"
-  value = "[default]"
+  value = "default"
   lifecycle {
     ignore_changes = [value]
   }
