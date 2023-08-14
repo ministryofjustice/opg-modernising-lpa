73d08399b16f625939fef9f40932ccf99b9a5e01
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-08-14 17:32:43 +0100

    add a scheduler ro invoke the lambda function

--- terraform/environment/region/modules/uploads_s3_bucket/variables.tf
+++ terraform/environment/region/modules/uploads_s3_bucket/variables.tf
@@ -11,23 +11,25 @@ variable "force_destroy" {
 
 variable "s3_replication" {
   type = object({
-    enabled                        = bool
-    destination_bucket_arn         = string
-    destination_encryption_key_arn = string
-    destination_account_id         = string
-    lambda_function_image_ecr_arn  = string
-    lambda_function_image_ecr_url  = string
-    lambda_function_image_tag      = string
+    enabled                                   = bool
+    destination_bucket_arn                    = string
+    destination_encryption_key_arn            = string
+    destination_account_id                    = string
+    lambda_function_image_ecr_arn             = string
+    lambda_function_image_ecr_url             = string
+    lambda_function_image_tag                 = string
+    enable_s3_batch_job_replication_scheduler = bool
   })
   description = <<EOT
     s3_replication = {
-      enabled                        = "Enable S3 object replication"
-      destination_bucket_arn         = "ARN of the destination bucket"
-      destination_encryption_key_arn = "ARN of the destination encryption key"
-      destination_account_id         = "Account ID of the destination bucket"
-      lambda_function_image_ecr_arn  = "ARN of the lambda function to be invoked on a schedule to create replication jobs"
-      lambda_function_image_ecr_url  = "URL of the lambda function to be invoked on a schedule to create replication jobs"
-      lambda_function_image_tag      = "Tag of the lambda function to be invoked on a schedule to create replication jobs"
+      enabled                                   = "Enable S3 object replication"
+      destination_bucket_arn                    = "ARN of the destination bucket"
+      destination_encryption_key_arn            = "ARN of the destination encryption key"
+      destination_account_id                    = "Account ID of the destination bucket"
+      lambda_function_image_ecr_arn             = "ARN of the lambda function to be invoked on a schedule to create replication jobs"
+      lambda_function_image_ecr_url             = "URL of the lambda function to be invoked on a schedule to create replication jobs"
+      lambda_function_image_tag                 = "Tag of the lambda function to be invoked on a schedule to create replication jobs"
+      enable_s3_batch_job_replication_scheduler = "Enable scheduler to create replication jobs"
     }
     EOT
 }
