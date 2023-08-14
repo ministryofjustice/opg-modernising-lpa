78d24085347b628ff63869cd5be6289081d6b9ee
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-08-14 11:34:16 +0100

    MLPAB-1303 - create a paramter to hold additional CIDR blocks for custom access (#638)
    * create a paramter to hold additional CIDR blocks for custom access
    
    * stringlist

---
+++ terraform/account/parameters.tf
@@ -0,0 +1,9 @@
+resource "aws_ssm_parameter" "additional_allowed_ingress" {
+  name  = "/modernising-lpa/additional-allowed-ingress-cidrs/${data.aws_default_tags.global.tags.account-name}"
+  type  = "StringList"
+  value = "default"
+  lifecycle {
+    ignore_changes = [value]
+  }
+  provider = aws.management_global
+}
