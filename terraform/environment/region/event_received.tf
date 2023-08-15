24a8cc8f7595bfbc20c30299d65417508531484c
Joshua Hawxwell <m@hawx.me>
2023-08-15 08:01:06 +0100

    Add lambda to receive events and handle 'evidence-received'

---
+++ terraform/environment/region/event_received.tf
@@ -0,0 +1,16 @@
+data "aws_ecr_repository" "event_received" {
+  name     = "modernising-lpa/event-received"
+  provider = aws.management
+}
+
+module "event_received" {
+  source                        = "./modules/event_received"
+  lambda_function_image_ecr_arn = data.aws_ecr_repository.event_received.arn
+  lambda_function_image_ecr_url = data.aws_ecr_repository.event_received.repository_url
+  lambda_function_image_tag     = var.app_service_container_version
+  lpas_table_name               = var.lpas_table.name
+
+  providers = {
+    aws.region = aws.region
+  }
+}
---
+++ terraform/environment/region/modules/event_received/lambda.tf
@@ -0,0 +1,66 @@
+module "event_received" {
+  source      = "../lambda"
+  lambda_name = "event-received"
+  description = "Function to react when an event is recieved"
+  environment_variables = {
+    ENVIRONMENT = data.aws_default_tags.current.tags.environment-name
+    LPAS_TABLE  = var.lpas_table_name
+  }
+  image_uri   = "${var.lambda_function_image_ecr_url}:${var.lambda_function_image_tag}"
+  ecr_arn     = var.lambda_function_image_ecr_arn
+  environment = data.aws_default_tags.current.tags.environment-name
+  kms_key     = data.aws_kms_alias.cloudwatch_application_logs_encryption.target_key_arn
+  timeout     = 300
+  memory      = 1024
+  providers = {
+    aws.region = aws.region
+  }
+}
+
+resource "aws_iam_role_policy" "event_received" {
+  name     = "event_received-${data.aws_default_tags.current.tags.environment-name}"
+  role     = module.event_received.lambda_role.id
+  policy   = data.aws_iam_policy_document.event_received.json
+  provider = aws.region
+}
+
+data "aws_kms_alias" "dynamodb_encryption_key" {
+  name     = "alias/${data.aws_default_tags.current.tags.application}_dynamodb_encryption"
+  provider = aws.region
+}
+
+locals {
+  policy_region_prefix = lower(replace(data.aws_region.current.name, "-", ""))
+}
+
+data "aws_iam_policy_document" "event_received" {
+  statement {
+    sid    = "${local.policy_region_prefix}DynamoDBEncryptionAccess"
+    effect = "Allow"
+
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:GenerateDataKey",
+    ]
+
+    resources = [
+      data.aws_kms_alias.dynamodb_encryption_key.target_key_arn,
+    ]
+  }
+
+  statement {
+    sid = "${local.policy_region_prefix}Allow"
+
+    actions = [
+      "dynamodb:PutItem",
+    ]
+
+    resources = [
+      var.lpas_table.arn,
+      "${var.lpas_table.arn}/index/*",
+    ]
+  }
+
+  provider = aws.region
+}
