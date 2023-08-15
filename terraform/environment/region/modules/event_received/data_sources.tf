fb4af0e569d50740a3bdbef210f07cc966807b26
Joshua Hawxwell <m@hawx.me>
2023-08-15 08:01:06 +0100

    Add lambda to receive events and handle 'evidence-received'

---
+++ terraform/environment/region/modules/event_received/data_sources.tf
@@ -0,0 +1,12 @@
+data "aws_default_tags" "current" {
+  provider = aws.region
+}
+
+data "aws_region" "current" {
+  provider = aws.region
+}
+
+data "aws_kms_alias" "cloudwatch_application_logs_encryption" {
+  name     = "alias/${data.aws_default_tags.current.tags.application}_cloudwatch_application_logs_encryption"
+  provider = aws.region
+}
