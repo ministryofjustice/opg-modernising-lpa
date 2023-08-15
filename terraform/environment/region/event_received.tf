3816753ae317a2f3da6d9524bea67c1c48b2aa18
Joshua Hawxwell <m@hawx.me>
2023-08-15 08:01:06 +0100

    Add lambda to receive events and handle 'evidence-received'

---
+++ terraform/environment/region/event_received.tf
@@ -0,0 +1,19 @@
+data "aws_ecr_repository" "event_received" {
+  name     = "modernising-lpa/event-received"
+  provider = aws.management
+}
+
+module "event_received" {
+  source                        = "./modules/event_received"
+  lambda_function_image_ecr_arn = data.aws_ecr_repository.event_received.arn
+  lambda_function_image_ecr_url = data.aws_ecr_repository.event_received.repository_url
+  lambda_function_image_tag     = var.app_service_container_version
+  lpas_table = {
+    arn  = var.lpas_table.arn
+    name = var.lpas_table.name
+  }
+
+  providers = {
+    aws.region = aws.region
+  }
+}
