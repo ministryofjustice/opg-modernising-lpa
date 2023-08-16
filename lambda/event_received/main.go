09abcefca185f0bcb97a127d75f9c13c82d404c8
Joshua Hawxwell <m@hawx.me>
2023-08-16 09:15:21 +0100

    Show when evidence has been received in the task list

--- lambda/event_received/main.go
+++ lambda/event_received/main.go
@@ -12,6 +12,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
 func Handler(ctx context.Context, event events.CloudWatchEvent) error {
@@ -22,6 +23,8 @@ func Handler(ctx context.Context, event events.CloudWatchEvent) error {
 		return fmt.Errorf("failed to load default config: %w", err)
 	}
 
+	db := dynamodb.NewFromConfig(cfg)
+
 	switch event.DetailType {
 	case "evidence-received":
 		var v struct {
@@ -31,15 +34,20 @@ func Handler(ctx context.Context, event events.CloudWatchEvent) error {
 			return fmt.Errorf("failed to unmarshal 'evidence-received' detail: %w", err)
 		}
 
+		id, err := resolveUID(ctx, db, tableName, v.UID)
+		if err != nil {
+			return fmt.Errorf("failed to resolve uid for 'evidence-received': %w", err)
+		}
+
 		item, err := attributevalue.MarshalMap(map[string]any{
-			"PK": "LPA#" + v.UID,
+			"PK": id,
 			"SK": "#EVIDENCE_RECEIVED",
 		})
 		if err != nil {
 			return fmt.Errorf("failed to marshal item in response to 'evidence-received': %w", err)
 		}
 
-		_, err = dynamodb.NewFromConfig(cfg).PutItem(ctx, &dynamodb.PutItemInput{
+		_, err = db.PutItem(ctx, &dynamodb.PutItemInput{
 			TableName: aws.String(tableName),
 			Item:      item,
 		})
@@ -48,6 +56,34 @@ func Handler(ctx context.Context, event events.CloudWatchEvent) error {
 	default:
 		return fmt.Errorf("unknown event received: %s", event.DetailType)
 	}
+}
+
+func resolveUID(ctx context.Context, db *dynamodb.Client, tableName, uid string) (string, error) {
+	skey, err := attributevalue.Marshal(uid)
+	if err != nil {
+		return "", fmt.Errorf("failed to marshal UID: %w", err)
+	}
+
+	response, err := db.Query(ctx, &dynamodb.QueryInput{
+		TableName:                 aws.String(tableName),
+		IndexName:                 aws.String("UidIndex"),
+		ExpressionAttributeNames:  map[string]string{"#SK": "SK"},
+		ExpressionAttributeValues: map[string]types.AttributeValue{":SK": skey},
+		KeyConditionExpression:    aws.String("#SK = :SK"),
+	})
+	if err != nil {
+		return "", fmt.Errorf("failed to query UID: %w", err)
+	}
+	if len(response.Items) != 1 {
+		return "", fmt.Errorf("expected to resolve UID but got %d items", len(response.Items))
+	}
+
+	var v struct{ PK, SK string }
+	if err := attributevalue.UnmarshalMap(response.Items[0], &v); err != nil {
+		return "", fmt.Errorf("failed to unmarshal UID response: %w", err)
+	}
+
+	return v.SK, nil
 }
 
 func main() {
