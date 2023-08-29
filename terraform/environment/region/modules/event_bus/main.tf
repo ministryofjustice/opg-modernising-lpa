75bb3a6ad94b008fdb6c2544eeb4f3ea5500fec0
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-08-29 15:53:23 +0100

    don't name except env

--- terraform/environment/region/modules/event_bus/main.tf
+++ terraform/environment/region/modules/event_bus/main.tf
@@ -1,12 +1,12 @@
 # Event bus for reduced fees
 
 resource "aws_cloudwatch_event_bus" "main" {
-  name     = "${data.aws_default_tags.current.tags.environment-name}-reduced-fees"
+  name     = data.aws_default_tags.current.tags.environment-name
   provider = aws.region
 }
 
 resource "aws_cloudwatch_event_archive" "main" {
-  name             = "${data.aws_default_tags.current.tags.environment-name}-reduced-fees"
+  name             = data.aws_default_tags.current.tags.environment-name
   event_source_arn = aws_cloudwatch_event_bus.main.arn
   provider         = aws.region
 }
