fecb2ec726e94a78d1f649b18b3d6309fe45f10c
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-08-29 15:23:23 +0100

    rename event bus resources to main

--- terraform/environment/refactor.tf
+++ terraform/environment/refactor.tf
@@ -55,5 +55,15 @@ moved {
 
 moved {
   from = module.reduced_fees[0]
-  to   = module.eu_west_1[0].module.event_bus
+  to   = module.eu_west_1[0].module.events
+}
+
+moved {
+  from = module.eu_west_1[0].module.events.aws_cloudwatch_event_bus.reduced_fees
+  to   = module.eu_west_1[0].module.events.aws_cloudwatch_event_bus.main
+}
+
+moved {
+  from = module.eu_west_1[0].module.events.aws_cloudwatch_event_archive.reduced_fees
+  to   = module.eu_west_1[0].module.events.aws_cloudwatch_event_archive.main
 }
