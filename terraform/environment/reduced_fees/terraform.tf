11143a3a36c14a86c0da557808e67296dca4f457
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-08-29 11:59:31 +0100

    remove unused table and other unused resources

--- terraform/environment/reduced_fees/terraform.tf
+++ terraform/environment/reduced_fees/terraform.tf
@@ -1,12 +1,9 @@
 terraform {
-  required_version = ">= 1.5.2"
-
   required_providers {
     aws = {
       source = "hashicorp/aws"
       configuration_aliases = [
         aws.region,
-        aws.global,
       ]
     }
   }
--- terraform/environment/reduced_fees/variables.tf
+++ terraform/environment/reduced_fees/variables.tf
@@ -1,7 +1,3 @@
 variable "target_event_bus_arn" {
   type = string
 }
-
-# variable "dynamodb_encryption_key_arn" {
-#   type = string
-# }
