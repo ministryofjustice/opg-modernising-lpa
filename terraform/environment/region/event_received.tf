c0d33310f172280976b6657ad80474ce0543346d
Alex Saunders <acsauk@gmail.com>
2023-08-30 10:54:24 +0100

    fix event_received

--- terraform/environment/region/event_received.tf
+++ terraform/environment/region/event_received.tf
@@ -8,11 +8,13 @@ module "event_received" {
   lambda_function_image_ecr_arn = data.aws_ecr_repository.event_received.arn
   lambda_function_image_ecr_url = data.aws_ecr_repository.event_received.repository_url
   lambda_function_image_tag     = var.app_service_container_version
+  event_bus_name                = module.event_bus.event_bus.name
+  app_public_url                = aws_route53_record.app.fqdn
+
   lpas_table = {
     arn  = var.lpas_table.arn
     name = var.lpas_table.name
   }
-  event_bus_name = var.reduced_fees.event_bus.name
 
   providers = {
     aws.region = aws.region
