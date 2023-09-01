b26a0050e97fe80b4e702536d41247cd2fe0b642
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-09-01 14:56:46 +0100

    create a dns firewall in the account (#681)

---
+++ terraform/account/region/modules/dns_firewall/data_sources.tf
@@ -0,0 +1,8 @@
+data "aws_kms_alias" "cloudwatch_application_logs_encryption" {
+  name     = var.cloudwatch_log_group_kms_key_alias
+  provider = aws.region
+}
+
+data "aws_region" "current" {
+  provider = aws.region
+}
---
+++ terraform/account/region/modules/dns_firewall/variables.tf
@@ -0,0 +1,7 @@
+variable "vpc_id" {
+  type = string
+}
+
+variable "cloudwatch_log_group_kms_key_alias" {
+  type = string
+}
