3332f9cf628e6eef4964c473fcba2fde3357eb6b
Joshua Hawxwell <m@hawx.me>
2023-09-01 12:50:41 +0100

    Rename our cmds and group docker related files

--- cmd/mock-os-api/main.go
+++ cmd/mock-os-api/main.go
@@ -18,16 +18,14 @@ func main() {
 
 		switch postcode {
 		case "INVALID":
-			postcodeJson, _ = os.ReadFile("testdata/invalid-postcode-error.json")
-			w.Write(postcodeJson)
+			postcodeJson, _ = os.ReadFile("data/invalid-postcode-error.json")
 		case "NE234EE":
-			postcodeJson, _ = os.ReadFile("testdata/no-addresses-found.json")
-			w.Write(postcodeJson)
+			postcodeJson, _ = os.ReadFile("data/no-addresses-found.json")
 		default:
-			postcodeJson, _ = os.ReadFile("testdata/multiple-addresses.json")
-			w.Write(postcodeJson)
+			postcodeJson, _ = os.ReadFile("data/multiple-addresses.json")
 		}
 
+		w.Write(postcodeJson)
 		// to aid debugging e2e test failures
 		log.Println("OS mock response:", string(postcodeJson))
 	})
