71e1a69997d2ddcf1f12f8cde57efb31348d8085
Joshua Hawxwell <m@hawx.me>
2023-09-28 13:51:27 +0100

    Add card design for new LPA
    And split out the dashboard fixtures

---
+++ internal/page/fixtures/fixtures.go
@@ -0,0 +1,119 @@
+package fixtures
+
+import (
+	"context"
+
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/actor"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/date"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/form"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/notify"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/page"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/place"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/validation"
+)
+
+type ShareCodeSender interface {
+	SendCertificateProvider(ctx context.Context, template notify.Template, appData page.AppData, identity bool, lpa *page.Lpa) error
+	SendAttorneys(ctx context.Context, appData page.AppData, lpa *page.Lpa) error
+	UseTestCode()
+}
+
+const (
+	testEmail  = "simulate-delivered@notifications.service.gov.uk"
+	testMobile = "07700900000"
+)
+
+type fixturesData struct {
+	App    page.AppData
+	Errors validation.List
+}
+
+type Name struct {
+	Firstnames, Lastname string
+}
+
+var (
+	attorneyNames = []Name{
+		{Firstnames: "Jessie", Lastname: "Jones"},
+		{Firstnames: "Robin", Lastname: "Redcar"},
+		{Firstnames: "Leslie", Lastname: "Lewis"},
+		{Firstnames: "Ashley", Lastname: "Alwinton"},
+		{Firstnames: "Frankie", Lastname: "Fernandes"},
+	}
+	replacementAttorneyNames = []Name{
+		{Firstnames: "Blake", Lastname: "Buckley"},
+		{Firstnames: "Taylor", Lastname: "Thompson"},
+		{Firstnames: "Marley", Lastname: "Morris"},
+		{Firstnames: "Alex", Lastname: "Abbott"},
+		{Firstnames: "Billie", Lastname: "Blair"},
+	}
+)
+
+func makeAttorney(name Name) actor.Attorney {
+	return actor.Attorney{
+		ID:          name.Firstnames + name.Lastname,
+		FirstNames:  name.Firstnames,
+		LastName:    name.Lastname,
+		Email:       testEmail,
+		DateOfBirth: date.New("2000", "1", "2"),
+		Address: place.Address{
+			Line1:      "2 RICHMOND PLACE",
+			Line2:      "KINGS HEATH",
+			Line3:      "WEST MIDLANDS",
+			TownOrCity: "BIRMINGHAM",
+			Postcode:   "B14 7ED",
+		},
+	}
+}
+
+func makeTrustCorporation(name string) actor.TrustCorporation {
+	return actor.TrustCorporation{
+		Name:          name,
+		CompanyNumber: "555555555",
+		Email:         testEmail,
+		Address: place.Address{
+			Line1:      "2 RICHMOND PLACE",
+			Line2:      "KINGS HEATH",
+			Line3:      "WEST MIDLANDS",
+			TownOrCity: "BIRMINGHAM",
+			Postcode:   "B14 7ED",
+		},
+	}
+}
+
+func makeDonor(firstNames, lastName string) actor.Donor {
+	return actor.Donor{
+		FirstNames: firstNames,
+		LastName:   lastName,
+		Address: place.Address{
+			Line1:      "1 RICHMOND PLACE",
+			Line2:      "KINGS HEATH",
+			Line3:      "WEST MIDLANDS",
+			TownOrCity: "BIRMINGHAM",
+			Postcode:   "B14 7ED",
+		},
+		Email:         testEmail,
+		DateOfBirth:   date.New("2000", "1", "2"),
+		ThinksCanSign: actor.Yes,
+		CanSign:       form.Yes,
+	}
+}
+
+func makeCertificateProvider() actor.CertificateProvider {
+	return actor.CertificateProvider{
+		FirstNames:         "Charlie",
+		LastName:           "Cooper",
+		Email:              testEmail,
+		Mobile:             testMobile,
+		Relationship:       actor.Personally,
+		RelationshipLength: "gte-2-years",
+		CarryOutBy:         actor.Online,
+		Address: place.Address{
+			Line1:      "5 RICHMOND PLACE",
+			Line2:      "KINGS HEATH",
+			Line3:      "WEST MIDLANDS",
+			TownOrCity: "BIRMINGHAM",
+			Postcode:   "B14 7ED",
+		},
+	}
+}
