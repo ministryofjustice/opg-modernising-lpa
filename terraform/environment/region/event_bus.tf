0406d943797242cf88f349fab1f78b8ff9ef2e5b
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2023-10-04 15:34:17 +0100

    Allow Sirius accounts to send events
    Add permissions to event bus to allow specified accounts to send events to the bus.
    
    For VEGA-2090 #minor

--- terraform/environment/region/event_bus.tf
+++ terraform/environment/region/event_bus.tf
@@ -2,6 +2,7 @@ module "event_bus" {
   source               = "./modules/event_bus"
   target_event_bus_arn = var.target_event_bus_arn
   iam_role             = var.iam_roles.cross_account_put
+  receive_account_id   = var.receive_account_id
   providers = {
     aws.region = aws.region
   }
--- terraform/environment/region/variables.tf
+++ terraform/environment/region/variables.tf
@@ -90,3 +90,9 @@ variable "target_event_bus_arn" {
   type        = string
   description = "ARN of the event bus to forward events to"
 }
+
+variable "receive_account_id" {
+  type        = string
+  description = "ID of account to receive messages from"
+  default     = ""
+}
