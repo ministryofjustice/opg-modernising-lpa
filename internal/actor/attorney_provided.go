dded91a077877a3d49f961cd6f29358daa60f255
Joshua Hawxwell <m@hawx.me>
2023-10-10 15:06:08 +0100

    Add attorney progress page

--- internal/actor/attorney_provided.go
+++ internal/actor/attorney_provided.go
@@ -6,7 +6,8 @@ import (
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/form"
 )
 
-// AttorneyProvidedDetails contains details about an attorney or replacement attorney, provided by the attorney or replacement attorney
+// AttorneyProvidedDetails contains details about an attorney or replacement
+// attorney, provided by the attorney or replacement attorney
 type AttorneyProvidedDetails struct {
 	PK, SK string
 	// The identifier of the attorney or replacement attorney being edited
@@ -15,39 +16,49 @@ type AttorneyProvidedDetails struct {
 	LpaID string
 	// Tracking when AttorneyProvidedDetails is updated
 	UpdatedAt time.Time
-	// IsReplacement is true when the details relate to an attorney appointed as a replacement
+	// IsReplacement is true when the details relate to an attorney appointed as a
+	// replacement
 	IsReplacement bool
 	// IsTrustCorporation is true when the details relate to a trust corporation
 	IsTrustCorporation bool
 	// Mobile number of the attorney or replacement attorney
 	Mobile string
-	// Confirming the attorney or replacement attorney agrees to responsibilities and confirms the tick box is a legal signature
+	// LpaSignedAt is when the Lpa was signed at the time of the attorney
+	// confirming. This allows us to verify that the Lpa hasn't been resigned
+	// since the attorney confirmed.
+	LpaSignedAt time.Time
+	// Confirming the attorney or replacement attorney agrees to responsibilities
+	// and confirms the tick box is a legal signature
 	Confirmed time.Time
 	// WouldLikeSecondSignatory captures whether two signatories will be used for a trust corporation
 	WouldLikeSecondSignatory form.YesNo
-	// AuthorisedSignatories captures the details of the person who signed on behalf of a trust corporation, if one is acting as an attorney
+	// AuthorisedSignatories captures the details of the person who signed on
+	// behalf of a trust corporation, if one is acting as an attorney
 	AuthorisedSignatories [2]TrustCorporationSignatory
 	// Used to show attorney task list
 	Tasks AttorneyTasks
 }
 
-func (d AttorneyProvidedDetails) Signed(after time.Time) bool {
-	if after.IsZero() {
+// Signed checks whether the attorney has confirmed and if that confirmation is
+// still valid by checking that it was made for the donor's current signature.
+func (d AttorneyProvidedDetails) Signed(lpaSignedAt time.Time) bool {
+	if lpaSignedAt.IsZero() {
 		return false
 	}
 
 	if d.IsTrustCorporation {
 		switch d.WouldLikeSecondSignatory {
 		case form.Yes:
-			return d.AuthorisedSignatories[0].Confirmed.After(after) && d.AuthorisedSignatories[1].Confirmed.After(after)
+			return lpaSignedAt.Equal(d.AuthorisedSignatories[0].LpaSignedAt) && !d.AuthorisedSignatories[0].Confirmed.IsZero() &&
+				lpaSignedAt.Equal(d.AuthorisedSignatories[1].LpaSignedAt) && !d.AuthorisedSignatories[1].Confirmed.IsZero()
 		case form.No:
-			return d.AuthorisedSignatories[0].Confirmed.After(after)
+			return lpaSignedAt.Equal(d.AuthorisedSignatories[0].LpaSignedAt) && !d.AuthorisedSignatories[0].Confirmed.IsZero()
 		default:
 			return false
 		}
 	}
 
-	return d.Confirmed.After(after)
+	return lpaSignedAt.Equal(d.LpaSignedAt) && !d.Confirmed.IsZero()
 }
 
 type AttorneyTasks struct {
@@ -63,4 +74,5 @@ type TrustCorporationSignatory struct {
 	LastName          string
 	ProfessionalTitle string
 	Confirmed         time.Time
+	LpaSignedAt       time.Time
 }
