b15515a4dc177c2193252748c1b869d16bcf3d4f
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-10-13 10:35:13 +0100

    remove interface endpoint policies

--- terraform/account/region/vpc_endpoints.tf
+++ terraform/account/region/vpc_endpoints.tf
@@ -56,27 +56,27 @@ resource "aws_vpc_endpoint" "private" {
   tags                = { Name = "${each.value}-private-${data.aws_region.current.name}" }
 }
 
-resource "aws_vpc_endpoint_policy" "private" {
-  provider        = aws.region
-  for_each        = local.interface_endpoint
-  vpc_endpoint_id = aws_vpc_endpoint.private[each.value].id
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Sid" : "AllowAll",
-        "Effect" : "Allow",
-        "Principal" : {
-          "AWS" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-        },
-        "Action" : [
-          "${startswith(each.value, "ecr") ? "ecr" : each.value}:*"
-        ],
-        "Resource" : "*"
-      }
-    ]
-  })
-}
+# resource "aws_vpc_endpoint_policy" "private" {
+#   provider        = aws.region
+#   for_each        = local.interface_endpoint
+#   vpc_endpoint_id = aws_vpc_endpoint.private[each.value].id
+#   policy = jsonencode({
+#     "Version" : "2012-10-17",
+#     "Statement" : [
+#       {
+#         "Sid" : "AllowAll",
+#         "Effect" : "Allow",
+#         "Principal" : {
+#           "AWS" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+#         },
+#         "Action" : [
+#           "${startswith(each.value, "ecr") ? "ecr" : each.value}:*"
+#         ],
+#         "Resource" : "*"
+#       }
+#     ]
+#   })
+# }
 
 data "aws_route_tables" "public" {
   provider = aws.region
@@ -92,27 +92,27 @@ resource "aws_vpc_endpoint" "s3" {
   service_name      = "com.amazonaws.${data.aws_region.current.name}.s3"
   route_table_ids   = tolist(data.aws_route_tables.public.ids)
   vpc_endpoint_type = "Gateway"
-  policy            = data.aws_iam_policy_document.s3_vpc_endpoint.json
-  tags              = { Name = "s3-private-${data.aws_region.current.name}" }
+  # policy            = data.aws_iam_policy_document.s3_vpc_endpoint.json
+  tags = { Name = "s3-private-${data.aws_region.current.name}" }
 }
 
-data "aws_iam_policy_document" "s3_vpc_endpoint" {
-  provider = aws.region
-  statement {
-    sid       = "S3VpcEndpointPolicy"
-    actions   = ["s3:*"]
-    resources = ["*"]
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-    condition {
-      test     = "StringEquals"
-      variable = "aws:PrincipalAccount"
-      values   = [data.aws_caller_identity.current.account_id]
-    }
-  }
-}
+# data "aws_iam_policy_document" "s3_vpc_endpoint" {
+#   provider = aws.region
+#   statement {
+#     sid       = "S3VpcEndpointPolicy"
+#     actions   = ["s3:*"]
+#     resources = ["*"]
+#     principals {
+#       type        = "AWS"
+#       identifiers = ["*"]
+#     }
+#     condition {
+#       test     = "StringEquals"
+#       variable = "aws:PrincipalAccount"
+#       values   = [data.aws_caller_identity.current.account_id]
+#     }
+#   }
+# }
 
 resource "aws_vpc_endpoint" "dynamodb" {
   provider          = aws.region
@@ -120,25 +120,25 @@ resource "aws_vpc_endpoint" "dynamodb" {
   service_name      = "com.amazonaws.${data.aws_region.current.name}.dynamodb"
   route_table_ids   = tolist(data.aws_route_tables.public.ids)
   vpc_endpoint_type = "Gateway"
-  policy            = data.aws_iam_policy_document.dynamodb_vpc_endpoint.json
-  tags              = { Name = "dynamodb-private-${data.aws_region.current.name}" }
+  # policy            = data.aws_iam_policy_document.dynamodb_vpc_endpoint.json
+  tags = { Name = "dynamodb-private-${data.aws_region.current.name}" }
 }
 
-data "aws_iam_policy_document" "dynamodb_vpc_endpoint" {
-  provider = aws.region
-  statement {
-    sid       = "DynamoDBVpcEndpointPolicy"
-    effect    = "Allow"
-    actions   = ["dynamodb:*"]
-    resources = ["*"]
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-    condition {
-      test     = "StringEquals"
-      variable = "aws:PrincipalAccount"
-      values   = [data.aws_caller_identity.current.account_id]
-    }
-  }
-}
+# data "aws_iam_policy_document" "dynamodb_vpc_endpoint" {
+#   provider = aws.region
+#   statement {
+#     sid       = "DynamoDBVpcEndpointPolicy"
+#     effect    = "Allow"
+#     actions   = ["dynamodb:*"]
+#     resources = ["*"]
+#     principals {
+#       type        = "AWS"
+#       identifiers = ["*"]
+#     }
+#     condition {
+#       test     = "StringEquals"
+#       variable = "aws:PrincipalAccount"
+#       values   = [data.aws_caller_identity.current.account_id]
+#     }
+#   }
+# }
