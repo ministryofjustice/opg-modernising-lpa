d1d172621c05add2ae712fa75b4dc08c3438cb3a
Joshua Hawxwell <m@hawx.me>
2023-10-30 07:34:12 +0000

    MLPAB-1306 Delete or withdraw an LPA (#804)

--- internal/page/guidance.go
+++ internal/page/guidance.go
@@ -2,6 +2,7 @@ package page
 
 import (
 	"net/http"
+	"net/url"
 
 	"github.com/ministryofjustice/opg-go-common/template"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/validation"
@@ -9,13 +10,15 @@ import (
 
 type guidanceData struct {
 	App    AppData
+	Query  url.Values
 	Errors validation.List
 }
 
 func Guidance(tmpl template.Template) Handler {
 	return func(appData AppData, w http.ResponseWriter, r *http.Request) error {
 		data := &guidanceData{
-			App: appData,
+			App:   appData,
+			Query: r.URL.Query(),
 		}
 
 		return tmpl(w, data)
