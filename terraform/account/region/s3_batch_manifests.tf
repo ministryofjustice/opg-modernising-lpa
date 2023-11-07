5bec3bd854b9d9f502dd7ab142c8655e9710e82a
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-11-07 14:55:18 +0000

    MLPAB-1366 - Use CMK for manifests storage (#828)
    * MLPAB-1580 format variables file
    
    * create kms key and encrypt manifests

--- terraform/account/region/s3_batch_manifests.tf
+++ terraform/account/region/s3_batch_manifests.tf
@@ -1,5 +1,6 @@
 module "s3_batch_manifests" {
-  source = "./modules/s3_batch_manifests"
+  source                      = "./modules/s3_batch_manifests"
+  s3_encryption_kms_key_alias = var.reduced_fees_uploads_s3_encryption_kms_key_alias
   providers = {
     aws.region = aws.region
   }
