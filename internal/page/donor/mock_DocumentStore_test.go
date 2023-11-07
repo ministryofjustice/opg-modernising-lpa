409c96b204f2d92632a0dcdb0c8a502c8aea6ffc
Joshua Hawxwell <m@hawx.me>
2023-11-07 12:42:57 +0000

    Send reduced-fee-requested correctly
    This moves where document stuff is handled to be only happening in
    UploadEvidence or things it calls directly. It prevents the problem of Sent
    being set, then not sending the reduced-fee-requested event as no documents need sending.

--- internal/page/donor/mock_DocumentStore_test.go
+++ internal/page/donor/mock_DocumentStore_test.go
@@ -106,6 +106,20 @@ func (_m *mockDocumentStore) Put(_a0 context.Context, _a1 page.Document) error {
 	return r0
 }
 
+// Submit provides a mock function with given fields: _a0, _a1, _a2
+func (_m *mockDocumentStore) Submit(_a0 context.Context, _a1 *page.Lpa, _a2 page.Documents) error {
+	ret := _m.Called(_a0, _a1, _a2)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, *page.Lpa, page.Documents) error); ok {
+		r0 = rf(_a0, _a1, _a2)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
 type mockConstructorTestingTnewMockDocumentStore interface {
 	mock.TestingT
 	Cleanup(func())
