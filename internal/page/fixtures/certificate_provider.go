e452d0f49b723e51f9ae0aeef645dc33e7bff15e
Joshua Hawxwell <m@hawx.me>
2023-11-09 10:20:13 +0000

    Add uids in all fixtures

--- internal/page/fixtures/certificate_provider.go
+++ internal/page/fixtures/certificate_provider.go
@@ -63,6 +63,7 @@ func CertificateProvider(
 			certificateProviderCtx = page.ContextWithSessionData(r.Context(), &page.SessionData{SessionID: certificateProviderSessionID, LpaID: lpa.ID})
 		)
 
+		lpa.UID = makeUid()
 		lpa.Donor = makeDonor()
 		lpa.Type = page.LpaTypePropertyFinance
 		if lpaType == "hw" {
