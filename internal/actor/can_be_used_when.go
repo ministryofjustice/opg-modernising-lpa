a058603c2dd528de68db5042d91a3ac3067d9c58
Joshua Hawxwell <m@hawx.me>
2023-11-17 15:00:37 +0000

    Set when lpa can be used if lpa type set to hw

--- internal/actor/can_be_used_when.go
+++ internal/actor/can_be_used_when.go
@@ -1,9 +1,10 @@
 package actor
 
-//go:generate enumerator -type CanBeUsedWhen -linecomment -trimprefix -empty
+//go:generate enumerator -type CanBeUsedWhen -linecomment -trimprefix
 type CanBeUsedWhen uint8
 
 const (
-	CanBeUsedWhenCapacityLost CanBeUsedWhen = iota + 1 // when-capacity-lost
-	CanBeUsedWhenHasCapacity                           // when-has-capacity
+	CanBeUsedWhenUnknown      CanBeUsedWhen = iota
+	CanBeUsedWhenCapacityLost               // when-capacity-lost
+	CanBeUsedWhenHasCapacity                // when-has-capacity
 )
--- internal/page/donor/choose_address.go
+++ internal/page/donor/choose_address.go
@@ -1,6 +1,5 @@
 package donor
 
-
 import (
 	"context"
 	"errors"
