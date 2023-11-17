d0514ab1e72293c21f72885db4e2ca1ae94ca5de
Alex Saunders <acsauk@gmail.com>
2023-11-17 15:25:22 +0000

    test fix

--- internal/page/certificateprovider/guidance_test.go
+++ internal/page/certificateprovider/guidance_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/actor"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/page"
 	"github.com/stretchr/testify/assert"
 )
@@ -14,18 +15,24 @@ func TestGuidance(t *testing.T) {
 	r, _ := http.NewRequest(http.MethodGet, "/", nil)
 
 	lpa := &page.Lpa{}
+	certificateProvider := &actor.CertificateProviderProvidedDetails{}
 
 	donorStore := newMockDonorStore(t)
 	donorStore.
 		On("GetAny", r.Context()).
 		Return(lpa, nil)
 
+	certificateProviderStore := newMockCertificateProviderStore(t)
+	certificateProviderStore.
+		On("Get", r.Context()).
+		Return(certificateProvider, nil)
+
 	template := newMockTemplate(t)
 	template.
-		On("Execute", w, &guidanceData{App: testAppData, Lpa: lpa}).
+		On("Execute", w, &guidanceData{App: testAppData, Lpa: lpa, CertificateProvider: certificateProvider}).
 		Return(nil)
 
-	err := Guidance(template.Execute, donorStore)(testAppData, w, r)
+	err := Guidance(template.Execute, donorStore, certificateProviderStore)(testAppData, w, r)
 	resp := w.Result()
 
 	assert.Nil(t, err)
@@ -42,7 +49,7 @@ func TestGuidanceWhenNilDataStores(t *testing.T) {
 
 	r, _ := http.NewRequest(http.MethodGet, "/", nil)
 
-	err := Guidance(template.Execute, nil)(testAppData, w, r)
+	err := Guidance(template.Execute, nil, nil)(testAppData, w, r)
 	resp := w.Result()
 
 	assert.Nil(t, err)
@@ -60,7 +67,28 @@ func TestGuidanceWhenDonorStoreErrors(t *testing.T) {
 		On("GetAny", r.Context()).
 		Return(lpa, expectedError)
 
-	err := Guidance(nil, donorStore)(testAppData, w, r)
+	err := Guidance(nil, donorStore, nil)(testAppData, w, r)
+
+	assert.Equal(t, expectedError, err)
+}
+
+func TestGuidanceWhenCertificateProviderStoreErrors(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodGet, "/", nil)
+
+	lpa := &page.Lpa{}
+
+	donorStore := newMockDonorStore(t)
+	donorStore.
+		On("GetAny", r.Context()).
+		Return(lpa, nil)
+
+	certificateProviderStore := newMockCertificateProviderStore(t)
+	certificateProviderStore.
+		On("Get", r.Context()).
+		Return(&actor.CertificateProviderProvidedDetails{}, expectedError)
+
+	err := Guidance(nil, donorStore, certificateProviderStore)(testAppData, w, r)
 
 	assert.Equal(t, expectedError, err)
 }
@@ -79,7 +107,7 @@ func TestGuidanceWhenTemplateErrors(t *testing.T) {
 		On("Execute", w, &guidanceData{App: testAppData, Lpa: &page.Lpa{}}).
 		Return(expectedError)
 
-	err := Guidance(template.Execute, donorStore)(testAppData, w, r)
+	err := Guidance(template.Execute, donorStore, nil)(testAppData, w, r)
 
 	assert.Equal(t, expectedError, err)
 }
