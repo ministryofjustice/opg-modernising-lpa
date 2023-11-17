7578927a8c055c60a1a2f3973972effc0ffc2404
Joshua Hawxwell <m@hawx.me>
2023-11-17 14:34:49 +0000

    Move other types out of donor_provided.go file

---
+++ internal/actor/replacement_attorneys_step_in_test.go
@@ -0,0 +1,51 @@
+package actor
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReplacementAttorneysStepIn(t *testing.T) {
+	values := map[ReplacementAttorneysStepIn]string{
+		ReplacementAttorneysStepInWhenAllCanNoLongerAct: "all",
+		ReplacementAttorneysStepInWhenOneCanNoLongerAct: "one",
+		ReplacementAttorneysStepInAnotherWay:            "other",
+	}
+
+	for value, s := range values {
+		t.Run(fmt.Sprintf("parse(%s)", s), func(t *testing.T) {
+			parsed, err := ParseReplacementAttorneysStepIn(s)
+			assert.Nil(t, err)
+			assert.Equal(t, value, parsed)
+		})
+
+		t.Run(fmt.Sprintf("string(%s)", s), func(t *testing.T) {
+			assert.Equal(t, s, value.String())
+		})
+	}
+
+	t.Run("parse invalid", func(t *testing.T) {
+		_, err := ParseReplacementAttorneysStepIn("invalid")
+		assert.NotNil(t, err)
+	})
+
+	t.Run("IsWhenAllCanNoLongerAct", func(t *testing.T) {
+		assert.True(t, ReplacementAttorneysStepInWhenAllCanNoLongerAct.IsWhenAllCanNoLongerAct())
+		assert.False(t, ReplacementAttorneysStepInWhenOneCanNoLongerAct.IsWhenAllCanNoLongerAct())
+		assert.False(t, ReplacementAttorneysStepInAnotherWay.IsWhenAllCanNoLongerAct())
+	})
+
+	t.Run("IsWhenOneCanNoLongerAct", func(t *testing.T) {
+		assert.True(t, ReplacementAttorneysStepInWhenOneCanNoLongerAct.IsWhenOneCanNoLongerAct())
+		assert.False(t, ReplacementAttorneysStepInWhenAllCanNoLongerAct.IsWhenOneCanNoLongerAct())
+		assert.False(t, ReplacementAttorneysStepInAnotherWay.IsWhenOneCanNoLongerAct())
+	})
+
+	t.Run("IsAnotherWay", func(t *testing.T) {
+		assert.True(t, ReplacementAttorneysStepInAnotherWay.IsAnotherWay())
+		assert.False(t, ReplacementAttorneysStepInWhenAllCanNoLongerAct.IsAnotherWay())
+		assert.False(t, ReplacementAttorneysStepInWhenOneCanNoLongerAct.IsAnotherWay())
+	})
+}
