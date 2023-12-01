3a89d80e8514e3ea7e06fe68cfd4070a788a9232
Alex Saunders <acsauk@gmail.com>
2023-12-01 12:24:13 +0000

    cover language preference handlers

---
+++ internal/page/attorney/your_preferred_language_test.go
@@ -0,0 +1,135 @@
+package attorney
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
+
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/actor"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/form"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/localize"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/page"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/validation"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestGetYourPreferredLanguage(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodGet, "/", nil)
+
+	template := newMockTemplate(t)
+	template.
+		On("Execute", w, &yourPreferredLanguageData{
+			App: testAppData,
+			Form: &form.LanguagePreferenceForm{
+				Preference: localize.Cy,
+			},
+			Options:    localize.LangValues,
+			FieldNames: form.FieldNames,
+		}).
+		Return(nil)
+
+	err := YourPreferredLanguage(template.Execute, nil)(testAppData, w, r, &actor.AttorneyProvidedDetails{LpaID: "lpa-id", ContactLanguagePreference: localize.Cy})
+
+	resp := w.Result()
+
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
+
+func TestGetYourPreferredLanguageWhenTemplateError(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodGet, "/", nil)
+
+	template := newMockTemplate(t)
+	template.
+		On("Execute", w, mock.Anything).
+		Return(expectedError)
+
+	err := YourPreferredLanguage(template.Execute, nil)(testAppData, w, r, &actor.AttorneyProvidedDetails{LpaID: "lpa-id", ContactLanguagePreference: localize.Cy})
+
+	resp := w.Result()
+
+	assert.Equal(t, expectedError, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
+
+func TestPostYourPreferredLanguage(t *testing.T) {
+	testCases := []localize.Lang{localize.En, localize.Cy}
+
+	for _, lang := range testCases {
+		t.Run(lang.String(), func(t *testing.T) {
+			formValues := url.Values{form.FieldNames.Preference: {lang.String()}}
+
+			w := httptest.NewRecorder()
+			r, _ := http.NewRequest(http.MethodPost, "/", strings.NewReader(formValues.Encode()))
+			r.Header.Add("Content-Type", page.FormUrlEncoded)
+
+			attorneyStore := newMockAttorneyStore(t)
+			attorneyStore.
+				On("Put", r.Context(), &actor.AttorneyProvidedDetails{LpaID: "lpa-id", ContactLanguagePreference: lang}).
+				Return(nil)
+
+			err := YourPreferredLanguage(nil, attorneyStore)(testAppData, w, r, &actor.AttorneyProvidedDetails{LpaID: "lpa-id"})
+
+			resp := w.Result()
+
+			assert.Nil(t, err)
+			assert.Equal(t, http.StatusFound, resp.StatusCode)
+			assert.Equal(t, page.Paths.Attorney.ConfirmYourDetails.Format("lpa-id"), resp.Header.Get("Location"))
+		})
+	}
+}
+
+func TestPostYourPreferredLanguageWhenAttorneyStoreError(t *testing.T) {
+	formValues := url.Values{form.FieldNames.Preference: {localize.En.String()}}
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodPost, "/", strings.NewReader(formValues.Encode()))
+	r.Header.Add("Content-Type", page.FormUrlEncoded)
+
+	attorneyStore := newMockAttorneyStore(t)
+	attorneyStore.
+		On("Put", r.Context(), mock.Anything).
+		Return(expectedError)
+
+	err := YourPreferredLanguage(nil, attorneyStore)(testAppData, w, r, &actor.AttorneyProvidedDetails{LpaID: "lpa-id"})
+
+	resp := w.Result()
+
+	assert.Equal(t, expectedError, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
+
+func TestPostYourPreferredLanguageWhenInvalidData(t *testing.T) {
+	formValues := url.Values{form.FieldNames.Preference: {"not-a-lang"}}
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodPost, "/", strings.NewReader(formValues.Encode()))
+	r.Header.Add("Content-Type", page.FormUrlEncoded)
+
+	template := newMockTemplate(t)
+	template.
+		On("Execute", w, &yourPreferredLanguageData{
+			App: testAppData,
+			Form: &form.LanguagePreferenceForm{
+				Error:      errors.New("invalid Lang 'not-a-lang'"),
+				ErrorLabel: "yourPreferredLanguageForWhenWeContactYou",
+			},
+			Options:    localize.LangValues,
+			FieldNames: form.FieldNames,
+			Errors:     validation.With(form.FieldNames.Preference, validation.SelectError{Label: "yourPreferredLanguageForWhenWeContactYou"}),
+		}).
+		Return(nil)
+
+	err := YourPreferredLanguage(template.Execute, nil)(testAppData, w, r, &actor.AttorneyProvidedDetails{LpaID: "lpa-id"})
+
+	resp := w.Result()
+
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
