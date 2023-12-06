a893092db82dd4ea5ba05a912d93c234f1c36ad2
Joshua Hawxwell <m@hawx.me>
2023-12-06 13:26:59 +0000

    Use replacement template for replacements

--- internal/notify/emails.go
+++ internal/notify/emails.go
@@ -17,3 +17,21 @@ func (e InitialOriginalAttorneyEmail) emailID(isProduction bool) string {
 
 	return "376d7ef2-7941-46c2-b372-bacca0e00c1d"
 }
+
+type InitialReplacementAttorneyEmail struct {
+	DonorFullName             string
+	LpaType                   string
+	AttorneyFullName          string
+	DonorFirstNames           string
+	AttorneyStartPageURL      string
+	ShareCode                 string
+	DonorFirstNamesPossessive string
+}
+
+func (e InitialReplacementAttorneyEmail) emailID(isProduction bool) string {
+	if isProduction {
+		return "8d335239-7002-4825-8393-cc00ad246648"
+	}
+
+	return "738d500f-b674-4e1e-8039-a7be53fce528"
+}
