16c7cdb27d76295e65bc06b871edb3981a7979fe
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-12-07 13:09:41 +0000

    cleanup

--- terraform/environment/region/ecs.tf
+++ terraform/environment/region/ecs.tf
@@ -73,9 +73,8 @@ module "mock_onelogin" {
   container_version               = var.mock_onelogin_service_container_version
   alb_deletion_protection_enabled = var.alb_deletion_protection_enabled
   container_port                  = 8080
-  # TODO: figure out how to internally reference this
-  public_access_enabled = var.public_access_enabled
-  redirect_base_url     = var.app_env_vars.auth_redirect_base_url
+  public_access_enabled           = var.public_access_enabled
+  redirect_base_url               = var.app_env_vars.auth_redirect_base_url
   network = {
     vpc_id              = data.aws_vpc.main.id
     application_subnets = data.aws_subnet.application.*.id
