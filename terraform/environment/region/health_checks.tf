ad0ee8bf1c0ae1a0c609e7438ea5c1ea93ea149b
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-12-13 17:34:26 +0000

    create sns topic for health checks

--- terraform/environment/region/health_checks.tf
+++ terraform/environment/region/health_checks.tf
@@ -8,10 +8,10 @@ resource "aws_route53_health_check" "service_health_check" {
   resource_path     = "/health-check/service"
   measure_latency   = true
   regions           = ["us-east-1", "eu-west-1", "ap-southeast-1"]
-  provider          = aws.global
   tags = {
     Name = "${data.aws_default_tags.current.tags.environment-name}-service-health-check-${data.aws_region.current.name}"
   }
+  provider = aws.global
 }
 
 resource "aws_cloudwatch_metric_alarm" "service_health_check" {
@@ -44,10 +44,10 @@ resource "aws_route53_health_check" "dependency_health_check" {
   resource_path     = "/health-check/dependency"
   measure_latency   = true
   regions           = ["us-east-1", "eu-west-1", "ap-southeast-1"]
-  provider          = aws.global
   tags = {
     Name = "${data.aws_default_tags.current.tags.environment-name}-dependency-health-check-${data.aws_region.current.name}"
   }
+  provider = aws.global
 }
 
 resource "aws_cloudwatch_metric_alarm" "dependency_health_check" {
@@ -66,18 +66,24 @@ resource "aws_cloudwatch_metric_alarm" "dependency_health_check" {
   dimensions = {
     HealthCheckId = aws_route53_health_check.dependency_health_check.id
   }
-
   provider = aws.global
 }
 
+resource "aws_sns_topic" "health_checks" {
+  name     = "${data.aws_default_tags.current.tags.environment-name}-health-checks-${data.aws_region.current.name}"
+  tags     = data.aws_default_tags.current.tags
+  provider = aws.region
+}
+
 resource "pagerduty_service_integration" "service_health_check" {
-  name    = "Modernising LPA ${data.aws_default_tags.current.tags.environment-name} ${data.aws_region.current.name} Service Health Check Alarm"
-  service = data.pagerduty_service.main.id
-  vendor  = data.pagerduty_vendor.cloudwatch.id
+  name     = "Modernising LPA ${data.aws_default_tags.current.tags.environment-name} ${data.aws_region.current.name} Service Health Check Alarm"
+  service  = data.pagerduty_service.main.id
+  vendor   = data.pagerduty_vendor.cloudwatch.id
+  provider = aws.region
 }
 
 resource "aws_sns_topic_subscription" "service_health_check" {
-  topic_arn              = data.aws_sns_topic.service_health_check.arn
+  topic_arn              = aws_sns_topic.health_checks.arn
   protocol               = "https"
   endpoint_auto_confirms = true
   endpoint               = "https://events.pagerduty.com/integration/${pagerduty_service_integration.service_health_check.integration_key}/enqueue"
@@ -91,7 +97,7 @@ resource "pagerduty_service_integration" "dependency_health_check" {
 }
 
 resource "aws_sns_topic_subscription" "dependency_health_check" {
-  topic_arn              = data.aws_sns_topic.service_health_check.arn
+  topic_arn              = aws_sns_topic.health_checks.arn
   protocol               = "https"
   endpoint_auto_confirms = true
   endpoint               = "https://events.pagerduty.com/integration/${pagerduty_service_integration.service_health_check.integration_key}/enqueue"
