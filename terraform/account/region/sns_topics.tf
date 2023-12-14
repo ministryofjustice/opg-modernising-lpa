2a4c04d15462a64c17fa715f212a7c0a81b57cf7
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-12-14 11:25:47 +0000

    rename topic and add a new one

--- terraform/account/region/sns_topics.tf
+++ terraform/account/region/sns_topics.tf
@@ -24,8 +24,29 @@ resource "aws_sns_topic" "ecs_autoscaling_alarms" {
   provider                                 = aws.region
 }
 
-resource "aws_sns_topic" "health_checks_global" {
-  name                                     = "health-checks"
+resource "aws_sns_topic" "service_health_checks_global" {
+  name                                     = "service-health-checks"
+  kms_master_key_id                        = data.aws_kms_alias.sns_kms_key_alias.target_key_id
+  application_failure_feedback_role_arn    = data.aws_iam_role.sns_failure_feedback.arn
+  application_success_feedback_role_arn    = data.aws_iam_role.sns_success_feedback.arn
+  application_success_feedback_sample_rate = 100
+  firehose_failure_feedback_role_arn       = data.aws_iam_role.sns_failure_feedback.arn
+  firehose_success_feedback_role_arn       = data.aws_iam_role.sns_success_feedback.arn
+  firehose_success_feedback_sample_rate    = 100
+  http_failure_feedback_role_arn           = data.aws_iam_role.sns_failure_feedback.arn
+  http_success_feedback_role_arn           = data.aws_iam_role.sns_success_feedback.arn
+  http_success_feedback_sample_rate        = 100
+  lambda_failure_feedback_role_arn         = data.aws_iam_role.sns_failure_feedback.arn
+  lambda_success_feedback_role_arn         = data.aws_iam_role.sns_success_feedback.arn
+  lambda_success_feedback_sample_rate      = 100
+  sqs_failure_feedback_role_arn            = data.aws_iam_role.sns_failure_feedback.arn
+  sqs_success_feedback_role_arn            = data.aws_iam_role.sns_success_feedback.arn
+  sqs_success_feedback_sample_rate         = 100
+  provider                                 = aws.global
+}
+
+resource "aws_sns_topic" "dependency_health_checks_global" {
+  name                                     = "dependency-health-checks"
   kms_master_key_id                        = data.aws_kms_alias.sns_kms_key_alias.target_key_id
   application_failure_feedback_role_arn    = data.aws_iam_role.sns_failure_feedback.arn
   application_success_feedback_role_arn    = data.aws_iam_role.sns_success_feedback.arn
