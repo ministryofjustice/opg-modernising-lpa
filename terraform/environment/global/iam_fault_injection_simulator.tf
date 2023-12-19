ba940cc6f1cc8b87785d0f648081eb73c0e88255
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-12-19 14:49:38 +0000

    expand abberv

--- terraform/environment/global/iam_fault_injection_simulator.tf
+++ terraform/environment/global/iam_fault_injection_simulator.tf
@@ -1,10 +1,10 @@
-resource "aws_iam_role" "fis" {
-  name               = "fis-${data.aws_default_tags.current.tags.environment-name}"
+resource "aws_iam_role" "fault_injection_simulator" {
+  name               = "fault-injection-simulator-${data.aws_default_tags.current.tags.environment-name}"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume.json
   provider           = aws.global
 }
 
-data "aws_iam_policy_document" "fis_assume" {
+data "aws_iam_policy_document" "fault_injection_simulator_assume" {
   statement {
     actions = ["sts:AssumeRole"]
 
@@ -16,8 +16,8 @@ data "aws_iam_policy_document" "fis_assume" {
   provider = aws.global
 }
 
-resource "aws_iam_role_policy_attachment" "fis" {
-  role       = aws_iam_role.fis.name
+resource "aws_iam_role_policy_attachment" "fault_injection_simulator" {
+  role       = aws_iam_role.fault_injection_simulator.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSFaultInjectionSimulatorECSAccess"
   provider   = aws.global
 }
