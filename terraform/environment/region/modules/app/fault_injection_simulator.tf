01bab40be7d49dc4134ec5cd9f757f78a43fff68
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-12-20 12:37:31 +0000

    add some permissions, remove logs and log encryption

--- terraform/environment/region/modules/app/fault_injection_simulator.tf
+++ terraform/environment/region/modules/app/fault_injection_simulator.tf
@@ -6,8 +6,8 @@ data "aws_kms_alias" "cloudwatch_application_logs_encryption" {
 resource "aws_cloudwatch_log_group" "fis_app_ecs_tasks" {
   name              = "/aws/fis/app-ecs-tasks-experiment-${data.aws_default_tags.current.tags.environment-name}"
   retention_in_days = 7
-  kms_key_id        = data.aws_kms_alias.cloudwatch_application_logs_encryption.target_key_arn
-  provider          = aws.region
+  # kms_key_id        = data.aws_kms_alias.cloudwatch_application_logs_encryption.target_key_arn
+  provider = aws.region
 }
 
 resource "aws_fis_experiment_template" "ecs_app" {
@@ -20,21 +20,9 @@ resource "aws_fis_experiment_template" "ecs_app" {
   }
 
   action {
-    action_id   = "aws:ecs:stop-task"
-    description = null
-    name        = "stop_task"
-    start_after = ["wait_before_stop_task"]
-    target {
-      key   = "Tasks"
-      value = "app-ecs-tasks-${data.aws_default_tags.current.tags.environment-name}"
-    }
-  }
-
-  action {
     action_id   = "aws:ecs:task-cpu-stress"
     description = null
     name        = "cpu_stress_100_percent"
-    start_after = ["wait"]
     parameter {
       key   = "duration"
       value = "PT5M"
@@ -42,51 +30,6 @@ resource "aws_fis_experiment_template" "ecs_app" {
     target {
       key   = "Tasks"
       value = "app-ecs-tasks-${data.aws_default_tags.current.tags.environment-name}"
-    }
-  }
-
-  action {
-    action_id   = "aws:ecs:task-network-latency"
-    description = null
-    name        = "ecs_network_latency"
-    start_after = []
-    parameter {
-      key   = "duration"
-      value = "PT5M"
-    }
-    target {
-      key   = "Tasks"
-      value = "app-ecs-tasks-${data.aws_default_tags.current.tags.environment-name}"
-    }
-  }
-
-  action {
-    action_id   = "aws:fis:wait"
-    description = null
-    name        = "wait"
-    start_after = ["stop_task"]
-    parameter {
-      key   = "duration"
-      value = "PT5M"
-    }
-  }
-
-  action {
-    action_id   = "aws:fis:wait"
-    description = null
-    name        = "wait_before_stop_task"
-    start_after = ["ecs_network_latency"]
-    parameter {
-      key   = "duration"
-      value = "PT5M"
-    }
-  }
-
-  log_configuration {
-    log_schema_version = 2
-
-    cloudwatch_logs_configuration {
-      log_group_arn = "${aws_cloudwatch_log_group.fis_app_ecs_tasks.arn}:*"
     }
   }
 
@@ -97,14 +40,18 @@ resource "aws_fis_experiment_template" "ecs_app" {
 
   target {
     name = "app-ecs-tasks-${data.aws_default_tags.current.tags.environment-name}"
-    parameters = {
-      "cluster" : var.ecs_cluster,
-      "service" : aws_ecs_service.app.name,
-    }
-    resource_tag {
-      key   = "aws:ecs:cluster-name"
-      value = var.ecs_cluster
-    }
+    resource_arns = [
+      "arn:aws:ecs:eu-west-1:653761790766:task/936mlpab157-eu-west-1/1b1aedd43f94458d9ef3475479e19169",
+      # aws_ecs_task_definition.mock_onelogin.arn,
+    ]
+    # parameters = {
+    #   "cluster" : var.ecs_cluster,
+    #   "service" : aws_ecs_service.app.name,
+    # }
+    # resource_tag {
+    #   key   = "aws:ecs:service"
+    #   value = aws_ecs_service.app.name
+    # }
     resource_type  = "aws:ecs:task"
     selection_mode = "ALL"
   }
