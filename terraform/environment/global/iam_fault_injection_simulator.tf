8332de5bc99e46b76bfd5a2b27906442261913d7
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-12-22 10:54:28 +0000

    Setup FIS permissions Prepare for FIS ECS Task permissions

--- terraform/environment/global/iam_fault_injection_simulator.tf
+++ terraform/environment/global/iam_fault_injection_simulator.tf
@@ -22,14 +22,22 @@ resource "aws_iam_role_policy_attachment" "fault_injection_simulator" {
   provider   = aws.global
 }
 
-resource "aws_iam_role_policy" "additional_permissions" {
+resource "aws_iam_role_policy" "fault_injection_simulator_additional_permissions" {
   name     = "additional-permissions"
   role     = aws_iam_role.fault_injection_simulator.name
-  policy   = data.aws_iam_policy_document.additional_permissions.json
+  policy   = data.aws_iam_policy_document.fault_injection_simulator_combined.json
   provider = aws.global
 }
 
-data "aws_iam_policy_document" "additional_permissions" {
+#TODO: consolidate this into a single document if possible
+data "aws_iam_policy_document" "fault_injection_simulator_combined" {
+  source_policy_documents = [
+    data.aws_iam_policy_document.fault_injection_simulator_additional_permissions.json,
+    data.aws_iam_policy_document.fis_autocreated_role.json
+  ]
+}
+
+data "aws_iam_policy_document" "fault_injection_simulator_additional_permissions" {
 
   statement {
     sid       = "AllowServiceLinkedRole"
@@ -65,5 +73,63 @@ data "aws_iam_policy_document" "additional_permissions" {
       "ssm:ListCommands",
       "ssm:CancelCommand",
     ]
+  }
+}
+
+data "aws_iam_policy_document" "fis_autocreated_role" {
+  # example taken from role created in console to be attached to the FIS role
+  version = "2012-10-17"
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecs:DescribeClusters",
+      "ecs:ListContainerInstances"
+    ]
+    resources = [
+      "arn:aws:ecs:*:*:cluster/*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecs:DescribeTasks",
+      "ecs:StopTask"
+    ]
+    resources = [
+      "arn:aws:ecs:*:*:task/*/*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecs:ListTasks",
+      "ecs:UpdateContainerInstancesState"
+    ]
+    resources = [
+      "arn:aws:ecs:*:*:container-instance/*/*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ssm:SendCommand"
+    ]
+    resources = [
+      "arn:aws:ssm:*:*:managed-instance/*",
+      "arn:aws:ssm:*:*:document/*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ssm:ListCommands",
+      "ssm:CancelCommand"
+    ]
+    resources = ["*"]
   }
 }
