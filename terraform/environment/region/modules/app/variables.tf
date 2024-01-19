26ecbe92401546a5cf21411fdc959dbfda87fe83
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-01-19 14:32:55 +0000

    refactor role to pull in name and arn

--- terraform/environment/region/modules/app/variables.tf
+++ terraform/environment/region/modules/app/variables.tf
@@ -15,7 +15,7 @@ variable "ecs_task_role" {
   description = "ARN of IAM role that allows your Amazon ECS container task to make calls to other AWS services."
 }
 
-variable "fault_injection_simulator_role_arn" {
+variable "fault_injection_simulator_role" {
   type        = any
   description = "ARN of IAM role that allows AWS FIS to make calls to other AWS services."
 }
