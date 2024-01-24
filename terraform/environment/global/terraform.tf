ea66bcdc9b9d5baaa7df4963684f4f679f0dbdcb
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-01-24 20:36:18 +0000

    remove unused

--- terraform/environment/global/terraform.tf
+++ terraform/environment/global/terraform.tf
@@ -18,11 +18,3 @@ terraform {
 data "aws_default_tags" "current" {
   provider = aws.global
 }
-
-data "aws_caller_identity" "global" {
-  provider = aws.global
-}
-
-data "aws_region" "global" {
-  provider = aws.global
-}
--- terraform/environment/region/modules/application_logs/terraform.tf
+++ terraform/environment/region/modules/application_logs/terraform.tf
@@ -11,10 +11,6 @@ terraform {
   }
 }
 
-data "aws_region" "current" {
-  provider = aws.region
-}
-
 data "aws_default_tags" "current" {
   provider = aws.region
 }
