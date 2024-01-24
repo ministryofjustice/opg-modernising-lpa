ec5e729188266782a8679701a94440dcb10f2c61
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-01-24 22:00:38 +0000

    remove unused and add description to lambda

--- terraform/environment/region/modules/uploads_s3_bucket/lambda.tf
+++ terraform/environment/region/modules/uploads_s3_bucket/lambda.tf
@@ -5,8 +5,7 @@ module "s3_create_batch_replication_jobs" {
   environment_variables = {
     ENVIRONMENT = data.aws_default_tags.current.tags.environment-name
   }
-  image_uri = "${var.s3_replication.lambda_function_image_ecr_url}:${var.s3_replication.lambda_function_image_tag}"
-  # ecr_arn     = var.s3_replication.lambda_function_image_ecr_arn
+  image_uri   = "${var.s3_replication.lambda_function_image_ecr_url}:${var.s3_replication.lambda_function_image_tag}"
   environment = data.aws_default_tags.current.tags.environment-name
   kms_key     = data.aws_kms_alias.cloudwatch_application_logs_encryption.target_key_arn
   timeout     = 900
