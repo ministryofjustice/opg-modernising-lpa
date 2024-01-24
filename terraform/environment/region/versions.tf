0df0fd064e3dabdede5b3a0e6e0a6fa7b87d528c
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-01-24 22:31:36 +0000

    fix linting issue

--- terraform/environment/region/versions.tf
+++ terraform/environment/region/versions.tf
@@ -3,8 +3,8 @@ terraform {
 
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      # version = "~> 5.32.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.32.0"
       configuration_aliases = [
         aws.region,
         aws.global,
