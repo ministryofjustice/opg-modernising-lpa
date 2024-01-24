9982aff7969a9ac5b63dfe67ca37b5729242039b
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-01-24 19:10:58 +0000

    create dash and document how to check them

--- terraform/environment/region/dashboard.tf
+++ terraform/environment/region/dashboard.tf
@@ -1,10 +1,5 @@
-# __generated__ by Terraform
-# Please review these resources and move them into your main configuration files.
-
-# __generated__ by Terraform from "health-checks-demo-environment"
-
-resource "aws_cloudwatch_dashboard" "health_check" {
-  provider = aws.global
+resource "aws_cloudwatch_dashboard" "health_checks" {
+  provider = aws.region
   dashboard_body = jsonencode({
     widgets = [
       {
@@ -25,6 +20,26 @@ resource "aws_cloudwatch_dashboard" "health_check" {
           end    = "P0D",
           period = 300,
           title  = "service health-check - average uptime of the service over 12 month window"
+        }
+      },
+      {
+        type   = "metric"
+        x      = 0
+        y      = 6
+        width  = 12
+        height = 5
+
+        properties = {
+          sparkline = true,
+          view      = "singleValue",
+          metrics = [
+            ["AWS/Route53", "HealthCheckPercentageHealthy", "HealthCheckId", aws_route53_health_check.dependency_health_check.id, { region = "us-east-1" }]
+          ],
+          region = "us-east-1",
+          start  = "-PT8640H",
+          end    = "P0D",
+          period = 300,
+          title  = "dependency health-check - average availability of service dependencies over 12 month window"
         }
       }
     ]
