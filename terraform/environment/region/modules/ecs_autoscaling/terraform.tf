6fd365f96894c9478aac210adf3be998d3629737
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-01-24 20:06:25 +0000

    remove unused declarations

--- terraform/environment/region/modules/ecs_autoscaling/terraform.tf
+++ terraform/environment/region/modules/ecs_autoscaling/terraform.tf
@@ -10,15 +10,3 @@ terraform {
     }
   }
 }
-
-data "aws_region" "current" {
-  provider = aws.region
-}
-
-data "aws_caller_identity" "current" {
-  provider = aws.region
-}
-
-data "aws_default_tags" "current" {
-  provider = aws.region
-}
