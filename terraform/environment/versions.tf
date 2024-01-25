c8c98628638706d287d8938ae1f04f9402fa7ee3
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-01-25 13:32:53 +0000

    pin provider

--- terraform/environment/versions.tf
+++ terraform/environment/versions.tf
@@ -6,7 +6,7 @@ terraform {
     }
     pagerduty = {
       source  = "PagerDuty/pagerduty"
-      version = "~> 3.5.0"
+      version = "= 3.4.1"
     }
     local = {
       source = "hashicorp/local"
