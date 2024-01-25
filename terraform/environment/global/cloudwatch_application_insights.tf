6d86c7200c55bd9d60477bc39aa0cc8bc47ae67c
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-01-25 12:33:51 +0000

    enable app insights

--- terraform/environment/global/cloudwatch_application_insights.tf
+++ terraform/environment/global/cloudwatch_application_insights.tf
@@ -1,6 +1,6 @@
-# resource "aws_applicationinsights_application" "environment_global" {
-#   resource_group_name = aws_resourcegroups_group.environment_global.name
-#   auto_config_enabled = true
-#   cwe_monitor_enabled = true
-#   provider            = aws.global
-# }
+resource "aws_applicationinsights_application" "environment_global" {
+  resource_group_name = aws_resourcegroups_group.environment_global.name
+  auto_config_enabled = true
+  cwe_monitor_enabled = true
+  provider            = aws.global
+}
