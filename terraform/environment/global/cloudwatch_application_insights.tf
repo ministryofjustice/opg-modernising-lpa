2c8f07983cc8ca4a990f336da5031fd0317cd9d2
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-01-25 16:31:53 +0000

    MLPAB-1062 - Enable app insights (#989)
    * enable app insights
    
    * enable autoconfigure
    
    * upgrade provider
    
    * per env setup for app insights
    
    * per env setup for app insights
    
    * only run app insights on long lived envs
    
    * remove comment

--- terraform/environment/global/cloudwatch_application_insights.tf
+++ terraform/environment/global/cloudwatch_application_insights.tf
@@ -1,6 +1,7 @@
-# resource "aws_applicationinsights_application" "environment_global" {
-#   resource_group_name = aws_resourcegroups_group.environment_global.name
-#   auto_config_enabled = true
-#   cwe_monitor_enabled = true
-#   provider            = aws.global
-# }
+resource "aws_applicationinsights_application" "environment_global" {
+  count               = var.cloudwatch_application_insights_enabled ? 1 : 0
+  resource_group_name = aws_resourcegroups_group.environment_global.name
+  auto_config_enabled = true
+  cwe_monitor_enabled = true
+  provider            = aws.global
+}
