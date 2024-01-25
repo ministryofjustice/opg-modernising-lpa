7689e93c397bae1ab67a633242b34b3e68e7fbae
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-01-25 12:34:47 +0000

    enable autoconfigure

--- terraform/environment/region/cloudwatch_application_insights.tf
+++ terraform/environment/region/cloudwatch_application_insights.tf
@@ -1,6 +1,6 @@
 resource "aws_applicationinsights_application" "environment" {
   resource_group_name = aws_resourcegroups_group.environment.name
-  auto_config_enabled = false # temporarily disabled until the bug int he provider is resolved https://github.com/hashicorp/terraform-provider-aws/issues/27277
+  auto_config_enabled = true # temporarily disabled until the bug int he provider is resolved https://github.com/hashicorp/terraform-provider-aws/issues/27277
   cwe_monitor_enabled = true
   ops_center_enabled  = false
   depends_on = [
