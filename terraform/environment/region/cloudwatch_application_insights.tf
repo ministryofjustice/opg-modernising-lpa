00a274849049c909e9281206cf4fc553c8c75272
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-01-25 16:12:44 +0000

    remove comment

--- terraform/environment/region/cloudwatch_application_insights.tf
+++ terraform/environment/region/cloudwatch_application_insights.tf
@@ -1,7 +1,7 @@
 resource "aws_applicationinsights_application" "environment" {
   count               = var.cloudwatch_application_insights_enabled ? 1 : 0
   resource_group_name = aws_resourcegroups_group.environment.name
-  auto_config_enabled = true # temporarily disabled until the bug int he provider is resolved https://github.com/hashicorp/terraform-provider-aws/issues/27277
+  auto_config_enabled = true
   cwe_monitor_enabled = true
   ops_center_enabled  = false
   depends_on = [
