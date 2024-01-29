5f21e33f3543be92abc85cbfc0251a579aa85e46
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-01-29 17:59:14 +0000

    add opsitems notifications

--- terraform/environment/region/cloudwatch_application_insights.tf
+++ terraform/environment/region/cloudwatch_application_insights.tf
@@ -1,9 +1,11 @@
+
 resource "aws_applicationinsights_application" "environment" {
-  count               = var.cloudwatch_application_insights_enabled ? 1 : 0
-  resource_group_name = aws_resourcegroups_group.environment.name
-  auto_config_enabled = true
-  cwe_monitor_enabled = true
-  ops_center_enabled  = false
+  count                  = var.cloudwatch_application_insights_enabled ? 1 : 0
+  resource_group_name    = aws_resourcegroups_group.environment.name
+  auto_config_enabled    = true
+  cwe_monitor_enabled    = true
+  ops_center_enabled     = true
+  ops_item_sns_topic_arn = data.aws_sns_topic.cloudwatch_application_insights.arn
   depends_on = [
     aws_ecs_cluster.main,
     module.app,
--- terraform/environment/region/pagerduty.tf
+++ terraform/environment/region/pagerduty.tf
@@ -19,3 +19,21 @@ resource "aws_sns_topic_subscription" "ecs_autoscaling_alarms" {
   endpoint               = "https://events.pagerduty.com/integration/${pagerduty_service_integration.ecs_autoscaling_alarms.integration_key}/enqueue"
   provider               = aws.region
 }
+
+data "aws_sns_topic" "cloudwatch_application_insights" {
+  name = "cloudwatch_application_insights"
+}
+
+resource "pagerduty_service_integration" "cloudwatch_application_insights" {
+  name    = "Modernising LPA ${data.aws_default_tags.current.tags.environment-name} ${data.aws_region.current.name} Cloudwatch Application Insights Ops Item Alarm"
+  service = data.pagerduty_service.main.id
+  vendor  = data.pagerduty_vendor.cloudwatch.id
+}
+
+resource "aws_sns_topic_subscription" "cloudwatch_application_insights" {
+  topic_arn              = data.aws_sns_topic.cloudwatch_application_insights.arn
+  protocol               = "https"
+  endpoint_auto_confirms = true
+  endpoint               = "https://events.pagerduty.com/integration/${pagerduty_service_integration.ecs_autoscaling_alarms.integration_key}/enqueue"
+  provider               = aws.region
+}
