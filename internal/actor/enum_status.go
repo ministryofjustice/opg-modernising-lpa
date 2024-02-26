83911f1f1ce3b78d6c78821439a9855e011d7d87
Alex Saunders <acsauk@gmail.com>
2024-02-26 16:19:34 +0000

    update status enum

--- internal/actor/enum_status.go
+++ internal/actor/enum_status.go
@@ -10,8 +10,8 @@ func _() {
 	// An "invalid array index" compiler error signifies that the constant values have changed.
 	// Re-run the stringer command to generate them again.
 	var x [1]struct{}
-	_ = x[Active-1]
-	_ = x[Suspended-2]
+	_ = x[Active-0]
+	_ = x[Suspended-1]
 }
 
 const _Status_name = "activesuspended"
@@ -19,9 +19,8 @@ const _Status_name = "activesuspended"
 var _Status_index = [...]uint8{0, 6, 15}
 
 func (i Status) String() string {
-	i -= 1
 	if i >= Status(len(_Status_index)-1) {
-		return "Status(" + strconv.FormatInt(int64(i+1), 10) + ")"
+		return "Status(" + strconv.FormatInt(int64(i), 10) + ")"
 	}
 	return _Status_name[_Status_index[i]:_Status_index[i+1]]
 }
