4d886dfe317b4dc45a0467027680c4f85f6eb30c
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-03-14 19:32:56 +0000

    ignore tfsec for this log group

--- terraform/environment/region/events_cloudwatch_log_group.tf
+++ terraform/environment/region/events_cloudwatch_log_group.tf
@@ -1,6 +1,6 @@
+#tfsec:ignore:aws-cloudwatch-log-group-customer-key
 resource "aws_cloudwatch_log_group" "events" {
   name              = "${data.aws_default_tags.current.tags.environment-name}-events"
   retention_in_days = 1
-  # kms_key_id        = data.aws_kms_alias.cloudwatch_application_logs_encryption.target_key_arn
-  provider = aws.region
+  provider          = aws.region
 }
