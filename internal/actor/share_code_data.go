02cbbae026677661a02a7f019e239a320220a0e9
Joshua Hawxwell <m@hawx.me>
2024-03-14 10:44:00 +0000

    Add page to invite donor to supporter made LPA

--- internal/actor/share_code_data.go
+++ internal/actor/share_code_data.go
@@ -1,12 +1,20 @@
 package actor
 
-import "github.com/ministryofjustice/opg-modernising-lpa/internal/actor/actoruid"
+import (
+	"time"
+
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/actor/actoruid"
+)
 
 type ShareCodeData struct {
 	PK, SK                string
+	UpdatedAt             time.Time
 	SessionID             string
 	LpaID                 string
 	ActorUID              actoruid.UID
 	IsReplacementAttorney bool
 	IsTrustCorporation    bool
+
+	// InviteSentTo is the email address the supporter sent the invite to
+	InviteSentTo string
 }
