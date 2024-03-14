47731dd86e603a9c1b09c1b1fb284e05cd33cc2c
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-03-14 21:19:09 +0000

    MLPAB-1999 - enable ECS deployment circuit breaker and rollback on failure (#1122)
    * enable circuit breaker and rollback on failure
    
    * create event rule for failed deployment
    
    * send events to log group
    
    * create encrypted SNS topic

--- terraform/environment/region/pagerduty.tf
+++ terraform/environment/region/pagerduty.tf
@@ -40,3 +40,38 @@ resource "aws_sns_topic_subscription" "cloudwatch_application_insights" {
   endpoint               = "https://events.pagerduty.com/integration/${pagerduty_service_integration.ecs_autoscaling_alarms.integration_key}/enqueue"
   provider               = aws.region
 }
+
+resource "aws_sns_topic" "event_alarms" {
+  name                                     = "${data.aws_default_tags.current.tags.environment-name}-event-alarms"
+  kms_master_key_id                        = data.aws_kms_alias.sns_kms_key_alias_global.target_key_id
+  application_failure_feedback_role_arn    = data.aws_iam_role.sns_failure_feedback.arn
+  application_success_feedback_role_arn    = data.aws_iam_role.sns_success_feedback.arn
+  application_success_feedback_sample_rate = 100
+  firehose_failure_feedback_role_arn       = data.aws_iam_role.sns_failure_feedback.arn
+  firehose_success_feedback_role_arn       = data.aws_iam_role.sns_success_feedback.arn
+  firehose_success_feedback_sample_rate    = 100
+  http_failure_feedback_role_arn           = data.aws_iam_role.sns_failure_feedback.arn
+  http_success_feedback_role_arn           = data.aws_iam_role.sns_success_feedback.arn
+  http_success_feedback_sample_rate        = 100
+  lambda_failure_feedback_role_arn         = data.aws_iam_role.sns_failure_feedback.arn
+  lambda_success_feedback_role_arn         = data.aws_iam_role.sns_success_feedback.arn
+  lambda_success_feedback_sample_rate      = 100
+  sqs_failure_feedback_role_arn            = data.aws_iam_role.sns_failure_feedback.arn
+  sqs_success_feedback_role_arn            = data.aws_iam_role.sns_success_feedback.arn
+  sqs_success_feedback_sample_rate         = 100
+  provider                                 = aws.region
+}
+
+resource "pagerduty_service_integration" "event_alarms" {
+  name    = "Modernising LPA ${data.aws_default_tags.current.tags.environment-name} ${data.aws_region.current.name} Eventbridge Event Alarm"
+  service = data.pagerduty_service.main.id
+  vendor  = data.pagerduty_vendor.cloudwatch.id
+}
+
+resource "aws_sns_topic_subscription" "event_alarms" {
+  topic_arn              = aws_sns_topic.event_alarms.arn
+  protocol               = "https"
+  endpoint_auto_confirms = true
+  endpoint               = "https://events.pagerduty.com/integration/${pagerduty_service_integration.ecs_autoscaling_alarms.integration_key}/enqueue"
+  provider               = aws.region
+}
