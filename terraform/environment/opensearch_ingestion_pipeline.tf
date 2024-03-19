90b731683427f125335a85b3c1256b32a9fc27a0
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-03-19 12:59:27 +0000

    create a pipeline with supporter route

--- terraform/environment/opensearch_ingestion_pipeline.tf
+++ terraform/environment/opensearch_ingestion_pipeline.tf
@@ -9,6 +9,16 @@ data "aws_iam_policy_document" "opensearch_ingestion" {
   version = "2012-10-17"
 
   statement {
+    sid    = "WorkWithCollection"
+    effect = "Allow"
+    actions = [
+      "aoss:BatchGetCollection",
+    ]
+    resources = [
+      aws_opensearchserverless_collection.lpas_collection.arn,
+    ]
+  }
+  statement {
     sid    = "allowRunExportJob"
     effect = "Allow"
     actions = [
@@ -68,6 +78,22 @@ data "aws_vpc" "main" {
   provider = aws.eu_west_1
 }
 
+data "aws_availability_zones" "available" {
+  provider = aws.eu_west_1
+}
+
+data "aws_subnet" "application" {
+  count             = 3
+  vpc_id            = data.aws_vpc.main.id
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  filter {
+    name   = "tag:Name"
+    values = ["application*"]
+  }
+  provider = aws.eu_west_1
+}
+
 resource "aws_security_group" "opensearch_ingestion" {
   name_prefix = "${local.default_tags.environment-name}-opensearch-ingestion"
   vpc_id      = data.aws_vpc.main.id
@@ -82,5 +108,64 @@ resource "aws_cloudwatch_log_group" "opensearch_ingestion" {
 
 resource "aws_s3_bucket" "opensearch_ingestion" {
   bucket   = "${local.default_tags.environment-name}-opensearch-ingestion"
+  provider = aws.eu_west_1
+}
+
+locals {
+  pipeline_configuration_tempalte_vars = {
+    source = {
+      tables = {
+        table_arn = aws_dynamodb_table.lpas_table.arn
+        stream = {
+          start_position = "LATEST"
+        }
+        export = {
+          s3_bucket = aws_s3_bucket.opensearch_ingestion.bucket
+          s3_region = "eu-west-1"
+          s3_prefix = "${local.default_tags.environment-name}/ddb-to-opensearch-export/"
+        }
+        aws = {
+          sts_role_arn = module.global.iam_roles.opensearch_ingestion.arn
+          region       = "eu-west-1"
+        }
+      }
+    }
+    supporter_lpas = {
+      route = "'contains(/SK, \"ORGANISATION#\")'"
+      sink = {
+        opensearch = {
+          hosts = aws_opensearchserverless_collection.lpas_collection.collection_endpoint
+          index = "lpas2"
+          aws = {
+            sts_role_arn = module.global.iam_roles.opensearch_ingestion.arn
+            region       = "eu-west-1"
+            serverless_options = {
+              network_policy_name = aws_opensearchserverless_security_policy.lpas_collection_network_policy.name
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "aws_osis_pipeline" "example" {
+  pipeline_name               = "lpas-${local.default_tags.environment-name}"
+  max_units                   = 1
+  min_units                   = 1
+  pipeline_configuration_body = templatefile("opensearch_pipeline/pipeline_configuration.yaml.tftpl", local.pipeline_configuration_tempalte_vars)
+  buffer_options {
+    persistent_buffer_enabled = false
+  }
+  log_publishing_options {
+    cloudwatch_log_destination {
+      log_group = aws_cloudwatch_log_group.opensearch_ingestion.name
+    }
+    is_logging_enabled = true
+  }
+  vpc_options {
+    security_group_ids = [aws_security_group.opensearch_ingestion.id]
+    subnet_ids         = data.aws_subnet.application[*].id
+  }
   provider = aws.eu_west_1
 }
