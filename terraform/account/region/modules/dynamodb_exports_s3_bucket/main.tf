1a8dd04140e787a272273a162f21cad4328bdd3f
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-03-27 11:31:50 +0000

    don't use cmk yet

--- terraform/account/region/modules/dynamodb_exports_s3_bucket/main.tf
+++ terraform/account/region/modules/dynamodb_exports_s3_bucket/main.tf
@@ -27,8 +27,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption
 
   rule {
     apply_server_side_encryption_by_default {
-      kms_master_key_id = var.s3_bucket_server_side_encryption_key_id
-      sse_algorithm     = "aws:kms"
+      sse_algorithm = "AES256"
     }
   }
   provider = aws.region
