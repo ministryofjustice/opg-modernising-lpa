6981d7418f020d4224888784d060968b0b0dcccc
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-03-28 17:28:15 +0000

    use s3 access logging bucket

--- terraform/account/region/dynamodb_exports_s3_bucket.tf
+++ terraform/account/region/dynamodb_exports_s3_bucket.tf
@@ -1,7 +1,6 @@
 module "dynamodb_exports_s3_bucket" {
   source = "./modules/dynamodb_exports_s3_bucket"
   # s3_bucket_server_side_encryption_key_id = var.dynamodb_exports_s3_bucket_server_side_encryption_key_id
-  s3_bucket_logging_target_bucket_id = aws_s3_bucket.access_log.id
   providers = {
     aws.region = aws.region
   }
--- terraform/account/region/modules/dynamodb_exports_s3_bucket/variables.tf
+++ terraform/account/region/modules/dynamodb_exports_s3_bucket/variables.tf
@@ -1,8 +1,3 @@
-variable "s3_bucket_logging_target_bucket_id" {
-  description = "The ID of the S3 bucket to which the access logs should be delivered."
-  type        = string
-}
-
 # variable "s3_bucket_server_side_encryption_key_id" {
 #   description = "The ID of the KMS key to use for server-side encryption of the bucket."
 #   type        = string
