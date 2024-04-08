f7fcfa0c08a84f079e164a691184ea19de7d029e
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-04-08 10:44:35 +0100

    use s3 access logging bucket (#1161)

--- terraform/account/region/modules/dynamodb_exports_s3_bucket/data_sources.tf
+++ terraform/account/region/modules/dynamodb_exports_s3_bucket/data_sources.tf
@@ -5,3 +5,8 @@ data "aws_region" "current" {
 data "aws_default_tags" "current" {
   provider = aws.region
 }
+
+data "aws_s3_bucket" "access_logging" {
+  bucket   = "s3-access-logs-${data.aws_default_tags.current.tags.application}-${data.aws_default_tags.current.tags.account-name}-${data.aws_region.current.name}"
+  provider = aws.region
+}
