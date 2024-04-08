ae778778acf4c3ea49017b505d5ee71638073c65
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-04-08 13:37:43 +0100

    cleanup

--- terraform/environment/global/iam_opensearch_ingestion_role.tf
+++ terraform/environment/global/iam_opensearch_ingestion_role.tf
@@ -5,16 +5,6 @@ resource "aws_iam_role" "opensearch_pipeline" {
 }
 
 data "aws_iam_policy_document" "opensearch_pipeline" {
-  # statement {
-  #   effect  = "Allow"
-  #   actions = ["sts:AssumeRole"]
-
-  #   principals {
-  #     identifiers = ["arn:aws:iam::${data.aws_caller_identity.global.account_id}:root"]
-  #     type        = "AWS"
-  #   }
-  # }
-
   statement {
     effect  = "Allow"
     actions = ["sts:AssumeRole"]
