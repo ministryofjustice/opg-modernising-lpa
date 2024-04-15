f87530e446d90c389ff011e4ff2f13aaa0963771
Joshua Hawxwell <m@hawx.me>
2024-04-15 10:48:33 +0100

    Skip the test to see if the next one breaks

--- cypress/e2e/attorney/progress.cy.js
+++ cypress/e2e/attorney/progress.cy.js
@@ -1,28 +1,28 @@
 describe('Progress', () => {
-    it('when nothing completed', () => {
-        cy.visit('/fixtures/attorney?redirect=/progress');
-        cy.checkA11yApp();
+  it('when nothing completed', () => {
+    cy.visit('/fixtures/attorney?redirect=/progress');
+    cy.checkA11yApp();
 
-        cy.contains('h1', 'Sam Smith’s property and affairs LPA');
-        cy.contains('li', 'You’ve signed the LPA In progress');
-        cy.contains('li', 'All attorneys have signed the LPA Not started');
-    });
+    cy.contains('h1', 'Sam Smith’s property and affairs LPA');
+    cy.contains('li', 'You’ve signed the LPA In progress');
+    cy.contains('li', 'All attorneys have signed the LPA Not started');
+  });
 
-    it('when signed', () => {
-        cy.visit('/fixtures/attorney?redirect=/progress&progress=signedByAttorney');
-        cy.checkA11yApp();
+  it('when signed', () => {
+    cy.visit('/fixtures/attorney?redirect=/progress&progress=signedByAttorney');
+    cy.checkA11yApp();
 
-        cy.contains('h1', 'Sam Smith’s property and affairs LPA');
-        cy.contains('li', 'You’ve signed the LPA Completed');
-        cy.contains('li', 'All attorneys have signed the LPA In progress');
-    });
+    cy.contains('h1', 'Sam Smith’s property and affairs LPA');
+    cy.contains('li', 'You’ve signed the LPA Completed');
+    cy.contains('li', 'All attorneys have signed the LPA In progress');
+  });
 
-    it('when all signed', () => {
-        cy.visit('/fixtures/attorney?redirect=/progress&progress=signedByAllAttorneys');
-        cy.checkA11yApp();
+  it('when all signed', () => {
+    cy.visit('/fixtures/attorney?redirect=/progress&progress=signedByAllAttorneys');
+    cy.checkA11yApp();
 
-        cy.contains('h1', 'Sam Smith’s property and affairs LPA');
-        cy.contains('li', 'You’ve signed the LPA Completed');
-        cy.contains('li', 'All attorneys have signed the LPA Completed');
-    });
+    cy.contains('h1', 'Sam Smith’s property and affairs LPA');
+    cy.contains('li', 'You’ve signed the LPA Completed');
+    cy.contains('li', 'All attorneys have signed the LPA Completed');
+  });
 });
