54a56c162315f1b14abedc92c893e17d92463f57
Alex Saunders <acsauk@gmail.com>
2024-04-19 11:34:00 +0100

    cover e2e

--- cypress/e2e/donor/lpa-progress.cy.js
+++ cypress/e2e/donor/lpa-progress.cy.js
@@ -33,4 +33,19 @@ describe('LPA progress', () => {
         cy.contains('li', 'Your 4-week waiting period has started Not started');
         cy.contains('li', 'Your LPA has been registered Not started');
     })
+
+    it('when registered', () => {
+        cy.visit('/fixtures?redirect=/progress&progress=registered');
+        cy.checkA11yApp();
+
+        cy.contains('li', 'You’ve signed your LPA Completed');
+        cy.contains('li', 'Charlie Cooper has provided their certificate Completed');
+        cy.contains('li', 'Your attorneys have signed your LPA Completed');
+        cy.contains('li', 'We have received your LPA Completed');
+        cy.contains('li', 'Your 4-week waiting period has started Completed');
+        cy.contains('li', 'Your LPA has been registered Completed');
+
+        const today = new Date().toLocaleDateString('en-uk', { year:"numeric", month:"long", day: 'numeric'})
+        cy.contains('li', `We sent an email on ${today} about about your LPA registration`);
+    })
 });
