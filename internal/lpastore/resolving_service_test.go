497e736702a5d46defb9c363053b762a6f801074
Joshua Hawxwell <m@hawx.me>
2024-04-22 14:12:32 +0100

    Use getlist lpa-store lambda for dashboard data

--- internal/lpastore/resolving_service_test.go
+++ internal/lpastore/resolving_service_test.go
@@ -148,6 +148,24 @@ func TestResolvingServiceGet(t *testing.T) {
 	}
 }
 
+func TestResolvingServiceGetWhenNoUID(t *testing.T) {
+	ctx := context.Background()
+
+	donorStore := newMockDonorStore(t)
+	donorStore.EXPECT().
+		GetAny(ctx).
+		Return(&actor.DonorProvidedDetails{LpaID: "1"}, nil)
+
+	service := NewResolvingService(donorStore, nil)
+	lpa, err := service.Get(ctx)
+
+	assert.Equal(t, &Lpa{
+		LpaID: "1",
+		Donor: actor.Donor{Channel: actor.ChannelOnline},
+	}, lpa)
+	assert.Nil(t, err)
+}
+
 func TestResolvingServiceGetWhenNotFound(t *testing.T) {
 	ctx := context.Background()
 
@@ -192,7 +210,7 @@ func TestResolvingServiceGetWhenLpaClientErrors(t *testing.T) {
 	donorStore := newMockDonorStore(t)
 	donorStore.EXPECT().
 		GetAny(ctx).
-		Return(&actor.DonorProvidedDetails{}, nil)
+		Return(&actor.DonorProvidedDetails{LpaUID: "M-1111"}, nil)
 
 	lpaClient := newMockLpaClient(t)
 	lpaClient.EXPECT().
@@ -203,4 +221,140 @@ func TestResolvingServiceGetWhenLpaClientErrors(t *testing.T) {
 	_, err := service.Get(ctx)
 
 	assert.Equal(t, expectedError, err)
+}
+
+func TestResolvingServiceResolveList(t *testing.T) {
+	testcases := map[string]struct {
+		donors   []*actor.DonorProvidedDetails
+		uids     []string
+		resolved []*Lpa
+		error    error
+		expected []*Lpa
+	}{
+		"online with all true": {
+			donors: []*actor.DonorProvidedDetails{{
+				SK:          dynamo.OrganisationKey("S"),
+				LpaID:       "1",
+				LpaUID:      "M-1111",
+				SubmittedAt: time.Now(),
+				CertificateProvider: actor.CertificateProvider{
+					FirstNames:   "Barry",
+					Relationship: actor.Personally,
+				},
+				Tasks: actor.DonorTasks{
+					PayForLpa: actor.PaymentTaskCompleted,
+				},
+				DonorIdentityUserData: identity.UserData{
+					OK: true,
+				},
+			}},
+			uids: []string{"M-1111"},
+			resolved: []*Lpa{{
+				LpaID:  "1",
+				LpaUID: "M-1111",
+				CertificateProvider: CertificateProvider{
+					FirstNames: "Paul",
+				},
+			}},
+			expected: []*Lpa{{
+				LpaID:                  "1",
+				LpaUID:                 "M-1111",
+				DonorIdentityConfirmed: true,
+				Submitted:              true,
+				Paid:                   true,
+				IsOrganisationDonor:    true,
+				CertificateProvider: CertificateProvider{
+					FirstNames:   "Paul",
+					Relationship: actor.Personally,
+				},
+				Donor: actor.Donor{Channel: actor.ChannelOnline},
+			}},
+		},
+		"online with no lpastore record": {
+			donors: []*actor.DonorProvidedDetails{{
+				SK:     dynamo.DonorKey("S"),
+				LpaUID: "M-1111",
+				CertificateProvider: actor.CertificateProvider{
+					FirstNames:   "John",
+					Relationship: actor.Personally,
+				},
+				Donor: actor.Donor{Channel: actor.ChannelOnline},
+				Attorneys: actor.Attorneys{
+					Attorneys:        []actor.Attorney{{FirstNames: "a"}},
+					TrustCorporation: actor.TrustCorporation{Name: "b"},
+				},
+				ReplacementAttorneys: actor.Attorneys{
+					Attorneys:        []actor.Attorney{{FirstNames: "c"}},
+					TrustCorporation: actor.TrustCorporation{Name: "d"},
+				},
+			}},
+			uids: []string{"M-1111"},
+			expected: []*Lpa{{
+				LpaUID: "M-1111",
+				CertificateProvider: CertificateProvider{
+					FirstNames:   "John",
+					Relationship: actor.Personally,
+				},
+				Donor: actor.Donor{Channel: actor.ChannelOnline},
+				Attorneys: Attorneys{
+					Attorneys:        []Attorney{{FirstNames: "a"}},
+					TrustCorporation: TrustCorporation{Name: "b"},
+				},
+				ReplacementAttorneys: Attorneys{
+					Attorneys:        []Attorney{{FirstNames: "c"}},
+					TrustCorporation: TrustCorporation{Name: "d"},
+				},
+			}},
+		},
+		"online with all false": {
+			donors: []*actor.DonorProvidedDetails{{
+				SK:     dynamo.DonorKey("S"),
+				LpaID:  "1",
+				LpaUID: "M-1111",
+			}},
+			uids:     []string{"M-1111"},
+			resolved: []*Lpa{{LpaID: "1", LpaUID: "M-1111"}},
+			expected: []*Lpa{{
+				LpaID:  "1",
+				LpaUID: "M-1111",
+				Donor:  actor.Donor{Channel: actor.ChannelOnline},
+			}},
+		},
+		"paper": {
+			donors: []*actor.DonorProvidedDetails{{
+				SK:     dynamo.DonorKey("PAPER"),
+				LpaID:  "1",
+				LpaUID: "M-1111",
+			}},
+			uids:     []string{"M-1111"},
+			resolved: []*Lpa{{LpaID: "1", LpaUID: "M-1111"}},
+			expected: []*Lpa{{
+				LpaID:     "1",
+				LpaUID:    "M-1111",
+				Submitted: true,
+				Paid:      true,
+				CertificateProvider: CertificateProvider{
+					Relationship: actor.Professionally,
+				},
+				Donor: actor.Donor{Channel: actor.ChannelPaper},
+			}},
+		},
+	}
+
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			ctx := context.Background()
+
+			lpaClient := newMockLpaClient(t)
+			lpaClient.EXPECT().
+				Lpas(ctx, tc.uids).
+				Return(tc.resolved, tc.error)
+
+			service := NewResolvingService(nil, lpaClient)
+			lpas, err := service.ResolveList(ctx, tc.donors)
+
+			assert.Nil(t, err)
+			assert.Equal(t, tc.expected, lpas)
+		})
+	}
 }
