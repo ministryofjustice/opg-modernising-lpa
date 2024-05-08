c7314a6bf9f4b37740bb0bce9b96391143936250
Joshua Hawxwell <m@hawx.me>
2024-05-08 09:55:39 +0100

    Add preproduction smoke test for onelogin

--- cypress/smoke/smoke.cy.js
+++ cypress/smoke/smoke.cy.js
@@ -1,20 +1,51 @@
+const { TOTP } = require("totp-generator");
+
 describe('Smoke tests', () => {
-    describe('external dependencies', () => {
-        describe('UID service', () => {
-            it('request signing and base URL are configured correctly', () => {
-                cy.request('/health-check/dependency').should((response) => {
-                    expect(response.status).not.to.eq(403)
-                })
-            })
+  describe('external dependencies', () => {
+    describe('UID service', () => {
+      it('request signing and base URL are configured correctly', () => {
+        cy.request('/health-check/dependency').should((response) => {
+          expect(response.status).not.to.eq(403)
         })
+      })
     })
+  })
 
-    describe('app', () => {
-        it('is available', () => {
-            cy.visit('/')
+  describe('app', () => {
+    it('is available', () => {
+      cy.visit('/')
 
-            cy.get('h1').should('contain', 'Make and register a lasting power of attorney (LPA)');
-            cy.contains('a', 'Start');
-        })
+      cy.get('h1').should('contain', 'Make and register a lasting power of attorney (LPA)');
+
+      cy.intercept('https://signin.integration.account.gov.uk/**', (req) => {
+        req.headers['Authorization'] = 'Basic aW50ZWdyYXRpb24tdXNlcjp3aW50ZXIyMDIx';
+      });
+
+      cy.contains('a', 'Start').click();
+
+      if (Cypress.config().baseUrl.includes('localhost')) {
+        cy.url().should('contain', '/authorize')
+      } else if (Cypress.config().baseUrl.includes('preproduction')) {
+        const { otp } = TOTP.generate(Cypress.env('ONELOGIN_TOTP_KEY'));
+
+        cy.origin('https://signin.integration.account.gov.uk', { args: { token: otp } }, ({ token }) => {
+          cy.url().should('contain', '/sign-in-or-create')
+
+          cy.contains('Sign in').click();
+          cy.get('[type=email]').type('opgteam+modernising-lpa@digital.justice.gov.uk');
+          cy.get('form').submit();
+          cy.get('[type=password]').type(Cypress.env('ONELOGIN_PASSWORD'));
+          cy.get('form').submit();
+
+          cy.get('[name=code]').type(token);
+          cy.contains('button', 'Continue').click();
+        });
+
+        cy.origin('https://preproduction.app.modernising.opg.service.justice.gov.uk', () => {
+          cy.url().should('contain', '/dashboard');
+          cy.contains('Manage your LPAs');
+        });
+      }
     })
+  })
 })
