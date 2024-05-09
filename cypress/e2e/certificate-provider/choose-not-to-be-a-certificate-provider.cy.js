3f8ef091a096b24a1a7078c26b63b4ad8caaee2f
Alex Saunders <acsauk@gmail.com>
2024-05-09 17:55:32 +0100

    cover both routes in e2e

--- cypress/e2e/certificate-provider/choose-not-to-be-a-certificate-provider.cy.js
+++ cypress/e2e/certificate-provider/choose-not-to-be-a-certificate-provider.cy.js
@@ -1,25 +1,51 @@
 const {randomShareCode, TestEmail} = require("../../support/e2e");
 
 describe('Choose not to be a certificate provider', () => {
-    it('can enter reference number to not be a certificate provider', () => {
-        const shareCode = randomShareCode()
-        cy.visit(`/fixtures/certificate-provider?redirect=/certificate-provider-enter-reference-number-opt-out&withShareCode=${shareCode}&email=${TestEmail}&progress=signedByDonor`)
+    describe('when LPA has been signed and witnessed', () => {
+        it('can enter reference number to not be a certificate provider', () => {
+            const shareCode = randomShareCode()
+            cy.visit(`/fixtures/certificate-provider?redirect=/certificate-provider-enter-reference-number-opt-out&withShareCode=${shareCode}&email=${TestEmail}&progress=signedByDonor`)
 
-        cy.checkA11yApp();
+            cy.checkA11yApp();
 
-        cy.get('#f-reference-number').type(shareCode);
-        cy.contains('Continue').click();
+            cy.get('#f-reference-number').type(shareCode);
+            cy.contains('Continue').click();
 
-        cy.url().should('contain', '/confirm-you-do-not-want-to-be-a-certificate-provider')
-        cy.checkA11yApp();
+            cy.url().should('contain', '/confirm-you-do-not-want-to-be-a-certificate-provider')
+            cy.checkA11yApp();
 
-        cy.contains('Property and affairs')
+            cy.contains('Property and affairs')
 
-        cy.contains('Confirm').click();
+            cy.contains('button', 'Confirm').click();
 
-        cy.url().should('contain', '/you-have-decided-not-to-be-a-certificate-provider')
-        cy.checkA11yApp();
+            cy.url().should('contain', '/you-have-decided-not-to-be-a-certificate-provider')
+            cy.checkA11yApp();
 
-        cy.contains('We have contacted Sam Smith')
-    });
+            cy.contains('We have contacted Sam Smith')
+        });
+    })
+
+    describe('when LPA has not been signed and witnessed', () => {
+        it('can enter reference number to not be a certificate provider', () => {
+            const shareCode = randomShareCode()
+            cy.visit(`/fixtures/certificate-provider?redirect=/certificate-provider-enter-reference-number-opt-out&withShareCode=${shareCode}&email=${TestEmail}`)
+
+            cy.checkA11yApp();
+
+            cy.get('#f-reference-number').type(shareCode);
+            cy.contains('Continue').click();
+
+            cy.url().should('contain', '/confirm-you-do-not-want-to-be-a-certificate-provider')
+            cy.checkA11yApp();
+
+            cy.contains('Property and affairs')
+
+            cy.contains('button', 'Confirm').click();
+
+            cy.url().should('contain', '/you-have-decided-not-to-be-a-certificate-provider')
+            cy.checkA11yApp();
+
+            cy.contains('We have contacted Sam Smith')
+        });
+    })
 })
