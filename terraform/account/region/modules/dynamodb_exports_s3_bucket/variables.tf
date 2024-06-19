eb2c87108c77829c507f0a64c14e6c5a3fbe6e5d
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-06-19 11:01:31 +0100

    add var for kms key

--- terraform/account/region/modules/dynamodb_exports_s3_bucket/variables.tf
+++ terraform/account/region/modules/dynamodb_exports_s3_bucket/variables.tf
@@ -1,4 +1,4 @@
-# variable "s3_bucket_server_side_encryption_key_id" {
-#   description = "The ID of the KMS key to use for server-side encryption of the bucket."
-#   type        = string
-# }
+variable "s3_bucket_server_side_encryption_key_id" {
+  description = "The ID of the KMS key to use for server-side encryption of the bucket."
+  type        = string
+}
