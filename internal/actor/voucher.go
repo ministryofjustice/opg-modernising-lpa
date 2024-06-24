42f35fbcb276673c796d119404cea23760c3d416
Joshua Hawxwell <m@hawx.me>
2024-06-24 15:09:15 +0100

    Add page to confirm voucher is different person

--- internal/actor/voucher.go
+++ internal/actor/voucher.go
@@ -1,11 +1,59 @@
 package actor
 
+import "strings"
+
 type Voucher struct {
 	FirstNames string
 	LastName   string
 	Email      string
+	Allowed    bool
 }
 
 func (v Voucher) FullName() string {
 	return v.FirstNames + " " + v.LastName
+}
+
+func (v Voucher) Matches(donor *DonorProvidedDetails) (match []Type) {
+	if v.FirstNames == "" && v.LastName == "" {
+		return nil
+	}
+
+	if strings.EqualFold(donor.Donor.FirstNames, v.FirstNames) && strings.EqualFold(donor.Donor.LastName, v.LastName) {
+		match = append(match, TypeDonor)
+	}
+
+	for _, attorney := range donor.Attorneys.Attorneys {
+		if strings.EqualFold(attorney.FirstNames, v.FirstNames) && strings.EqualFold(attorney.LastName, v.LastName) {
+			match = append(match, TypeAttorney)
+			break
+		}
+	}
+
+	for _, attorney := range donor.ReplacementAttorneys.Attorneys {
+		if strings.EqualFold(attorney.FirstNames, v.FirstNames) && strings.EqualFold(attorney.LastName, v.LastName) {
+			match = append(match, TypeReplacementAttorney)
+			break
+		}
+	}
+
+	if strings.EqualFold(donor.CertificateProvider.FirstNames, v.FirstNames) && strings.EqualFold(donor.CertificateProvider.LastName, v.LastName) {
+		match = append(match, TypeCertificateProvider)
+	}
+
+	for _, person := range donor.PeopleToNotify {
+		if strings.EqualFold(person.FirstNames, v.FirstNames) && strings.EqualFold(person.LastName, v.LastName) {
+			match = append(match, TypePersonToNotify)
+			break
+		}
+	}
+
+	if strings.EqualFold(donor.AuthorisedSignatory.FirstNames, v.FirstNames) && strings.EqualFold(donor.AuthorisedSignatory.LastName, v.LastName) {
+		match = append(match, TypeAuthorisedSignatory)
+	}
+
+	if strings.EqualFold(donor.IndependentWitness.FirstNames, v.FirstNames) && strings.EqualFold(donor.IndependentWitness.LastName, v.LastName) {
+		match = append(match, TypeIndependentWitness)
+	}
+
+	return match
 }
