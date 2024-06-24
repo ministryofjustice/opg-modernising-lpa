f7b4a7d9ee43d5693a4734e6e3a7fca16499ff0f
Joshua Hawxwell <m@hawx.me>
2024-06-24 15:09:15 +0100

    Add page to confirm voucher is different person

---
+++ cypress/e2e/donor/confirm-person-allowed-to-vouch.cy.js
@@ -0,0 +1,36 @@
+describe('Enter voucher', () => {
+  beforeEach(() => {
+    cy.visit('/fixtures?redirect=/enter-voucher&progress=payForTheLpa');
+  });
+
+  it('can confirm', () => {
+    cy.get('#f-first-names').type('Sam');
+    cy.get('#f-last-name').type('Smith');
+    cy.get('#f-email').type('voucher@example.com');
+    cy.contains('button', 'Save and continue').click();
+
+    cy.url().should('contain', '/confirm-person-allowed-to-vouch');
+    cy.checkA11yApp();
+
+    cy.contains('You have entered a name which matches your name, Sam Smith.');
+    cy.get('input[name=yes-no]').check('yes');
+    cy.contains('button', 'Save and continue').click();
+
+    cy.url().should('contain', '/task-list');
+  });
+
+  it('can select another', () => {
+    cy.get('#f-first-names').type('Sam');
+    cy.get('#f-last-name').type('Smith');
+    cy.get('#f-email').type('voucher@example.com');
+    cy.contains('button', 'Save and continue').click();
+
+    cy.get('input[name=yes-no]').check('no');
+    cy.contains('button', 'Save and continue').click();
+
+    cy.url().should('contain', '/enter-voucher');
+    cy.get('#f-first-names').should('have.value', '');
+    cy.get('#f-last-name').should('have.value', '');
+    cy.get('#f-email').should('have.value', '');
+  });
+});
