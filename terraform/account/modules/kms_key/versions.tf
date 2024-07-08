7752d099992045dbaf03279391eff67e391b1b9f
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-07-08 13:16:36 +0100

    createe a module to creatre kms keys

---
+++ terraform/account/modules/kms_key/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.57.0"
+      configuration_aliases = [
+        aws.eu_west_1,
+        aws.eu_west_2,
+      ]
+    }
+    pagerduty = {
+      source  = "PagerDuty/pagerduty"
+      version = "3.14.5"
+    }
+  }
+  required_version = "1.9.1"
+}
