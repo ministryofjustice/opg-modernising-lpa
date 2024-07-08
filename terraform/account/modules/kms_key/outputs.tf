df9c13b7342a2cc0eb9294224190258097adee8c
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-07-08 16:35:29 +0100

    rename output

--- terraform/account/modules/kms_key/outputs.tf
+++ terraform/account/modules/kms_key/outputs.tf
@@ -2,10 +2,10 @@ output "kms_key_alias_name" {
   value = aws_kms_alias.main_eu_west_1.name
 }
 
-output "kms_target_key_arn_eu_west_1" {
+output "eu_west_1_target_key_arn" {
   value = aws_kms_alias.main_eu_west_1.target_key_arn
 }
 
-output "kms_target_key_arn_eu_west_2" {
+output "eu_west_2_target_key_arn" {
   value = aws_kms_alias.main_eu_west_2.target_key_arn
 }
--- terraform/account/opensearch.tf
+++ terraform/account/opensearch.tf
@@ -17,7 +17,7 @@ resource "aws_opensearchserverless_security_policy" "lpas_collection_encryption_
       }
     ],
     AWSOwnedKey = false
-    KmsARN      = module.opensearch.kms_target_key_arn_eu_west_1
+    KmsARN      = module.opensearch.eu_west_1_target_key_arn
   })
   provider = aws.eu_west_1
 }
