deaa911290830cd21c7d5a239934cff0e1dd1377
Joshua Hawxwell <m@hawx.me>
2024-07-12 15:16:49 +0100

    Add an .editorconfig

--- cypress/e2e/accessibility/data-loss-warning.cy.js
+++ cypress/e2e/accessibility/data-loss-warning.cy.js
@@ -1,119 +1,119 @@
 describe('Data loss warnings', () => {
-  describe('Return to task list', () => {
-    it('locks focus to data loss warning dialog', () => {
-      cy.visit('/fixtures?redirect=/choose-attorneys&progress=provideYourDetails');
+    describe('Return to task list', () => {
+        it('locks focus to data loss warning dialog', () => {
+            cy.visit('/fixtures?redirect=/choose-attorneys&progress=provideYourDetails');
 
-      cy.get('#f-first-names').type('John');
-      cy.contains('a', 'Return to task list').click()
+            cy.get('#f-first-names').type('John');
+            cy.contains('a', 'Return to task list').click()
 
-      cy.get('#dialog').should('be.visible')
-      cy.get('#dialog-overlay').should('not.have.class', 'govuk-!-display-none')
+            cy.get('#dialog').should('be.visible')
+            cy.get('#dialog-overlay').should('not.have.class', 'govuk-!-display-none')
 
-      cy.focused().should('contain', 'Back to page')
+            cy.focused().should('contain', 'Back to page')
 
-      cy.realPress("Tab")
-      cy.focused().should('contain', 'Continue without saving')
+            cy.realPress("Tab")
+            cy.focused().should('contain', 'Continue without saving')
 
-      cy.realPress("Tab")
-      cy.focused().should('have.attr', 'id', 'dialog-title')
+            cy.realPress("Tab")
+            cy.focused().should('have.attr', 'id', 'dialog-title')
 
-      cy.realPress("Tab")
-      cy.focused().should('have.attr', 'id', 'dialog-description')
+            cy.realPress("Tab")
+            cy.focused().should('have.attr', 'id', 'dialog-description')
 
-      cy.realPress("Tab")
-      cy.focused().should('contain', 'Back to page')
+            cy.realPress("Tab")
+            cy.focused().should('contain', 'Back to page')
 
-      cy.contains('button', 'Back to page').click()
+            cy.contains('button', 'Back to page').click()
 
-      cy.get('#dialog').should('not.be.visible')
-      cy.get('#dialog-overlay').should('have.class', 'govuk-!-display-none')
+            cy.get('#dialog').should('not.be.visible')
+            cy.get('#dialog-overlay').should('have.class', 'govuk-!-display-none')
 
-      cy.focused().should('contain', 'Return to task list')
+            cy.focused().should('contain', 'Return to task list')
 
-      cy.contains('a', 'Return to task list').click()
+            cy.contains('a', 'Return to task list').click()
 
-      cy.get('#dialog').should('be.visible')
-      cy.get('#dialog-overlay').should('not.have.class', 'govuk-!-display-none')
+            cy.get('#dialog').should('be.visible')
+            cy.get('#dialog-overlay').should('not.have.class', 'govuk-!-display-none')
 
-      cy.focused().should('contain', 'Back to page')
+            cy.focused().should('contain', 'Back to page')
 
-      cy.realPress(["Shift", "Tab"])
-      cy.focused().should('have.attr', 'id', 'dialog-description')
+            cy.realPress(["Shift", "Tab"])
+            cy.focused().should('have.attr', 'id', 'dialog-description')
 
-      cy.realPress(["Shift", "Tab"])
-      cy.focused().should('have.attr', 'id', 'dialog-title')
+            cy.realPress(["Shift", "Tab"])
+            cy.focused().should('have.attr', 'id', 'dialog-title')
 
-      cy.realPress(["Shift", "Tab"])
-      cy.focused().should('contain', 'Continue without saving')
+            cy.realPress(["Shift", "Tab"])
+            cy.focused().should('contain', 'Continue without saving')
 
-      cy.realPress(["Shift", "Tab"])
-      cy.focused().should('contain', 'Back to page')
+            cy.realPress(["Shift", "Tab"])
+            cy.focused().should('contain', 'Back to page')
 
-      cy.realType('{esc}')
+            cy.realType('{esc}')
 
-      cy.get('#dialog').should('not.be.visible')
-      cy.get('#dialog-overlay').should('have.class', 'govuk-!-display-none')
+            cy.get('#dialog').should('not.be.visible')
+            cy.get('#dialog-overlay').should('have.class', 'govuk-!-display-none')
 
-      cy.focused().should('contain', 'Return to task list')
+            cy.focused().should('contain', 'Return to task list')
+        })
     })
-  })
 
-  describe('Change language', () => {
-    it('locks focus to data loss warning dialog', () => {
-      cy.visit('/fixtures?redirect=/choose-attorneys&progress=provideYourDetails');
+    describe('Change language', () => {
+        it('locks focus to data loss warning dialog', () => {
+            cy.visit('/fixtures?redirect=/choose-attorneys&progress=provideYourDetails');
 
-      cy.get('#f-first-names').type('John');
-      cy.contains('a', 'Cymraeg').click()
+            cy.get('#f-first-names').type('John');
+            cy.contains('a', 'Cymraeg').click()
 
-      cy.get('#language-dialog').should('be.visible')
-      cy.get('#dialog-overlay').should('not.have.class', 'govuk-!-display-none')
+            cy.get('#language-dialog').should('be.visible')
+            cy.get('#dialog-overlay').should('not.have.class', 'govuk-!-display-none')
 
-      cy.focused().should('contain', 'Back to page')
+            cy.focused().should('contain', 'Back to page')
 
-      cy.realPress("Tab")
-      cy.focused().should('contain', 'Continue without saving')
+            cy.realPress("Tab")
+            cy.focused().should('contain', 'Continue without saving')
 
-      cy.realPress("Tab")
-      cy.focused().should('have.attr', 'id', 'language-dialog-title')
+            cy.realPress("Tab")
+            cy.focused().should('have.attr', 'id', 'language-dialog-title')
 
-      cy.realPress("Tab")
-      cy.focused().should('have.attr', 'id', 'language-dialog-description')
+            cy.realPress("Tab")
+            cy.focused().should('have.attr', 'id', 'language-dialog-description')
 
-      cy.realPress("Tab")
-      cy.focused().should('contain', 'Back to page')
+            cy.realPress("Tab")
+            cy.focused().should('contain', 'Back to page')
 
-      cy.contains('#language-dialog button', 'Back to page').click()
+            cy.contains('#language-dialog button', 'Back to page').click()
 
-      cy.get('#dialog').should('not.be.visible')
-      cy.get('#dialog-overlay').should('have.class', 'govuk-!-display-none')
+            cy.get('#dialog').should('not.be.visible')
+            cy.get('#dialog-overlay').should('have.class', 'govuk-!-display-none')
 
-      cy.focused().should('contain', 'Cymraeg')
+            cy.focused().should('contain', 'Cymraeg')
 
-      cy.contains('a', 'Cymraeg').click()
+            cy.contains('a', 'Cymraeg').click()
 
-      cy.get('#language-dialog').should('be.visible')
-      cy.get('#dialog-overlay').should('not.have.class', 'govuk-!-display-none')
+            cy.get('#language-dialog').should('be.visible')
+            cy.get('#dialog-overlay').should('not.have.class', 'govuk-!-display-none')
 
-      cy.focused().should('contain', 'Back to page')
+            cy.focused().should('contain', 'Back to page')
 
-      cy.realPress(["Shift", "Tab"])
-      cy.focused().should('have.attr', 'id', 'language-dialog-description')
+            cy.realPress(["Shift", "Tab"])
+            cy.focused().should('have.attr', 'id', 'language-dialog-description')
 
-      cy.realPress(["Shift", "Tab"])
-      cy.focused().should('have.attr', 'id', 'language-dialog-title')
+            cy.realPress(["Shift", "Tab"])
+            cy.focused().should('have.attr', 'id', 'language-dialog-title')
 
-      cy.realPress(["Shift", "Tab"])
-      cy.focused().should('contain', 'Continue without saving')
+            cy.realPress(["Shift", "Tab"])
+            cy.focused().should('contain', 'Continue without saving')
 
-      cy.realPress(["Shift", "Tab"])
-      cy.focused().should('contain', 'Back to page')
+            cy.realPress(["Shift", "Tab"])
+            cy.focused().should('contain', 'Back to page')
 
-      cy.realType('{esc}')
+            cy.realType('{esc}')
 
-      cy.get('#language-dialog').should('not.be.visible')
-      cy.get('#dialog-overlay').should('have.class', 'govuk-!-display-none')
+            cy.get('#language-dialog').should('not.be.visible')
+            cy.get('#dialog-overlay').should('have.class', 'govuk-!-display-none')
 
-      cy.focused().should('contain', 'Cymraeg')
+            cy.focused().should('contain', 'Cymraeg')
+        })
     })
-  })
 })
--- web/assets/data-loss-warning.js
+++ web/assets/data-loss-warning.js
@@ -1,115 +1,115 @@
 export class DataLossWarning {
-  constructor(trigger, dialog) {
-    this.trigger = trigger
-    this.dialog = dialog
-  }
-
-  init() {
-    this.dialogOverlay = document.getElementById('dialog-overlay')
-    // so we can reference the same func when removing event
-    this.handleTrapFocus = this.handleTrapFocus.bind(this)
-
-    if (this.dialog && this.dialogOverlay && this.trigger) {
-      this.submitButton = document.querySelector('button[type=submit]')
-      this.backToPage = this.dialog.querySelector('button')
-      this.continueWithoutSaving = this.dialog.querySelector('a')
-      this.formValuesOnPageLoad = this.getEncodedStringifiedFormValues()
-      this.formValuesPriorToPageLoad = this.getFormValuesFromCookie()
-      this.registerListeners()
+    constructor(trigger, dialog) {
+        this.trigger = trigger
+        this.dialog = dialog
     }
-  }
 
-  changesMade() {
-    return this.formValuesOnPageLoad !== this.getEncodedStringifiedFormValues() ||
-      // to account for page reload on validation error
-      this.formValuesPriorToPageLoad === this.getEncodedStringifiedFormValues()
-  }
+    init() {
+        this.dialogOverlay = document.getElementById('dialog-overlay')
+        // so we can reference the same func when removing event
+        this.handleTrapFocus = this.handleTrapFocus.bind(this)
 
-  formEmpty() {
-    const encodedEmptyFormValues = encodeURIComponent(JSON.stringify([]))
-
-    return this.getEncodedStringifiedFormValues() === encodedEmptyFormValues
-  }
-
-  getEncodedStringifiedFormValues() {
-    const formValues = new FormData(document.querySelector("form:not([action])"))
-    formValues.delete('csrf')
-
-    const sanitisedValues = [...formValues.values()].filter(subArray => subArray.length > 0)
-
-    return encodeURIComponent(JSON.stringify(sanitisedValues))
-  }
-
-  toggleDialogVisibility() {
-    if (this.changesMade() && !this.formEmpty()) {
-      this.dialog.classList.toggle('govuk-!-display-none')
-      this.dialogOverlay.classList.toggle('govuk-!-display-none')
-
-      if (this.dialogVisible()) {
-        this.dialog.addEventListener('keydown', this.handleTrapFocus)
-        this.backToPage.focus()
-      } else {
-        this.dialog.removeEventListener('keydown', this.handleTrapFocus)
-        this.trigger.focus()
-      }
-    }
-  }
-
-  dialogVisible() {
-    return !this.dialog.classList.contains('govuk-!-display-none') && !this.dialogOverlay.classList.contains('govuk-!-display-none')
-  }
-
-  registerListeners() {
-    this.trigger.addEventListener('click', (e) => {
-      if (this.changesMade() && !this.formEmpty()) {
-        e.preventDefault()
-      }
-    })
-    this.submitButton.addEventListener('click', this.addFormValuesToCookie.bind(this))
-    this.trigger.addEventListener('click', this.toggleDialogVisibility.bind(this))
-    this.backToPage.addEventListener('click', this.toggleDialogVisibility.bind(this))
-    this.continueWithoutSaving.addEventListener('click', this.toggleDialogVisibility.bind(this))
-  }
-
-  handleTrapFocus(e) {
-    const focusable = this.dialog.querySelectorAll('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])')
-    const firstFocusableEl = focusable[0]
-    const lastFocusableEl = focusable[focusable.length - 1]
-
-    const KEY_CODE_TAB = 9
-    const KEY_CODE_ESC = 27
-
-    const tabPressed = (e.key === 'Tab' || e.keyCode === KEY_CODE_TAB)
-    const escPressed = (e.key === 'Esc' || e.keyCode === KEY_CODE_ESC)
-
-    if (tabPressed) {
-      if (e.shiftKey) { /* shift + tab */
-        if (document.activeElement === firstFocusableEl) {
-          lastFocusableEl.focus()
-          e.preventDefault()
+        if (this.dialog && this.dialogOverlay && this.trigger) {
+            this.submitButton = document.querySelector('button[type=submit]')
+            this.backToPage = this.dialog.querySelector('button')
+            this.continueWithoutSaving = this.dialog.querySelector('a')
+            this.formValuesOnPageLoad = this.getEncodedStringifiedFormValues()
+            this.formValuesPriorToPageLoad = this.getFormValuesFromCookie()
+            this.registerListeners()
         }
-      } else /* tab */ {
-        if (document.activeElement === lastFocusableEl) {
-          firstFocusableEl.focus()
-          e.preventDefault()
+    }
+
+    changesMade() {
+        return this.formValuesOnPageLoad !== this.getEncodedStringifiedFormValues() ||
+            // to account for page reload on validation error
+            this.formValuesPriorToPageLoad === this.getEncodedStringifiedFormValues()
+    }
+
+    formEmpty() {
+        const encodedEmptyFormValues = encodeURIComponent(JSON.stringify([]))
+
+        return this.getEncodedStringifiedFormValues() === encodedEmptyFormValues
+    }
+
+    getEncodedStringifiedFormValues() {
+        const formValues = new FormData(document.querySelector("form:not([action])"))
+        formValues.delete('csrf')
+
+        const sanitisedValues = [...formValues.values()].filter(subArray => subArray.length > 0)
+
+        return encodeURIComponent(JSON.stringify(sanitisedValues))
+    }
+
+    toggleDialogVisibility() {
+        if (this.changesMade() && !this.formEmpty()) {
+            this.dialog.classList.toggle('govuk-!-display-none')
+            this.dialogOverlay.classList.toggle('govuk-!-display-none')
+
+            if (this.dialogVisible()) {
+                this.dialog.addEventListener('keydown', this.handleTrapFocus)
+                this.backToPage.focus()
+            } else {
+                this.dialog.removeEventListener('keydown', this.handleTrapFocus)
+                this.trigger.focus()
+            }
         }
-      }
     }
 
-    if (escPressed) {
-      this.toggleDialogVisibility()
+    dialogVisible() {
+        return !this.dialog.classList.contains('govuk-!-display-none') && !this.dialogOverlay.classList.contains('govuk-!-display-none')
     }
-  }
 
-  addFormValuesToCookie() {
-    // so the cookie isn't available for longer than required
-    const tenSecondsFutureDate = new Date()
-    tenSecondsFutureDate.setSeconds(tenSecondsFutureDate.getSeconds() + 10)
+    registerListeners() {
+        this.trigger.addEventListener('click', (e) => {
+            if (this.changesMade() && !this.formEmpty()) {
+                e.preventDefault()
+            }
+        })
+        this.submitButton.addEventListener('click', this.addFormValuesToCookie.bind(this))
+        this.trigger.addEventListener('click', this.toggleDialogVisibility.bind(this))
+        this.backToPage.addEventListener('click', this.toggleDialogVisibility.bind(this))
+        this.continueWithoutSaving.addEventListener('click', this.toggleDialogVisibility.bind(this))
+    }
 
-    document.cookie = `formValues=${this.getEncodedStringifiedFormValues()}; expires=${tenSecondsFutureDate.toUTCString()}; SameSite=Lax; Secure`
-  }
+    handleTrapFocus(e) {
+        const focusable = this.dialog.querySelectorAll('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])')
+        const firstFocusableEl = focusable[0]
+        const lastFocusableEl = focusable[focusable.length - 1]
 
-  getFormValuesFromCookie() {
-    return document.cookie.split("; ").find((row) => row.startsWith("formValues="))?.split("=")[1]
-  }
+        const KEY_CODE_TAB = 9
+        const KEY_CODE_ESC = 27
+
+        const tabPressed = (e.key === 'Tab' || e.keyCode === KEY_CODE_TAB)
+        const escPressed = (e.key === 'Esc' || e.keyCode === KEY_CODE_ESC)
+
+        if (tabPressed) {
+            if (e.shiftKey) { /* shift + tab */
+                if (document.activeElement === firstFocusableEl) {
+                    lastFocusableEl.focus()
+                    e.preventDefault()
+                }
+            } else /* tab */ {
+                if (document.activeElement === lastFocusableEl) {
+                    firstFocusableEl.focus()
+                    e.preventDefault()
+                }
+            }
+        }
+
+        if (escPressed) {
+            this.toggleDialogVisibility()
+        }
+    }
+
+    addFormValuesToCookie() {
+        // so the cookie isn't available for longer than required
+        const tenSecondsFutureDate = new Date()
+        tenSecondsFutureDate.setSeconds(tenSecondsFutureDate.getSeconds() + 10)
+
+        document.cookie = `formValues=${this.getEncodedStringifiedFormValues()}; expires=${tenSecondsFutureDate.toUTCString()}; SameSite=Lax; Secure`
+    }
+
+    getFormValuesFromCookie() {
+        return document.cookie.split("; ").find((row) => row.startsWith("formValues="))?.split("=")[1]
+    }
 }
