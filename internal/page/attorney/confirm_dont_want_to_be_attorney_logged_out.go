2bf1c43d2577a93401cc70eed364008064adb29d
Joshua Hawxwell <m@hawx.me>
2024-07-23 08:33:09 +0100

    Allow attorney to opt-out

---
+++ internal/page/attorney/confirm_dont_want_to_be_attorney_logged_out.go
@@ -0,0 +1,88 @@
+package attorney
+
+import (
+	"net/http"
+	"net/url"
+
+	"github.com/ministryofjustice/opg-go-common/template"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/actor"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/actor/actoruid"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/lpastore"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/notify"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/page"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/validation"
+)
+
+type confirmDontWantToBeAttorneyDataLoggedOut struct {
+	App    page.AppData
+	Errors validation.List
+	Lpa    *lpastore.Lpa
+}
+
+func ConfirmDontWantToBeAttorneyLoggedOut(tmpl template.Template, shareCodeStore ShareCodeStore, lpaStoreResolvingService LpaStoreResolvingService, sessionStore SessionStore, notifyClient NotifyClient, appPublicURL string) page.Handler {
+	return func(appData page.AppData, w http.ResponseWriter, r *http.Request) error {
+		session, err := sessionStore.LpaData(r)
+		if err != nil {
+			return err
+		}
+
+		ctx := page.ContextWithSessionData(r.Context(), &page.SessionData{LpaID: session.LpaID})
+
+		lpa, err := lpaStoreResolvingService.Get(ctx)
+		if err != nil {
+			return err
+		}
+
+		data := &confirmDontWantToBeAttorneyDataLoggedOut{
+			App: appData,
+			Lpa: lpa,
+		}
+
+		if r.Method == http.MethodPost {
+			shareCode, err := shareCodeStore.Get(r.Context(), actor.TypeAttorney, r.URL.Query().Get("referenceNumber"))
+			if err != nil {
+				return err
+			}
+
+			email := notify.AttorneyOptedOutEmail{
+				AttorneyFullName:  findAttorneyFullName(lpa, shareCode.ActorUID, shareCode.IsTrustCorporation, shareCode.IsReplacementAttorney),
+				DonorFullName:     lpa.Donor.FullName(),
+				LpaType:           appData.Localizer.T(lpa.Type.String()),
+				LpaUID:            lpa.LpaUID,
+				DonorStartPageURL: appPublicURL + page.Paths.Start.Format(),
+			}
+
+			if err := notifyClient.SendActorEmail(ctx, lpa.Donor.Email, lpa.LpaUID, email); err != nil {
+				return err
+			}
+
+			if err := shareCodeStore.Delete(r.Context(), shareCode); err != nil {
+				return err
+			}
+
+			return page.Paths.Attorney.YouHaveDecidedNotToBeAttorney.RedirectQuery(w, r, appData, url.Values{"donorFullName": {lpa.Donor.FullName()}})
+		}
+
+		return tmpl(w, data)
+	}
+}
+
+func findAttorneyFullName(lpa *lpastore.Lpa, uid actoruid.UID, isTrustCorporation, isReplacement bool) string {
+	if t := lpa.ReplacementAttorneys.TrustCorporation; t.UID == uid {
+		return t.Name
+	}
+
+	if t := lpa.Attorneys.TrustCorporation; t.UID == uid {
+		return t.Name
+	}
+
+	if a, ok := lpa.ReplacementAttorneys.Get(uid); ok {
+		return a.FullName()
+	}
+
+	if a, ok := lpa.Attorneys.Get(uid); ok {
+		return a.FullName()
+	}
+
+	panic("TODO error")
+}
---
+++ internal/page/attorney/confirm_dont_want_to_be_attorney_test.go
@@ -0,0 +1,199 @@
+package attorney
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/actor"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/actor/actoruid"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/lpastore"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/notify"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/page"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestGetConfirmDontWantToBeAttorney(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodGet, "/", nil)
+
+	lpa := lpastore.Lpa{LpaUID: "lpa-uid"}
+
+	lpaStoreResolvingService := newMockLpaStoreResolvingService(t)
+	lpaStoreResolvingService.EXPECT().
+		Get(r.Context()).
+		Return(&lpa, nil)
+
+	template := newMockTemplate(t)
+	template.EXPECT().
+		Execute(w, &confirmDontWantToBeAttorneyData{
+			App: testAppData,
+			Lpa: &lpa,
+		}).
+		Return(nil)
+
+	err := ConfirmDontWantToBeAttorney(template.Execute, lpaStoreResolvingService, nil, nil, "example.com")(testAppData, w, r, &actor.AttorneyProvidedDetails{})
+	resp := w.Result()
+
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
+
+func TestGetConfirmDontWantToBeAttorneyWhenLpaStoreResolvingServiceErrors(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodGet, "/", nil)
+
+	lpaStoreResolvingService := newMockLpaStoreResolvingService(t)
+	lpaStoreResolvingService.EXPECT().
+		Get(mock.Anything).
+		Return(&lpastore.Lpa{}, expectedError)
+
+	err := ConfirmDontWantToBeAttorney(nil, lpaStoreResolvingService, nil, nil, "example.com")(testAppData, w, r, &actor.AttorneyProvidedDetails{})
+	resp := w.Result()
+
+	assert.Equal(t, expectedError, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
+
+func TestGetConfirmDontWantToBeAttorneyWhenTemplateErrors(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodGet, "/", nil)
+
+	lpaStoreResolvingService := newMockLpaStoreResolvingService(t)
+	lpaStoreResolvingService.EXPECT().
+		Get(mock.Anything).
+		Return(&lpastore.Lpa{}, nil)
+
+	template := newMockTemplate(t)
+	template.EXPECT().
+		Execute(mock.Anything, mock.Anything).
+		Return(expectedError)
+
+	err := ConfirmDontWantToBeAttorney(template.Execute, lpaStoreResolvingService, nil, nil, "example.com")(testAppData, w, r, &actor.AttorneyProvidedDetails{})
+	resp := w.Result()
+
+	assert.Equal(t, expectedError, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
+
+func TestPostConfirmDontWantToBeAttorney(t *testing.T) {
+	r, _ := http.NewRequestWithContext(page.ContextWithSessionData(context.Background(), &page.SessionData{LpaID: "123", SessionID: "456"}), http.MethodPost, "/?referenceNumber=123", nil)
+	w := httptest.NewRecorder()
+	uid := actoruid.New()
+
+	lpaStoreResolvingService := newMockLpaStoreResolvingService(t)
+	lpaStoreResolvingService.EXPECT().
+		Get(r.Context()).
+		Return(&lpastore.Lpa{
+			LpaUID:   "lpa-uid",
+			SignedAt: time.Now(),
+			Donor: lpastore.Donor{
+				FirstNames: "a b", LastName: "c", Email: "a@example.com",
+			},
+			Attorneys: lpastore.Attorneys{
+				Attorneys: []lpastore.Attorney{
+					{FirstNames: "d e", LastName: "f", UID: uid},
+				},
+			},
+			Type: actor.LpaTypePersonalWelfare,
+		}, nil)
+
+	certificateProviderStore := newMockAttorneyStore(t)
+	certificateProviderStore.EXPECT().
+		Delete(r.Context()).
+		Return(nil)
+
+	localizer := newMockLocalizer(t)
+	localizer.EXPECT().
+		T("personal-welfare").
+		Return("Personal welfare")
+
+	testAppData.Localizer = localizer
+
+	notifyClient := newMockNotifyClient(t)
+	notifyClient.EXPECT().
+		SendActorEmail(r.Context(), "a@example.com", "lpa-uid", notify.AttorneyOptedOutEmail{
+			AttorneyFullName:  "d e f",
+			DonorFullName:     "a b c",
+			LpaType:           "Personal welfare",
+			LpaUID:            "lpa-uid",
+			DonorStartPageURL: "example.com" + page.Paths.Start.Format(),
+		}).
+		Return(nil)
+
+	err := ConfirmDontWantToBeAttorney(nil, lpaStoreResolvingService, certificateProviderStore, notifyClient, "example.com")(testAppData, w, r, &actor.AttorneyProvidedDetails{
+		UID: uid,
+	})
+
+	resp := w.Result()
+
+	assert.Nil(t, err)
+	assert.Equal(t, page.Paths.Attorney.YouHaveDecidedNotToBeAttorney.Format()+"?donorFirstNames=a+b&donorFullName=a+b+c", resp.Header.Get("Location"))
+	assert.Equal(t, http.StatusFound, resp.StatusCode)
+}
+
+func TestPostConfirmDontWantToBeAttorneyErrors(t *testing.T) {
+	r, _ := http.NewRequest(http.MethodPost, "/?referenceNumber=123", nil)
+
+	testcases := map[string]struct {
+		certificateProviderStore func(*testing.T) *mockAttorneyStore
+		notifyClient             func(*testing.T) *mockNotifyClient
+	}{
+		"when attorneyStore.Delete() error": {
+			certificateProviderStore: func(t *testing.T) *mockAttorneyStore {
+				certificateProviderStore := newMockAttorneyStore(t)
+				certificateProviderStore.EXPECT().
+					Delete(mock.Anything).
+					Return(expectedError)
+
+				return certificateProviderStore
+			},
+		},
+		"when notifyClient.SendActorEmail() error": {
+			certificateProviderStore: func(t *testing.T) *mockAttorneyStore {
+				certificateProviderStore := newMockAttorneyStore(t)
+				certificateProviderStore.EXPECT().
+					Delete(mock.Anything).
+					Return(nil)
+
+				return certificateProviderStore
+			},
+			notifyClient: func(t *testing.T) *mockNotifyClient {
+				client := newMockNotifyClient(t)
+				client.EXPECT().
+					SendActorEmail(mock.Anything, mock.Anything, mock.Anything, mock.Anything).
+					Return(expectedError)
+
+				return client
+			},
+		},
+	}
+
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+
+			lpaStoreResolvingService := newMockLpaStoreResolvingService(t)
+			lpaStoreResolvingService.EXPECT().
+				Get(r.Context()).
+				Return(&lpastore.Lpa{LpaUID: "lpa-uid", SignedAt: time.Now()}, nil)
+
+			localizer := newMockLocalizer(t)
+			localizer.EXPECT().
+				T(mock.Anything).
+				Return("a")
+
+			testAppData.Localizer = localizer
+
+			err := ConfirmDontWantToBeAttorney(nil, lpaStoreResolvingService, evalT(tc.certificateProviderStore, t), evalT(tc.notifyClient, t), "example.com")(testAppData, w, r, &actor.AttorneyProvidedDetails{})
+
+			resp := w.Result()
+
+			assert.Equal(t, expectedError, err)
+			assert.Equal(t, http.StatusOK, resp.StatusCode)
+		})
+	}
+}
