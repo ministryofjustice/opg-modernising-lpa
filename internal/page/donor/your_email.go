c3d8119f70d80cbaa770de77d167e59edab1c37a
Joshua Hawxwell <m@hawx.me>
2024-07-26 09:31:30 +0100

    Split your details page

---
+++ internal/page/donor/your_email.go
@@ -0,0 +1,63 @@
+package donor
+
+import (
+	"net/http"
+
+	"github.com/ministryofjustice/opg-go-common/template"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/actor"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/page"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/validation"
+)
+
+type yourEmailData struct {
+	App         page.AppData
+	Errors      validation.List
+	Form        *yourEmailForm
+	CanTaskList bool
+}
+
+func YourEmail(tmpl template.Template, donorStore DonorStore) Handler {
+	return func(appData page.AppData, w http.ResponseWriter, r *http.Request, donor *actor.DonorProvidedDetails) error {
+		data := &yourEmailData{
+			App: appData,
+			Form: &yourEmailForm{
+				Email: donor.Donor.Email,
+			},
+			CanTaskList: !donor.Type.Empty(),
+		}
+
+		if r.Method == http.MethodPost {
+			data.Form = readYourEmailForm(r)
+			data.Errors = data.Form.Validate()
+
+			if data.Errors.None() {
+				donor.Donor.Email = data.Form.Email
+
+				if err := donorStore.Put(r.Context(), donor); err != nil {
+					return err
+				}
+
+				return page.Paths.CanYouSignYourLpa.Redirect(w, r, appData, donor)
+			}
+		}
+
+		return tmpl(w, data)
+	}
+}
+
+type yourEmailForm struct {
+	Email string
+}
+
+func readYourEmailForm(r *http.Request) *yourEmailForm {
+	return &yourEmailForm{Email: page.PostFormString(r, "email")}
+}
+
+func (f *yourEmailForm) Validate() validation.List {
+	var errors validation.List
+
+	errors.String("email", "email", f.Email,
+		validation.Email())
+
+	return errors
+}
--- internal/page/donor/your_name.go
+++ internal/page/donor/your_name.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/ministryofjustice/opg-go-common/template"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/actor"
@@ -12,13 +13,15 @@ import (
 )
 
 type yourNameData struct {
-	App         page.AppData
-	Errors      validation.List
-	Form        *yourNameForm
-	NameWarning *actor.SameNameWarning
+	App              page.AppData
+	Errors           validation.List
+	Form             *yourNameForm
+	NameWarning      *actor.SameNameWarning
+	CanTaskList      bool
+	MakingAnotherLPA bool
 }
 
-func YourName(tmpl template.Template, donorStore DonorStore) Handler {
+func YourName(tmpl template.Template, donorStore DonorStore, sessionStore SessionStore) Handler {
 	return func(appData page.AppData, w http.ResponseWriter, r *http.Request, donor *actor.DonorProvidedDetails) error {
 		data := &yourNameData{
 			App: appData,
@@ -27,6 +30,8 @@ func YourName(tmpl template.Template, donorStore DonorStore) Handler {
 				LastName:   donor.Donor.LastName,
 				OtherNames: donor.Donor.OtherNames,
 			},
+			CanTaskList:      !donor.Type.Empty(),
+			MakingAnotherLPA: r.FormValue("makingAnotherLPA") == "1",
 		}
 
 		if r.Method == http.MethodPost {
@@ -48,7 +53,23 @@ func YourName(tmpl template.Template, donorStore DonorStore) Handler {
 
 			if data.Errors.None() && data.NameWarning == nil {
 				if !donor.NamesChanged(data.Form.FirstNames, data.Form.LastName, data.Form.OtherNames) {
-					return page.Paths.MakeANewLPA.Redirect(w, r, appData, donor)
+					if data.MakingAnotherLPA {
+						return page.Paths.MakeANewLPA.Redirect(w, r, appData, donor)
+					}
+
+					return page.Paths.YourDetails.Redirect(w, r, appData, donor)
+				}
+
+				if appData.SupporterData == nil {
+					loginSession, err := sessionStore.Login(r)
+					if err != nil {
+						return err
+					}
+					if loginSession.Email == "" {
+						return fmt.Errorf("no email in login session")
+					}
+
+					donor.Donor.Email = loginSession.Email
 				}
 
 				donor.Donor.FirstNames = data.Form.FirstNames
@@ -60,7 +81,11 @@ func YourName(tmpl template.Template, donorStore DonorStore) Handler {
 					return err
 				}
 
-				return page.Paths.WeHaveUpdatedYourDetails.RedirectQuery(w, r, appData, donor, url.Values{"detail": {"name"}})
+				if data.MakingAnotherLPA {
+					return page.Paths.WeHaveUpdatedYourDetails.RedirectQuery(w, r, appData, donor, url.Values{"detail": {"name"}})
+				}
+
+				return page.Paths.YourDateOfBirth.Redirect(w, r, appData, donor)
 			}
 		}
 
@@ -76,14 +101,12 @@ type yourNameForm struct {
 }
 
 func readYourNameForm(r *http.Request) *yourNameForm {
-	d := &yourNameForm{}
-
-	d.FirstNames = page.PostFormString(r, "first-names")
-	d.LastName = page.PostFormString(r, "last-name")
-	d.OtherNames = page.PostFormString(r, "other-names")
-	d.IgnoreNameWarning = page.PostFormString(r, "ignore-name-warning")
-
-	return d
+	return &yourNameForm{
+		FirstNames:        page.PostFormString(r, "first-names"),
+		LastName:          page.PostFormString(r, "last-name"),
+		OtherNames:        page.PostFormString(r, "other-names"),
+		IgnoreNameWarning: page.PostFormString(r, "ignore-name-warning"),
+	}
 }
 
 func (f *yourNameForm) Validate() validation.List {
@@ -101,4 +124,42 @@ func (f *yourNameForm) Validate() validation.List {
 		validation.StringTooLong(50))
 
 	return errors
+}
+
+func donorMatches(donor *actor.DonorProvidedDetails, firstNames, lastName string) actor.Type {
+	if firstNames == "" && lastName == "" {
+		return actor.TypeNone
+	}
+
+	for _, attorney := range donor.Attorneys.Attorneys {
+		if strings.EqualFold(attorney.FirstNames, firstNames) && strings.EqualFold(attorney.LastName, lastName) {
+			return actor.TypeAttorney
+		}
+	}
+
+	for _, attorney := range donor.ReplacementAttorneys.Attorneys {
+		if strings.EqualFold(attorney.FirstNames, firstNames) && strings.EqualFold(attorney.LastName, lastName) {
+			return actor.TypeReplacementAttorney
+		}
+	}
+
+	if strings.EqualFold(donor.CertificateProvider.FirstNames, firstNames) && strings.EqualFold(donor.CertificateProvider.LastName, lastName) {
+		return actor.TypeCertificateProvider
+	}
+
+	for _, person := range donor.PeopleToNotify {
+		if strings.EqualFold(person.FirstNames, firstNames) && strings.EqualFold(person.LastName, lastName) {
+			return actor.TypePersonToNotify
+		}
+	}
+
+	if strings.EqualFold(donor.AuthorisedSignatory.FirstNames, firstNames) && strings.EqualFold(donor.AuthorisedSignatory.LastName, lastName) {
+		return actor.TypeAuthorisedSignatory
+	}
+
+	if strings.EqualFold(donor.IndependentWitness.FirstNames, firstNames) && strings.EqualFold(donor.IndependentWitness.LastName, lastName) {
+		return actor.TypeIndependentWitness
+	}
+
+	return actor.TypeNone
 }
