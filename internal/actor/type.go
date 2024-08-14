6a3bf633c7f1a4daff0675a12de2917f68f66d5e
Joshua Hawxwell <m@hawx.me>
2024-08-14 14:40:54 +0100

    Use generated enum for actor.Type

--- internal/actor/type.go
+++ internal/actor/type.go
@@ -1,72 +1,18 @@
 package actor
 
+//go:generate enumerator -type Type -linecomment -trimprefix
 type Type uint8
 
 const (
-	TypeNone Type = iota
-	TypeDonor
-	TypeAttorney
-	TypeReplacementAttorney
-	TypeCertificateProvider
-	TypePersonToNotify
-	TypeAuthorisedSignatory
-	TypeIndependentWitness
-	TypeTrustCorporation
-	TypeReplacementTrustCorporation
-	TypeVoucher
+	TypeNone                        Type = iota
+	TypeDonor                            // donor
+	TypeAttorney                         // attorney
+	TypeReplacementAttorney              // replacementAttorney
+	TypeCertificateProvider              // certificateProvider
+	TypePersonToNotify                   // personToNotify
+	TypeAuthorisedSignatory              // signatory
+	TypeIndependentWitness               // independentWitness
+	TypeTrustCorporation                 // trustCorporation
+	TypeReplacementTrustCorporation      // replacementTrustCorporation
+	TypeVoucher                          // voucher
 )
-
-func (t Type) String() string {
-	switch t {
-	case TypeDonor:
-		return "donor"
-	case TypeAttorney:
-		return "attorney"
-	case TypeReplacementAttorney:
-		return "replacementAttorney"
-	case TypeCertificateProvider:
-		return "certificateProvider"
-	case TypePersonToNotify:
-		return "personToNotify"
-	case TypeAuthorisedSignatory:
-		return "signatory"
-	case TypeIndependentWitness:
-		return "independentWitness"
-	case TypeTrustCorporation:
-		return "trustCorporation"
-	case TypeReplacementTrustCorporation:
-		return "replacementTrustCorporation"
-	case TypeVoucher:
-		return "voucher"
-	default:
-		return ""
-	}
-}
-
-type Types struct {
-	None                        Type
-	Donor                       Type
-	Attorney                    Type
-	ReplacementAttorney         Type
-	TrustCorporation            Type
-	ReplacementTrustCorporation Type
-	CertificateProvider         Type
-	PersonToNotify              Type
-	AuthorisedSignatory         Type
-	IndependentWitness          Type
-	Voucher                     Type
-}
-
-var ActorTypes = Types{
-	None:                        TypeNone,
-	Donor:                       TypeDonor,
-	Attorney:                    TypeAttorney,
-	ReplacementAttorney:         TypeReplacementAttorney,
-	TrustCorporation:            TypeTrustCorporation,
-	ReplacementTrustCorporation: TypeReplacementTrustCorporation,
-	CertificateProvider:         TypeCertificateProvider,
-	PersonToNotify:              TypePersonToNotify,
-	AuthorisedSignatory:         TypeAuthorisedSignatory,
-	IndependentWitness:          TypeIndependentWitness,
-	Voucher:                     TypeVoucher,
-}
