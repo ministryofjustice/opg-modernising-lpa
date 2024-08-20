d1f5b145cc750fe4fa33fb0cac648d4eefcf04fd
Joshua Hawxwell <m@hawx.me>
2024-08-20 10:37:13 +0100

    Add confirm your identity to voucher fixtures

--- internal/voucher/voucherpage/mock_test.go
+++ internal/voucher/voucherpage/mock_test.go
@@ -2,6 +2,7 @@ package voucherpage
 
 import (
 	"errors"
+	"time"
 
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/appcontext"
 )
@@ -9,4 +10,6 @@ import (
 var (
 	expectedError = errors.New("err")
 	testAppData   = appcontext.Data{LpaID: "lpa-id"}
+	testNow       = time.Now()
+	testNowFn     = func() time.Time { return testNow }
 )
