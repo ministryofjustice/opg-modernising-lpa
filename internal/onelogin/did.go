b018927ff19c3144a6f5ef771efbef2dfeb408fb
Joshua Hawxwell <m@hawx.me>
2024-09-11 10:21:12 +0100

    Add tests for did request loop

--- internal/onelogin/did.go
+++ internal/onelogin/did.go
@@ -30,24 +30,26 @@ type didAssertionMethod struct {
 }
 
 type didClient struct {
-	ctx            context.Context
-	identityURL    string
-	http           Doer
-	logger         Logger
-	now            func() time.Time
-	refreshRequest chan (struct{})
+	ctx              context.Context
+	identityURL      string
+	http             Doer
+	logger           Logger
+	now              func() time.Time
+	refreshRateLimit time.Duration
+	refreshRequest   chan (struct{})
 
 	controllerID     string
 	assertionMethods map[string]crypto.PublicKey
 }
 
-func getDID(ctx context.Context, logger Logger, httpClient *http.Client, identityURL string) *didClient {
+func getDID(ctx context.Context, logger Logger, httpClient Doer, identityURL string) *didClient {
 	client := &didClient{
-		ctx:         ctx,
-		identityURL: identityURL,
-		http:        httpClient,
-		logger:      logger,
-		now:         time.Now,
+		ctx:              ctx,
+		identityURL:      identityURL,
+		http:             httpClient,
+		logger:           logger,
+		now:              time.Now,
+		refreshRateLimit: refreshRateLimit,
 		// only allow a single request to be waiting
 		refreshRequest: make(chan struct{}, 1),
 	}
@@ -83,32 +85,29 @@ func (c *didClient) ForKID(kid string) (crypto.PublicKey, error) {
 
 // refresh updates the did documents.
 func (c *didClient) refresh() (time.Duration, error) {
+	const errRefresh = time.Minute
+
 	ctx, cancel := context.WithTimeout(c.ctx, refreshTimeout)
 	defer cancel()
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, c.identityURL+didDocumentEndpoint, nil)
 	if err != nil {
-		return 0, err
+		return errRefresh, err
 	}
 
 	resp, err := c.http.Do(req)
 	if err != nil {
-		return 0, err
+		return errRefresh, err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return 0, fmt.Errorf("unexpected response status code %d for %s", resp.StatusCode, c.identityURL+didDocumentEndpoint)
-	}
-
-	maxAge, ok := parseCacheControl(resp.Header.Get("Cache-Control"))
-	if !ok {
-		maxAge = refreshInterval
+		return errRefresh, fmt.Errorf("unexpected response status code %d for %s", resp.StatusCode, c.identityURL+didDocumentEndpoint)
 	}
 
 	var document didDocument
 	if err := json.NewDecoder(resp.Body).Decode(&document); err != nil {
-		return 0, err
+		return errRefresh, err
 	}
 
 	assertionMethods := map[string]crypto.PublicKey{}
@@ -116,7 +115,7 @@ func (c *didClient) refresh() (time.Duration, error) {
 	for _, method := range document.AssertionMethods {
 		jwk, err := jwkset.NewJWKFromMarshal(method.PublicKeyJWK, jwkset.JWKMarshalOptions{}, jwkset.JWKValidateOptions{})
 		if err != nil {
-			return 0, fmt.Errorf("could not unmarshal public key jwk for %s: %w", method.ID, err)
+			return errRefresh, fmt.Errorf("could not unmarshal public key jwk for %s: %w", method.ID, err)
 		}
 
 		assertionMethods[method.ID] = jwk.Key().(crypto.PublicKey)
@@ -125,7 +124,7 @@ func (c *didClient) refresh() (time.Duration, error) {
 	c.controllerID = document.ID
 	c.assertionMethods = assertionMethods
 
-	return maxAge, nil
+	return parseCacheControl(resp.Header.Get("Cache-Control")), nil
 }
 
 // requestRefresh will request that the DID document is refreshed, if no other request is waiting
@@ -144,21 +143,20 @@ func (c *didClient) backgroundRefresh() {
 	)
 
 	for {
-		refreshIn, err = c.refresh()
-		if err != nil {
-			c.logger.WarnContext(c.ctx, "problem refreshing did document", slog.Any("err", err.Error()))
-			refreshIn = time.Minute
-		}
-		lastRefresh = c.now()
-
 		select {
 		case <-time.After(refreshIn):
 			c.requestRefresh()
 
 		case <-c.refreshRequest:
-			if lastRefresh.Add(refreshRateLimit).After(c.now()) {
+			if lastRefresh.Add(c.refreshRateLimit).After(c.now()) {
 				continue
 			}
+
+			refreshIn, err = c.refresh()
+			if err != nil {
+				c.logger.WarnContext(c.ctx, "problem refreshing did document", slog.Any("err", err.Error()))
+			}
+			lastRefresh = c.now()
 
 		case <-c.ctx.Done():
 			return
@@ -166,7 +164,7 @@ func (c *didClient) backgroundRefresh() {
 	}
 }
 
-func parseCacheControl(s string) (maxAge time.Duration, ok bool) {
+func parseCacheControl(s string) time.Duration {
 	for _, directive := range strings.Split(s, ",") {
 		key, val, _ := strings.Cut(strings.TrimSpace(directive), "=")
 		switch key {
@@ -176,9 +174,9 @@ func parseCacheControl(s string) (maxAge time.Duration, ok bool) {
 				continue
 			}
 
-			return time.Duration(i) * time.Second, true
+			return time.Duration(i) * time.Second
 		}
 	}
 
-	return 0, false
+	return refreshInterval
 }
--- internal/onelogin/did_test.go
+++ internal/onelogin/did_test.go
@@ -14,11 +14,11 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
-	"github.com/stretchr/testify/require"
 )
 
-func TestDIDClientRefresh(t *testing.T) {
-	const body = `{
+var (
+	validKID         = "did:web:identity.integration.account.gov.uk#c9f8da1c87525bb41653583c2d05274e85805ab7d0abc58376c7128129daa936"
+	validDIDDocument = `{
 	"@context" : [ "https://www.w3.org/ns/did/v1", "https://w3id.org/security/jwk/v1" ],
 	"id" : "did:web:identity.integration.account.gov.uk",
 	"assertionMethod" : [ {
@@ -35,35 +35,209 @@ func TestDIDClientRefresh(t *testing.T) {
 	} ]
 }`
 
+	validXBytes, _ = base64.RawURLEncoding.DecodeString("NPGA7cyIKtH1nz2CJIH14s9_CtC93NwdCQcEi-ADvxg")
+	validYBytes, _ = base64.RawURLEncoding.DecodeString("2cTdmHAmZjighly34lXcxEw50cbKFV7FTOdZKhOG7ps")
+	validPublicKey = &ecdsa.PublicKey{
+		Curve: elliptic.P256(),
+		X:     new(big.Int).SetBytes(validXBytes),
+		Y:     new(big.Int).SetBytes(validYBytes),
+	}
+)
+
+func TestGetDIDWhenRefreshAfterCacheControlDuration(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	doer := newMockDoer(t)
+	doer.EXPECT().
+		Do(mock.Anything).
+		Return(&http.Response{
+			StatusCode: http.StatusOK,
+			Header: http.Header{
+				"Cache-Control": {"max-age=1"},
+			},
+			Body: io.NopCloser(strings.NewReader(validDIDDocument)),
+		}, nil).
+		Once()
+	doer.EXPECT().
+		Do(mock.Anything).
+		Return(&http.Response{
+			StatusCode: http.StatusOK,
+			Body:       io.NopCloser(strings.NewReader(validDIDDocument)),
+		}, nil).
+		Run(func(req *http.Request) {
+			// this needs to run after the doer call is finished, hence this go routine to cancel() later
+			go func() {
+				time.Sleep(time.Millisecond)
+				cancel()
+			}()
+		}).
+		Once()
+
+	logger := newMockLogger(t)
+
+	client := getDID(ctx, logger, doer, "identity-url")
+	client.refreshRateLimit = time.Duration(0)
+
+	select {
+	case <-ctx.Done():
+		key, err := client.ForKID(validKID)
+		assert.Nil(t, err)
+		assert.NotNil(t, key)
+	case <-time.After(5 * time.Second):
+		t.Log("test timed out")
+		t.Fail()
+	}
+}
+
+func TestGetDIDWhenRefreshAfterError(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	doer := newMockDoer(t)
+	doer.EXPECT().
+		Do(mock.Anything).
+		Return(&http.Response{
+			StatusCode: http.StatusBadRequest,
+			Body:       io.NopCloser(strings.NewReader("")),
+		}, nil).
+		Run(func(req *http.Request) {
+			// this needs to run after the doer call is finished, hence this go routine to cancel() later
+			go func() {
+				time.Sleep(time.Millisecond)
+				cancel()
+			}()
+		}).
+		Once()
+
+	logger := newMockLogger(t)
+	logger.EXPECT().
+		WarnContext(ctx, "problem refreshing did document", mock.Anything)
+
+	client := getDID(ctx, logger, doer, "identity-url")
+	client.refreshRateLimit = time.Duration(0)
+
+	select {
+	case <-ctx.Done():
+		_, err := client.ForKID(validKID)
+		assert.Equal(t, ErrConfigurationMissing, err)
+	case <-time.After(5 * time.Second):
+		t.Log("test timed out")
+		t.Fail()
+	}
+}
+
+func TestGetDIDWhenRefreshForced(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	var client *didClient
+
+	doer := newMockDoer(t)
+	doer.EXPECT().
+		Do(mock.Anything).
+		Return(&http.Response{
+			StatusCode: http.StatusOK,
+			Body:       io.NopCloser(strings.NewReader(validDIDDocument)),
+		}, nil).
+		Run(func(req *http.Request) {
+			client.requestRefresh()
+		}).
+		Once()
+	doer.EXPECT().
+		Do(mock.Anything).
+		Return(&http.Response{
+			StatusCode: http.StatusOK,
+			Body:       io.NopCloser(strings.NewReader(validDIDDocument)),
+		}, nil).
+		Run(func(req *http.Request) {
+			// this needs to run after the doer call is finished, hence this go routine to cancel() later
+			go func() {
+				time.Sleep(time.Millisecond)
+				cancel()
+			}()
+		}).
+		Once()
+
+	logger := newMockLogger(t)
+
+	client = getDID(ctx, logger, doer, "identity-url")
+	client.refreshRateLimit = time.Duration(0)
+
+	select {
+	case <-ctx.Done():
+		key, err := client.ForKID(validKID)
+		assert.Nil(t, err)
+		assert.NotNil(t, key)
+	case <-time.After(5 * time.Second):
+		t.Log("test timed out")
+		t.Fail()
+	}
+}
+
+func TestGetDIDWhenRefreshRateLimited(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	var client *didClient
+
+	doer := newMockDoer(t)
+	doer.EXPECT().
+		Do(mock.Anything).
+		Return(&http.Response{
+			StatusCode: http.StatusOK,
+			Body:       io.NopCloser(strings.NewReader(validDIDDocument)),
+		}, nil).
+		Run(func(req *http.Request) {
+			client.requestRefresh()
+			// this needs to run after the doer call is finished, hence this go routine to cancel() later
+			go func() {
+				time.Sleep(50 * time.Millisecond)
+				cancel()
+			}()
+		}).
+		Once()
+
+	logger := newMockLogger(t)
+
+	client = getDID(ctx, logger, doer, "identity-url")
+	client.refreshRateLimit = time.Second
+
+	select {
+	case <-ctx.Done():
+		key, err := client.ForKID(validKID)
+		assert.Nil(t, err)
+		assert.NotNil(t, key)
+	case <-time.After(5 * time.Second):
+		t.Log("test timed out")
+		t.Fail()
+	}
+}
+
+func TestDIDClientRefresh(t *testing.T) {
 	doer := newMockDoer(t)
 	doer.EXPECT().
 		Do(mock.MatchedBy(func(req *http.Request) bool {
-			return true
+			return assert.Equal(t, http.MethodGet, req.Method) &&
+				assert.Equal(t, "identity-url/.well-known/did.json", req.URL.String())
 		})).
 		Return(&http.Response{
 			StatusCode: http.StatusOK,
 			Header: http.Header{
 				"Cache-Control": {"max-age=3600, private"},
 			},
-			Body: io.NopCloser(strings.NewReader(body)),
+			Body: io.NopCloser(strings.NewReader(validDIDDocument)),
 		}, nil)
 
-	client := &didClient{ctx: context.Background(), http: doer}
-
-	xbytes, _ := base64.RawURLEncoding.DecodeString("NPGA7cyIKtH1nz2CJIH14s9_CtC93NwdCQcEi-ADvxg")
-	ybytes, _ := base64.RawURLEncoding.DecodeString("2cTdmHAmZjighly34lXcxEw50cbKFV7FTOdZKhOG7ps")
+	client := &didClient{ctx: context.Background(), http: doer, identityURL: "identity-url"}
 
 	refreshIn, err := client.refresh()
 	assert.Nil(t, err)
 	assert.Equal(t, time.Hour, refreshIn)
 
-	key, err := client.ForKID("did:web:identity.integration.account.gov.uk#c9f8da1c87525bb41653583c2d05274e85805ab7d0abc58376c7128129daa936")
+	key, err := client.ForKID(validKID)
 	assert.Nil(t, err)
-	assert.Equal(t, &ecdsa.PublicKey{
-		Curve: elliptic.P256(),
-		X:     new(big.Int).SetBytes(xbytes),
-		Y:     new(big.Int).SetBytes(ybytes),
-	}, key)
+	assert.Equal(t, validPublicKey, key)
 }
 
 func TestDIDClientRefreshWhenDoerError(t *testing.T) {
@@ -76,7 +250,7 @@ func TestDIDClientRefreshWhenDoerError(t *testing.T) {
 
 	timeout, err := client.refresh()
 
-	assert.Equal(t, time.Duration(0), timeout)
+	assert.Equal(t, time.Minute, timeout)
 	assert.Error(t, err)
 }
 
@@ -93,8 +267,8 @@ func TestDIDClientRefreshWhenUnexpectedStatusCode(t *testing.T) {
 
 	timeout, err := client.refresh()
 
-	assert.Equal(t, time.Duration(0), timeout)
-	require.EqualError(t, err, "unexpected response status code 400 for http://example.com/.well-known/did.json")
+	assert.Equal(t, time.Minute, timeout)
+	assert.EqualError(t, err, "unexpected response status code 400 for http://example.com/.well-known/did.json")
 }
 
 func TestDIDClientRefreshWhenCannotUnmarshalPublicKey(t *testing.T) {
@@ -132,8 +306,8 @@ func TestDIDClientRefreshWhenCannotUnmarshalPublicKey(t *testing.T) {
 
 	timeout, err := client.refresh()
 
-	assert.Equal(t, time.Duration(0), timeout)
-	require.EqualError(t, err, "could not unmarshal public key jwk for did:web:identity.integration.account.gov.uk#c9f8da1c87525bb41653583c2d05274e85805ab7d0abc58376c7128129daa936: failed to unmarshal JSON Web Key: unsupported key: some (kty)")
+	assert.Equal(t, time.Minute, timeout)
+	assert.EqualError(t, err, "could not unmarshal public key jwk for did:web:identity.integration.account.gov.uk#c9f8da1c87525bb41653583c2d05274e85805ab7d0abc58376c7128129daa936: failed to unmarshal JSON Web Key: unsupported key: some (kty)")
 }
 
 func TestDIDClientForKIDWhenNoControllerID(t *testing.T) {
@@ -149,7 +323,7 @@ func TestDIDClientForKIDWhenMalformedKID(t *testing.T) {
 
 	_, err := client.ForKID("not-a-valid-kid")
 
-	require.EqualError(t, err, "malformed kid missing '#'")
+	assert.EqualError(t, err, "malformed kid missing '#'")
 }
 
 func TestDIDClientForKIDWhenUnexpectedControllerID(t *testing.T) {
@@ -157,7 +331,7 @@ func TestDIDClientForKIDWhenUnexpectedControllerID(t *testing.T) {
 
 	_, err := client.ForKID("did:web:identity.integration.account.gov.uk#c9f8da1c87525bb41653583c2d05274e85805ab7d0abc58376c7128129daa936")
 
-	require.EqualError(t, err, "controller id does not match: unexpected-controller-id != did:web:identity.integration.account.gov.uk")
+	assert.EqualError(t, err, "controller id does not match: unexpected-controller-id != did:web:identity.integration.account.gov.uk")
 }
 
 func TestDIDClientForKIDWhenMissingJWKForKID(t *testing.T) {
@@ -165,5 +339,31 @@ func TestDIDClientForKIDWhenMissingJWKForKID(t *testing.T) {
 
 	_, err := client.ForKID("did:web:identity.integration.account.gov.uk#c9f8da1c87525bb41653583c2d05274e85805ab7d0abc58376c7128129daa936")
 
-	require.EqualError(t, err, "missing jwk for kid did:web:identity.integration.account.gov.uk#c9f8da1c87525bb41653583c2d05274e85805ab7d0abc58376c7128129daa936")
+	assert.EqualError(t, err, "missing jwk for kid did:web:identity.integration.account.gov.uk#c9f8da1c87525bb41653583c2d05274e85805ab7d0abc58376c7128129daa936")
+}
+
+func TestParseCacheControl(t *testing.T) {
+	testcases := map[string]struct {
+		value    string
+		expected time.Duration
+	}{
+		"valid": {
+			value:    "max-age=300",
+			expected: 300 * time.Second,
+		},
+		"some valid": {
+			value:    "ok, max-age=what , max-age=300",
+			expected: 300 * time.Second,
+		},
+		"invalid": {
+			value:    "heyyyy",
+			expected: refreshInterval,
+		},
+	}
+
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, tc.expected, parseCacheControl(tc.value))
+		})
+	}
 }
