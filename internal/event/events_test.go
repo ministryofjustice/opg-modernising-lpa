3a81a2e43b466ecd97afbdfb1a7ce331700c2349
Joshua Hawxwell <m@hawx.me>
2024-09-12 15:26:12 +0100

    Update event tests

--- internal/event/events_test.go
+++ internal/event/events_test.go
@@ -2,6 +2,7 @@ package event
 
 import (
 	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 	"time"
@@ -10,86 +11,123 @@ import (
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/date"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/place"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/random"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/uid"
 	"github.com/stretchr/testify/assert"
 	"github.com/xeipuuv/gojsonschema"
 )
 
-func TestEventSchema(t *testing.T) {
-	eventTypes := map[string]map[string]any{
-		"application-updated": {
-			"valid": ApplicationUpdated{
-				UID:       "M-0000-0000-0000",
-				Type:      "personal-welfare",
-				CreatedAt: time.Now(),
-				Donor: ApplicationUpdatedDonor{
-					FirstNames:  "syz",
-					LastName:    "syz",
-					DateOfBirth: date.New("2000", "01", "01"),
-					Address: place.Address{
-						Line1:      "line1",
-						Line2:      "line2",
-						Line3:      "line3",
-						TownOrCity: "townOrCity",
-						Postcode:   "F1 1FF",
-						Country:    "GB",
-					},
+var eventTests = map[string]map[string]any{
+	"application-updated": {
+		"valid": ApplicationUpdated{
+			UID:       "M-0000-0000-0000",
+			Type:      "personal-welfare",
+			CreatedAt: time.Now(),
+			Donor: ApplicationUpdatedDonor{
+				FirstNames:  "syz",
+				LastName:    "syz",
+				DateOfBirth: date.New("2000", "01", "01"),
+				Address: place.Address{
+					Line1:      "line1",
+					Line2:      "line2",
+					Line3:      "line3",
+					TownOrCity: "townOrCity",
+					Postcode:   "F1 1FF",
+					Country:    "GB",
 				},
 			},
 		},
-		"reduced-fee-requested": {
-			"upload": ReducedFeeRequested{
-				UID:         "M-0000-0000-0000",
-				RequestType: "NoFee",
-				Evidence: []Evidence{
-					{Path: "M-0000-0000-0000/evidence/a-uid", Filename: "a-file.pdf"},
-					{Path: "M-0000-0000-0000/evidence/b-uid", Filename: "b-file.pdf"},
-				},
-				EvidenceDelivery: "upload",
+	},
+	"reduced-fee-requested": {
+		"upload": ReducedFeeRequested{
+			UID:         "M-0000-0000-0000",
+			RequestType: "NoFee",
+			Evidence: []Evidence{
+				{Path: "M-0000-0000-0000/evidence/a-uid", Filename: "a-file.pdf"},
+				{Path: "M-0000-0000-0000/evidence/b-uid", Filename: "b-file.pdf"},
 			},
-			"post": ReducedFeeRequested{
-				UID:              "M-0000-0000-0000",
-				RequestType:      "NoFee",
-				EvidenceDelivery: "post",
+			EvidenceDelivery: "upload",
+		},
+		"post": ReducedFeeRequested{
+			UID:              "M-0000-0000-0000",
+			RequestType:      "NoFee",
+			EvidenceDelivery: "post",
+		},
+	},
+	"notification-sent": {
+		"valid": NotificationSent{
+			UID:            "M-0000-0000-0000",
+			NotificationID: random.UuidString(),
+		},
+	},
+	"paper-form-requested": {
+		"certificate provider": PaperFormRequested{
+			UID:       "M-0000-0000-0000",
+			ActorUID:  actoruid.Prefixed(actoruid.New()),
+			ActorType: "certificateProvider",
+		},
+		"attorney": PaperFormRequested{
+			UID:       "M-0000-0000-0000",
+			ActorUID:  actoruid.Prefixed(actoruid.New()),
+			ActorType: "attorney",
+		},
+		"replacement attorney": PaperFormRequested{
+			UID:       "M-0000-0000-0000",
+			ActorUID:  actoruid.Prefixed(actoruid.New()),
+			ActorType: "replacementAttorney",
+		},
+		"trust corporation": PaperFormRequested{
+			UID:       "M-0000-0000-0000",
+			ActorUID:  actoruid.Prefixed(actoruid.New()),
+			ActorType: "trustCorporation",
+		},
+		"replacement trust corporation": PaperFormRequested{
+			UID:       "M-0000-0000-0000",
+			ActorUID:  actoruid.Prefixed(actoruid.New()),
+			ActorType: "replacementTrustCorporation",
+		},
+	},
+	"application-deleted": {
+		"valid": ApplicationDeleted{
+			UID: "M-0000-0000-0000",
+		},
+	},
+	"attorney-started": {
+		"valid": AttorneyStarted{
+			LpaUID:   "M-0000-0000-0000",
+			ActorUID: actoruid.Prefixed(actoruid.New()),
+		},
+	},
+	"certificate-provider-started": {
+		"valid": CertificateProviderStarted{
+			UID: "M-0000-0000-0000",
+		},
+	},
+	"payment-received": {
+		"valid": PaymentReceived{
+			UID:       "M-0000-0000-0000",
+			PaymentID: "123",
+			Amount:    5,
+		},
+	},
+	"uid-requested": {
+		"valid": UidRequested{
+			LpaID:          "ffec5e7a-9cea-4e46-a99b-6c086fbf1a27",
+			DonorSessionID: "blah",
+			OrganisationID: "blahhh",
+			Type:           "pfa",
+			Donor: uid.DonorDetails{
+				Name:     "hey",
+				Dob:      date.Today(),
+				Postcode: "W1 1AA",
 			},
 		},
-		"notification-sent": {
-			"valid": NotificationSent{
-				UID:            "M-0000-0000-0000",
-				NotificationID: random.UuidString(),
-			},
-		},
-		"paper-form-requested": {
-			"certificate provider": PaperFormRequested{
-				UID:       "M-0000-0000-0000",
-				ActorUID:  actoruid.New(),
-				ActorType: "certificateProvider",
-			},
-			"attorney": PaperFormRequested{
-				UID:       "M-0000-0000-0000",
-				ActorUID:  actoruid.New(),
-				ActorType: "attorney",
-			},
-			"replacement attorney": PaperFormRequested{
-				UID:       "M-0000-0000-0000",
-				ActorUID:  actoruid.New(),
-				ActorType: "replacementAttorney",
-			},
-			"trust corporation": PaperFormRequested{
-				UID:       "M-0000-0000-0000",
-				ActorUID:  actoruid.New(),
-				ActorType: "trustCorporation",
-			},
-			"replacement trust corporation": PaperFormRequested{
-				UID:       "M-0000-0000-0000",
-				ActorUID:  actoruid.New(),
-				ActorType: "replacementTrustCorporation",
-			},
-		},
-	}
+	},
+}
 
+func TestEventSchema(t *testing.T) {
 	dir, _ := os.Getwd()
 
-	for eventType, tcs := range eventTypes {
+	for eventType, tcs := range eventTests {
 		for name, event := range tcs {
 			t.Run(eventType+"/"+name, func(t *testing.T) {
 				schemaLoader := gojsonschema.NewReferenceLoader("file:///" + dir + "/testdata/" + eventType + ".json")
@@ -109,4 +147,21 @@ func TestEventSchema(t *testing.T) {
 			})
 		}
 	}
+}
+
+func TestEventsAllTested(t *testing.T) {
+	err := filepath.WalkDir("testdata", func(path string, d os.DirEntry, err error) error {
+		if d.IsDir() {
+			return nil
+		}
+
+		name := strings.TrimSuffix(d.Name(), ".json")
+		if _, ok := eventTests[name]; !ok {
+			t.Fail()
+			t.Log("missing testcase for event:", name)
+		}
+
+		return nil
+	})
+	assert.Nil(t, err)
 }
