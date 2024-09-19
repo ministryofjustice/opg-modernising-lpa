162a332c590db37f140a78c0868746ef2e5a1ead
Alex Saunders <acsauk@gmail.com>
2024-09-19 11:03:49 +0100

    MLPAB-2433 enable low confidence feature flag for cp

--- internal/certificateprovider/certificateproviderpage/register.go
+++ internal/certificateprovider/certificateproviderpage/register.go
@@ -48,6 +48,7 @@ type CertificateProviderStore interface {
 
 type OneLoginClient interface {
 	AuthCodeURL(state, nonce, locale string, identity bool) (string, error)
+	EnableLowConfidenceFeatureFlag(ctx context.Context) error
 	Exchange(ctx context.Context, code, nonce string) (idToken, accessToken string, err error)
 	UserInfo(ctx context.Context, accessToken string) (onelogin.UserInfo, error)
 	ParseIdentityClaim(ctx context.Context, userInfo onelogin.UserInfo) (identity.UserData, error)
