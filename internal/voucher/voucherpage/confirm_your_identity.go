1ac27081be6ffdebe303242c99b0c9a5cd8e3bad
Alex Saunders <acsauk@gmail.com>
2024-09-19 16:55:50 +0100

    MLPAB-2433 more test fix

--- internal/voucher/voucherpage/confirm_your_identity.go
+++ internal/voucher/voucherpage/confirm_your_identity.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/ministryofjustice/opg-go-common/template"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/appcontext"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/lpastore/lpadata"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/validation"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/voucher/voucherdata"
 )
@@ -13,13 +14,20 @@ type confirmYourIdentityData struct {
 	App                  appcontext.Data
 	Errors               validation.List
 	LowConfidenceEnabled bool
+	Lpa                  *lpadata.Lpa
 }
 
-func ConfirmYourIdentity(tmpl template.Template, lowConfidenceEnabled bool) Handler {
+func ConfirmYourIdentity(tmpl template.Template, lowConfidenceEnabled bool, lpaStoreResolvingService LpaStoreResolvingService) Handler {
 	return func(appData appcontext.Data, w http.ResponseWriter, r *http.Request, _ *voucherdata.Provided) error {
+		lpa, err := lpaStoreResolvingService.Get(r.Context())
+		if err != nil {
+			return err
+		}
+
 		return tmpl(w, &confirmYourIdentityData{
 			App:                  appData,
 			LowConfidenceEnabled: lowConfidenceEnabled,
+			Lpa:                  lpa,
 		})
 	}
 }
--- internal/voucher/voucherpage/confirm_your_identity_test.go
+++ internal/voucher/voucherpage/confirm_your_identity_test.go
@@ -6,6 +6,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/lpastore/lpadata"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
@@ -20,16 +21,24 @@ func TestConfirmYourIdentity(t *testing.T) {
 			w := httptest.NewRecorder()
 			r, _ := http.NewRequest(http.MethodGet, "/", nil)
 
+			lpa := &lpadata.Lpa{LpaID: "lpa-id"}
+
+			resolvingService := newMockLpaStoreResolvingService(t)
+			resolvingService.EXPECT().
+				Get(r.Context()).
+				Return(lpa, nil)
+
 			template := newMockTemplate(t)
 			template.EXPECT().
 				Execute(w, &confirmYourIdentityData{
 					App:                  testAppData,
 					Errors:               nil,
 					LowConfidenceEnabled: enabled,
+					Lpa:                  lpa,
 				}).
 				Return(nil)
 
-			err := ConfirmYourIdentity(template.Execute, enabled)(testAppData, w, r, nil)
+			err := ConfirmYourIdentity(template.Execute, enabled, resolvingService)(testAppData, w, r, nil)
 			resp := w.Result()
 
 			assert.Nil(t, err)
@@ -38,16 +47,37 @@ func TestConfirmYourIdentity(t *testing.T) {
 	}
 }
 
+func TestConfirmYourIdentityResolvingServiceError(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodGet, "/", nil)
+
+	resolvingService := newMockLpaStoreResolvingService(t)
+	resolvingService.EXPECT().
+		Get(r.Context()).
+		Return(nil, expectedError)
+
+	err := ConfirmYourIdentity(nil, true, resolvingService)(testAppData, w, r, nil)
+	resp := w.Result()
+
+	assert.Error(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
+
 func TestConfirmYourIdentityTemplateError(t *testing.T) {
 	w := httptest.NewRecorder()
 	r, _ := http.NewRequest(http.MethodGet, "/", nil)
+
+	resolvingService := newMockLpaStoreResolvingService(t)
+	resolvingService.EXPECT().
+		Get(r.Context()).
+		Return(nil, nil)
 
 	template := newMockTemplate(t)
 	template.EXPECT().
 		Execute(mock.Anything, mock.Anything).
 		Return(expectedError)
 
-	err := ConfirmYourIdentity(template.Execute, true)(testAppData, w, r, nil)
+	err := ConfirmYourIdentity(template.Execute, true, resolvingService)(testAppData, w, r, nil)
 	resp := w.Result()
 
 	assert.Error(t, err)
