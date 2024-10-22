6abb042c21a5eac5bb054a49b6af495d162d3d22
Joshua Hawxwell <m@hawx.me>
2024-10-22 14:07:52 +0100

    Handle SQS events with lambda

--- terraform/environment/region/modules/event_received/data_sources.tf
+++ terraform/environment/region/modules/event_received/data_sources.tf
@@ -6,10 +6,6 @@ data "aws_region" "current" {
   provider = aws.region
 }
 
-data "aws_caller_identity" "current" {
-  provider = aws.region
-}
-
 data "aws_kms_alias" "cloudwatch_application_logs_encryption" {
   name     = "alias/${data.aws_default_tags.current.tags.application}_cloudwatch_application_logs_encryption"
   provider = aws.region
