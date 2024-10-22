c53a3c5ab0e0941e2d1b258b3706beabe9218542
Joshua Hawxwell <m@hawx.me>
2024-10-22 14:07:52 +0100

    Handle SQS events with lambda

--- terraform/environment/region/modules/event_received/main.tf
+++ terraform/environment/region/modules/event_received/main.tf
@@ -98,7 +98,7 @@ resource "aws_lambda_event_source_mapping" "reveive_events_mapping" {
   event_source_arn = aws_sqs_queue.receive_events_queue.arn
   enabled          = true
   function_name    = module.event_received.lambda.arn
-  batch_size       = 1
+  batch_size       = 10
   provider         = aws.region
 }
 
