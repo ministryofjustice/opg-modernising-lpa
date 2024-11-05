1dfb5e154cccb1676d4f3785dfe1ed60b68013ae
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-11-05 10:06:20 +0000

    use module output for opensearch vpc endpoint

--- terraform/account/region/outputs.tf
+++ terraform/account/region/outputs.tf
@@ -1,3 +1,7 @@
 output "ecs_autoscaling_alarm_sns_topic" {
   value = aws_sns_topic.ecs_autoscaling_alarms
 }
+
+output "opensearch_lpas_collection_vpc_endpoint" {
+  value = aws_opensearchserverless_vpc_endpoint.lpas_collection_vpc_endpoint
+}
