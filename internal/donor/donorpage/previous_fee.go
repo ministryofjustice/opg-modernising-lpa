3282c91ea0ff09e5702bd8de1a1f20b6eac977c7
Joshua Hawxwell <m@hawx.me>
2024-11-19 08:51:38 +0000

    Add generic form for enums

--- internal/donor/donorpage/previous_fee.go
+++ internal/donor/donorpage/previous_fee.go
@@ -7,35 +7,31 @@ import (
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/appcontext"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/donor"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/donor/donordata"
-	"github.com/ministryofjustice/opg-modernising-lpa/internal/page"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/form"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/pay"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/validation"
 )
 
 type previousFeeData struct {
-	App     appcontext.Data
-	Errors  validation.List
-	Form    *previousFeeForm
-	Options pay.PreviousFeeOptions
+	App    appcontext.Data
+	Errors validation.List
+	Form   *form.SelectForm[pay.PreviousFee, pay.PreviousFeeOptions, *pay.PreviousFee]
 }
 
 func PreviousFee(tmpl template.Template, payer Handler, donorStore DonorStore) Handler {
 	return func(appData appcontext.Data, w http.ResponseWriter, r *http.Request, provided *donordata.Provided) error {
 		data := &previousFeeData{
-			App: appData,
-			Form: &previousFeeForm{
-				PreviousFee: provided.PreviousFee,
-			},
-			Options: pay.PreviousFeeValues,
+			App:  appData,
+			Form: form.NewSelectForm(provided.PreviousFee, pay.PreviousFeeValues, "howMuchYouPreviouslyPaid"),
 		}
 
 		if r.Method == http.MethodPost {
-			data.Form = readPreviousFeeForm(r)
+			data.Form.Read(r)
 			data.Errors = data.Form.Validate()
 
 			if data.Errors.None() {
-				if provided.PreviousFee != data.Form.PreviousFee {
-					provided.PreviousFee = data.Form.PreviousFee
+				if provided.PreviousFee != data.Form.Selected {
+					provided.PreviousFee = data.Form.Selected
 
 					if err := donorStore.Put(r.Context(), provided); err != nil {
 						return err
@@ -52,25 +48,4 @@ func PreviousFee(tmpl template.Template, payer Handler, donorStore DonorStore) H
 
 		return tmpl(w, data)
 	}
-}
-
-type previousFeeForm struct {
-	PreviousFee pay.PreviousFee
-}
-
-func readPreviousFeeForm(r *http.Request) *previousFeeForm {
-	previousFee, _ := pay.ParsePreviousFee(page.PostFormString(r, "previous-fee"))
-
-	return &previousFeeForm{
-		PreviousFee: previousFee,
-	}
-}
-
-func (f *previousFeeForm) Validate() validation.List {
-	var errors validation.List
-
-	errors.Enum("previous-fee", "howMuchYouPreviouslyPaid", f.PreviousFee,
-		validation.Selected())
-
-	return errors
 }
