dd5f352d9066fccd484272be1a1d86f03a5f9ae1
Alex Saunders <acsauk@gmail.com>
2024-11-20 09:49:33 +0000

    MLPAB-2605 bump timeout

--- docker/localstack/localstack-init.sh
+++ docker/localstack/localstack-init.sh
@@ -106,7 +106,8 @@ awslocal lambda create-function \
   --handler bootstrap \
   --runtime provided.al2023 \
   --role arn:aws:iam::000000000000:role/lambda-role \
-  --zip-file fileb:///etc/scheduled-task-adder.zip
+  --zip-file fileb:///etc/scheduled-task-adder.zip \
+  --timeout 100
 
 awslocal lambda wait function-active-v2 --region eu-west-1 --function-name scheduled-task-adder
 
