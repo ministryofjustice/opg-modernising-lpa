fb945d1e4c2eacb345a41918a7f9f2167dae2a3b
Alex Saunders <acsauk@gmail.com>
2024-11-21 11:41:45 +0000

    MLPAB-2321 persist LPA UID on scheduled tasks, ignore scheduled task if cannot register

--- internal/scheduled/event.go
+++ internal/scheduled/event.go
@@ -20,4 +20,6 @@ type Event struct {
 	TargetLpaKey dynamo.LpaKeyType
 	// TargetLpaOwnerKey is used to specify the target of the action
 	TargetLpaOwnerKey dynamo.LpaOwnerKeyType
+	// TargetLpaUID is the UID of the action target
+	TargetLpaUID string
 }
