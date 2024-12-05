1e79b6161fe563be03a0c73afc64017290419788
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-12-05 10:09:14 +0000

    retain dhcp association

--- terraform/account/region/network.tf
+++ terraform/account/region/network.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source                         = "github.com/ministryofjustice/opg-terraform-aws-firewalled-network?ref=v0.2.6"
+  source                         = "github.com/ministryofjustice/opg-terraform-aws-firewalled-network?ref=v0.2.7"
   cidr                           = var.network_cidr_block
   enable_dns_hostnames           = true
   enable_dns_support             = true
