c3125991beaa3012ee8b4af38d9b0b3b47a4329f
Joshua Hawxwell <m@hawx.me>
2024-12-11 12:44:28 +0000

    Emit lpa-access-granted when an lpa is registered

--- cmd/event-received/lpastore_event_handler_test.go
+++ cmd/event-received/lpastore_event_handler_test.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"testing"
 
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/actor"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/actor/actoruid"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/dashboard/dashboarddata"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/donor/donordata"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/dynamo"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/event"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/lpastore/lpadata"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
@@ -29,6 +35,133 @@ func TestLpaStoreEventHandlerHandleLpaUpdatedWhenChangeTypeNotExpected(t *testin
 
 	err := handler.Handle(ctx, nil, event)
 	assert.Nil(t, err)
+}
+
+func TestLpaStoreEventHandlerHandleLpaUpdatedRegister(t *testing.T) {
+	v := &events.CloudWatchEvent{
+		DetailType: "lpa-updated",
+		Detail:     json.RawMessage(`{"uid":"M-1111-2222-3333","changeType":"REGISTER"}`),
+	}
+
+	lpaStoreClient := newMockLpaStoreClient(t)
+	lpaStoreClient.EXPECT().
+		Lpa(ctx, "M-1111-2222-3333").
+		Return(&lpadata.Lpa{Type: lpadata.LpaTypePersonalWelfare}, nil)
+
+	donorUID := actoruid.New()
+	attorneyUID := actoruid.New()
+	replacementTrustCorporationUID := actoruid.New()
+
+	client := newMockDynamodbClient(t)
+	client.EXPECT().
+		AllByLpaUIDAndPartialSK(ctx, "M-1111-2222-3333", dynamo.SubKey(""), mock.Anything).
+		Return(nil).
+		SetData([]dashboarddata.LpaLink{{
+			SK:        dynamo.SubKey(base64.StdEncoding.EncodeToString([]byte("donor-sub"))),
+			UID:       donorUID,
+			ActorType: actor.TypeDonor,
+		}, {
+			SK:        dynamo.SubKey(base64.StdEncoding.EncodeToString([]byte("attorney-sub"))),
+			UID:       attorneyUID,
+			ActorType: actor.TypeAttorney,
+		}, {
+			SK:        dynamo.SubKey(base64.StdEncoding.EncodeToString([]byte("certificate-provided-sub"))),
+			UID:       actoruid.New(),
+			ActorType: actor.TypeCertificateProvider,
+		}, {
+			SK:        dynamo.SubKey(base64.StdEncoding.EncodeToString([]byte("replacement-trust-sub"))),
+			UID:       replacementTrustCorporationUID,
+			ActorType: actor.TypeReplacementTrustCorporation,
+		}})
+
+	eventClient := newMockEventClient(t)
+	eventClient.EXPECT().
+		SendLpaAccessGranted(ctx, event.LpaAccessGranted{
+			UID:     "M-1111-2222-3333",
+			LpaType: "personal-welfare",
+			Actors: []event.LpaAccessGrantedActor{{
+				SubjectID: "donor-sub",
+				ActorUID:  donorUID.String(),
+			}, {
+				SubjectID: "attorney-sub",
+				ActorUID:  attorneyUID.String(),
+			}, {
+				SubjectID: "replacement-trust-sub",
+				ActorUID:  replacementTrustCorporationUID.String(),
+			}},
+		}).
+		Return(nil)
+
+	factory := newMockFactory(t)
+	factory.EXPECT().DynamoClient().Return(client)
+	factory.EXPECT().LpaStoreClient().Return(lpaStoreClient, nil)
+	factory.EXPECT().EventClient().Return(eventClient)
+
+	handler := &lpastoreEventHandler{}
+
+	err := handler.Handle(ctx, factory, v)
+	assert.Nil(t, err)
+}
+
+func TestLpaStoreEventHandlerHandleLpaUpdatedRegisterWhenErrorCreatingLpaStore(t *testing.T) {
+	v := &events.CloudWatchEvent{
+		DetailType: "lpa-updated",
+		Detail:     json.RawMessage(`{"uid":"M-1111-2222-3333","changeType":"REGISTER"}`),
+	}
+
+	factory := newMockFactory(t)
+	factory.EXPECT().LpaStoreClient().Return(nil, expectedError)
+
+	handler := &lpastoreEventHandler{}
+
+	err := handler.Handle(ctx, factory, v)
+	assert.ErrorIs(t, err, expectedError)
+}
+
+func TestLpaStoreEventHandlerHandleLpaUpdatedRegisterWhenLpaStoreErrors(t *testing.T) {
+	lpaStoreClient := newMockLpaStoreClient(t)
+	lpaStoreClient.EXPECT().
+		Lpa(mock.Anything, mock.Anything).
+		Return(nil, expectedError)
+
+	err := handleRegister(ctx, nil, lpaStoreClient, nil, lpaUpdatedEvent{})
+	assert.ErrorIs(t, err, expectedError)
+}
+
+func TestLpaStoreEventHandlerHandleLpaUpdatedRegisterWhenDynamoErrors(t *testing.T) {
+	lpaStoreClient := newMockLpaStoreClient(t)
+	lpaStoreClient.EXPECT().
+		Lpa(mock.Anything, mock.Anything).
+		Return(&lpadata.Lpa{}, nil)
+
+	client := newMockDynamodbClient(t)
+	client.EXPECT().
+		AllByLpaUIDAndPartialSK(mock.Anything, mock.Anything, mock.Anything, mock.Anything).
+		Return(expectedError)
+
+	err := handleRegister(ctx, client, lpaStoreClient, nil, lpaUpdatedEvent{})
+	assert.ErrorIs(t, err, expectedError)
+}
+
+func TestLpaStoreEventHandlerHandleLpaUpdatedRegisterWhenEventClientErrors(t *testing.T) {
+	lpaStoreClient := newMockLpaStoreClient(t)
+	lpaStoreClient.EXPECT().
+		Lpa(mock.Anything, mock.Anything).
+		Return(&lpadata.Lpa{}, nil)
+
+	client := newMockDynamodbClient(t)
+	client.EXPECT().
+		AllByLpaUIDAndPartialSK(mock.Anything, mock.Anything, mock.Anything, mock.Anything).
+		Return(nil).
+		SetData([]dashboarddata.LpaLink{})
+
+	eventClient := newMockEventClient(t)
+	eventClient.EXPECT().
+		SendLpaAccessGranted(mock.Anything, mock.Anything).
+		Return(expectedError)
+
+	err := handleRegister(ctx, client, lpaStoreClient, eventClient, lpaUpdatedEvent{})
+	assert.ErrorIs(t, err, expectedError)
 }
 
 func TestLpaStoreEventHandlerHandleLpaUpdatedStatutoryWaitingPeriod(t *testing.T) {
