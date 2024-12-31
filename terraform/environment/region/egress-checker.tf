65adeb032cffc66420a68ec6d1d3716495602785
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-12-31 14:02:48 +0000

    pull current tag

--- terraform/environment/region/egress-checker.tf
+++ terraform/environment/region/egress-checker.tf
@@ -1,7 +1,7 @@
 module "egress_checker" {
   source                        = "./modules/egress_checker"
   lambda_function_image_ecr_url = "311462405659.dkr.ecr.eu-west-1.amazonaws.com/egress-checker"
-  lambda_function_image_tag     = "v0.1604.0-MLPAB2748checkrequestsforaddressesnotontheallowlistaredropped.2"
+  lambda_function_image_tag     = var.app_service_container_version
   event_received_lambda_role    = var.iam_roles.event_received_lambda
   vpc_config = {
     subnet_ids         = data.aws_subnet.application[*].id
