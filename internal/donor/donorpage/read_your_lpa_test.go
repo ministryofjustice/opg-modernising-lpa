c34958350762105668db7c5297af7abb832a4801
Joshua Hawxwell <m@hawx.me>
2025-02-06 14:15:47 +0000

    Show warnings on read the lpa when switching language
    Also prevent donor from reading the lpa in a different language

---
+++ internal/donor/donorpage/read_your_lpa_test.go
@@ -0,0 +1,66 @@
+package donorpage
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/appcontext"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/donor/donordata"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/localize"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestReadYourLpa(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodGet, "/", nil)
+
+	donor := &donordata.Provided{Donor: donordata.Donor{LpaLanguagePreference: localize.Cy}}
+	localizer := &localize.Localizer{Lang: localize.Cy}
+
+	bundle := newMockBundle(t)
+	bundle.EXPECT().
+		For(localize.Cy).
+		Return(localizer)
+
+	template := newMockTemplate(t)
+	template.EXPECT().
+		Execute(w, &readYourLpaData{
+			App: testAppData,
+			LpaLanguageApp: appcontext.Data{
+				SessionID:         "session-id",
+				LpaID:             "lpa-id",
+				Lang:              localize.Cy,
+				Localizer:         localizer,
+				LoginSessionEmail: "logged-in@example.com",
+			},
+			Donor: donor,
+		}).
+		Return(nil)
+
+	err := ReadYourLpa(template.Execute, bundle)(testAppData, w, r, donor)
+	resp := w.Result()
+
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
+
+func TestReadYourLpaWhenTemplateErrors(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodGet, "/", nil)
+
+	bundle := newMockBundle(t)
+	bundle.EXPECT().
+		For(mock.Anything).
+		Return(&localize.Localizer{})
+
+	template := newMockTemplate(t)
+	template.EXPECT().
+		Execute(w, mock.Anything).
+		Return(expectedError)
+
+	err := ReadYourLpa(template.Execute, bundle)(testAppData, w, r, &donordata.Provided{})
+
+	assert.Equal(t, expectedError, err)
+}
