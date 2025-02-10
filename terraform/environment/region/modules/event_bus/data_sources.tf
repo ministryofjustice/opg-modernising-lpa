e3a18d2d8b2f524cae6bf63cbf38e8452f19613a
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-02-10 13:51:29 +0000

    add sns topic for dlq alarm and subscribe pagerduty

--- terraform/environment/region/modules/event_bus/data_sources.tf
+++ terraform/environment/region/modules/event_bus/data_sources.tf
@@ -14,3 +14,18 @@ data "aws_default_tags" "current" {
 data "aws_caller_identity" "current" {
   provider = aws.region
 }
+
+data "aws_iam_role" "sns_success_feedback" {
+  name     = "SNSSuccessFeedback"
+  provider = aws.global
+}
+
+data "aws_iam_role" "sns_failure_feedback" {
+  name     = "SNSFailureFeedback"
+  provider = aws.global
+}
+
+data "aws_kms_alias" "sns_kms_key_alias" {
+  name     = "alias/${data.aws_default_tags.current.tags.application}_sns_secret_encryption_key"
+  provider = aws.region
+}
--- terraform/environment/region/s3_antivirus.tf
+++ terraform/environment/region/s3_antivirus.tf
@@ -21,8 +21,3 @@ module "s3_antivirus" {
     aws.region = aws.region
   }
 }
-
-data "aws_sns_topic" "custom_cloudwatch_alarms" {
-  name     = "custom_cloudwatch_alarms"
-  provider = aws.region
-}
