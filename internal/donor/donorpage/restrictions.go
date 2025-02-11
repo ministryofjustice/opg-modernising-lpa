5a930185ec59b051ee9ac3d4e6166fc26e472ee9
Joshua Hawxwell <m@hawx.me>
2025-02-11 15:25:08 +0000

    Validate for no links in textareas

--- internal/donor/donorpage/restrictions.go
+++ internal/donor/donorpage/restrictions.go
@@ -15,23 +15,27 @@ import (
 type restrictionsData struct {
 	App    appcontext.Data
 	Errors validation.List
+	Form   *restrictionsForm
 	Donor  *donordata.Provided
 }
 
 func Restrictions(tmpl template.Template, donorStore DonorStore) Handler {
 	return func(appData appcontext.Data, w http.ResponseWriter, r *http.Request, provided *donordata.Provided) error {
 		data := &restrictionsData{
-			App:   appData,
+			App: appData,
+			Form: &restrictionsForm{
+				Restrictions: provided.Restrictions,
+			},
 			Donor: provided,
 		}
 
 		if r.Method == http.MethodPost {
-			form := readRestrictionsForm(r)
-			data.Errors = form.Validate()
+			data.Form = readRestrictionsForm(r)
+			data.Errors = data.Form.Validate()
 
 			if data.Errors.None() {
 				provided.Tasks.Restrictions = task.StateCompleted
-				provided.Restrictions = form.Restrictions
+				provided.Restrictions = data.Form.Restrictions
 
 				if err := donorStore.Put(r.Context(), provided); err != nil {
 					return err
@@ -60,6 +64,8 @@ func (f *restrictionsForm) Validate() validation.List {
 
 	errors.String("restrictions", "restrictions", f.Restrictions,
 		validation.StringTooLong(10000))
+	errors.String("restrictions", "yourRestrictionsAndConditions", f.Restrictions,
+		validation.NoLinks())
 
 	return errors
 }
