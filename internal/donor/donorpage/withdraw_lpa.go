6a5111579e8b979a25292be42a6035f4a45c7eb4
Joshua Hawxwell <m@hawx.me>
2025-02-12 14:18:14 +0000

    Send email to voucher when lpa is deleted or revoked

--- internal/donor/donorpage/withdraw_lpa.go
+++ internal/donor/donorpage/withdraw_lpa.go
@@ -1,6 +1,7 @@
 package donorpage
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
 	"time"
@@ -8,6 +9,8 @@ import (
 	"github.com/ministryofjustice/opg-go-common/template"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/appcontext"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/donor/donordata"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/localize"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/notify"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/page"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/validation"
 )
@@ -18,9 +21,21 @@ type withdrawLpaData struct {
 	Donor  *donordata.Provided
 }
 
-func WithdrawLpa(tmpl template.Template, donorStore DonorStore, now func() time.Time, lpaStoreClient LpaStoreClient) Handler {
+func WithdrawLpa(tmpl template.Template, donorStore DonorStore, now func() time.Time, lpaStoreClient LpaStoreClient, notifyClient NotifyClient) Handler {
 	return func(appData appcontext.Data, w http.ResponseWriter, r *http.Request, provided *donordata.Provided) error {
 		if r.Method == http.MethodPost {
+			if !provided.VoucherInvitedAt.IsZero() {
+				if err := notifyClient.SendActorEmail(r.Context(), notify.ToVoucher(provided.Voucher), provided.LpaUID, notify.VoucherLpaRevoked{
+					DonorFullName:           provided.Donor.FullName(),
+					DonorFullNamePossessive: appData.Localizer.Possessive(provided.Donor.FullName()),
+					InvitedDate:             appData.Localizer.FormatDate(provided.VoucherInvitedAt),
+					LpaType:                 localize.LowerFirst(appData.Localizer.T(provided.Type.String())),
+					VoucherFullName:         provided.Voucher.FullName(),
+				}); err != nil {
+					return fmt.Errorf("error sending voucher email: %w", err)
+				}
+			}
+
 			provided.WithdrawnAt = now()
 			if err := donorStore.Put(r.Context(), provided); err != nil {
 				return err
