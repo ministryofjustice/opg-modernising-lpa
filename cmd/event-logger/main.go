be5c404c5a7ae8592ccb8f22e36837d70ecee438
Alex Saunders <acsauk@gmail.com>
2025-02-13 17:42:53 +0000

    MLPAB-2830 add sirius event emitting to event-logger, add change confirmed tests to e2e

--- cmd/event-logger/main.go
+++ cmd/event-logger/main.go
@@ -17,7 +17,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
-	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
+	sqstypes "github.com/aws/aws-sdk-go-v2/service/sqs/types"
 	"github.com/ministryofjustice/opg-go-common/env"
 )
 
@@ -34,12 +34,19 @@ const (
 	waitMaxTicks = 10
 )
 
+type sqsClient struct {
+	svc      *sqs.Client
+	queueURL string
+}
+
 type message struct {
 	Time       time.Time
 	Detail     json.RawMessage
 	DetailType string `json:"detail-type"`
 	Source     string
 }
+
+var cfg aws.Config
 
 func main() {
 	var (
@@ -52,7 +59,8 @@ func main() {
 		messages []message
 	)
 
-	cfg, err := config.LoadDefaultConfig(ctx)
+	var err error
+	cfg, err = config.LoadDefaultConfig(ctx)
 	if err != nil {
 		log.Fatal(fmt.Errorf("unable to load SDK config: %w", err))
 	}
@@ -77,7 +85,7 @@ func main() {
 			}
 
 			messageResponse, err := client.ReceiveMessage(ctx, &sqs.ReceiveMessageInput{
-				MessageAttributeNames: []string{string(types.QueueAttributeNameAll)},
+				MessageAttributeNames: []string{string(sqstypes.QueueAttributeNameAll)},
 				QueueUrl:              aws.String(queueURL),
 				MaxNumberOfMessages:   10, // may as well ask for as many as possible
 				VisibilityTimeout:     5,
@@ -92,10 +100,10 @@ func main() {
 				continue
 			}
 
-			var toDelete []types.DeleteMessageBatchRequestEntry
+			var toDelete []sqstypes.DeleteMessageBatchRequestEntry
 
 			for _, m := range messageResponse.Messages {
-				toDelete = append(toDelete, types.DeleteMessageBatchRequestEntry{Id: m.MessageId, ReceiptHandle: m.ReceiptHandle})
+				toDelete = append(toDelete, sqstypes.DeleteMessageBatchRequestEntry{Id: m.MessageId, ReceiptHandle: m.ReceiptHandle})
 
 				var v message
 				if err := json.Unmarshal([]byte(*m.Body), &v); err != nil {
@@ -167,5 +175,102 @@ func main() {
 		fmt.Fprint(w, "</tbody></table></body>")
 	})
 
+	http.HandleFunc("/emit-sirius-event", func(w http.ResponseWriter, r *http.Request) {
+		sqsClient := &sqsClient{
+			svc:      sqs.NewFromConfig(cfg),
+			queueURL: "http://localhost:4566/000000000000/event-bus-queue",
+		}
+
+		switch r.FormValue("detailType") {
+		case "immaterial-change-confirmed":
+			if err = sqsClient.SendImmaterialChangeConfirmed(
+				r.Context(),
+				r.FormValue("uid"),
+				r.FormValue("actorType"),
+				r.FormValue("actorUID"),
+			); err != nil {
+				log.Printf("failed to send immaterial-change-confirmed: %v", err)
+			}
+		case "material-change-confirmed":
+			if err = sqsClient.SendMaterialChangeConfirmed(
+				r.Context(),
+				r.FormValue("uid"),
+				r.FormValue("actorType"),
+				r.FormValue("actorUID"),
+			); err != nil {
+				log.Printf("failed to send material-change-confirmed: %v", err)
+			}
+		default:
+			log.Println("unsupported event type:", r.FormValue("detailType"))
+		}
+
+		log.Println("successfully handled", r.FormValue("detailType"))
+
+	})
+
 	http.ListenAndServe(":"+port, nil)
+}
+
+type CloudWatchEvent struct {
+	Version    string          `json:"version"`
+	ID         string          `json:"id"`
+	DetailType string          `json:"detail-type"`
+	Source     string          `json:"source"`
+	Account    string          `json:"account"`
+	Time       time.Time       `json:"time"`
+	Region     string          `json:"region"`
+	Resources  []string        `json:"resources"`
+	Detail     json.RawMessage `json:"detail"`
+}
+
+type ChangeConfirmedEvent struct {
+	UID       string `json:"uid"`
+	ActorType string `json:"actorType"`
+	ActorUID  string `json:"actorUID"`
+}
+
+func (c sqsClient) SendImmaterialChangeConfirmed(ctx context.Context, lpaUID, actorType, actorUID string) error {
+	detail, _ := json.Marshal(ChangeConfirmedEvent{
+		UID:       lpaUID,
+		ActorType: actorType,
+		ActorUID:  actorUID,
+	})
+
+	return c.SendMessage(ctx, "immaterial-change-confirmed", detail)
+}
+
+func (c sqsClient) SendMaterialChangeConfirmed(ctx context.Context, lpaUID, actorType, actorUID string) error {
+	detail, _ := json.Marshal(ChangeConfirmedEvent{
+		UID:       lpaUID,
+		ActorType: actorType,
+		ActorUID:  actorUID,
+	})
+
+	return c.SendMessage(ctx, "material-change-confirmed", detail)
+}
+
+func (c sqsClient) SendMessage(ctx context.Context, detailType string, detail json.RawMessage) error {
+	v, err := json.Marshal(CloudWatchEvent{
+		Version:    "0",
+		ID:         "63eb7e5f-1f10-4744-bba9-e16d327c3b98",
+		DetailType: detailType,
+		Source:     "opg.poas.sirius",
+		Account:    "653761790766",
+		Time:       time.Now().UTC(),
+		Region:     "eu-west-1",
+		Resources:  []string{},
+		Detail:     detail,
+	})
+	if err != nil {
+		return err
+	}
+
+	if _, err = c.svc.SendMessage(ctx, &sqs.SendMessageInput{
+		QueueUrl:    aws.String(c.queueURL),
+		MessageBody: aws.String(string(v)),
+	}); err != nil {
+		return fmt.Errorf("failed to send %s message: %w", detailType, err)
+	}
+
+	return nil
 }
