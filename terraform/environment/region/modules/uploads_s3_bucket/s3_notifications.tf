a0ad1d7d16986d4b1697f51e6863d1db8f871b4f
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-02-25 12:09:18 +0000

    enable event received trigger from object tagging

--- terraform/environment/region/modules/uploads_s3_bucket/s3_notifications.tf
+++ terraform/environment/region/modules/uploads_s3_bucket/s3_notifications.tf
@@ -26,12 +26,12 @@
 #   provider       = aws.region
 # }
 
-# resource "aws_lambda_permission" "object_tagging" {
-#   statement_id   = "AllowExecutionFromS3BucketObjectTagging"
-#   action         = "lambda:InvokeFunction"
-#   function_name  = var.events_received_lambda_function.function_name
-#   principal      = "s3.amazonaws.com"
-#   source_account = data.aws_caller_identity.current.account_id
-#   source_arn     = aws_s3_bucket.bucket.arn
-#   provider       = aws.region
-# }
+resource "aws_lambda_permission" "object_tagging" {
+  statement_id   = "AllowExecutionFromS3BucketObjectTagging"
+  action         = "lambda:InvokeFunction"
+  function_name  = var.events_received_lambda_function.function_name
+  principal      = "s3.amazonaws.com"
+  source_account = data.aws_caller_identity.current.account_id
+  source_arn     = aws_s3_bucket.bucket.arn
+  provider       = aws.region
+}
