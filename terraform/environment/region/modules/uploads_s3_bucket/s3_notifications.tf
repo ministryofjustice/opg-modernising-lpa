d1fcc927d66d389e570c0ee28a9a64c8deeaf6ec
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-02-25 12:10:19 +0000

    add bucket notification for event received

--- terraform/environment/region/modules/uploads_s3_bucket/s3_notifications.tf
+++ terraform/environment/region/modules/uploads_s3_bucket/s3_notifications.tf
@@ -1,20 +1,14 @@
-# resource "aws_s3_bucket_notification" "bucket_notification" {
-#   bucket = aws_s3_bucket.bucket.id
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.bucket.id
 
-#   lambda_function {
-#     id                  = "bucket-av-scan"
-#     lambda_function_arn = var.s3_antivirus_lambda_function.arn
-#     events              = ["s3:ObjectCreated:Put"]
-#   }
-
-#   lambda_function {
-#     id                  = "av-object-tagging"
-#     lambda_function_arn = var.events_received_lambda_function.arn
-#     events              = ["s3:ObjectTagging:Put"]
-#   }
-#   depends_on = [aws_lambda_permission.av_scan, aws_lambda_permission.object_tagging]
-#   provider   = aws.region
-# }
+  lambda_function {
+    id                  = "av-object-tagging"
+    lambda_function_arn = var.events_received_lambda_function.arn
+    events              = ["s3:ObjectTagging:Put"]
+  }
+  depends_on = [aws_lambda_permission.object_tagging]
+  provider   = aws.region
+}
 
 # resource "aws_lambda_permission" "av_scan" {
 #   statement_id   = "AllowExecutionFromS3BucketAvScan"
