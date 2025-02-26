4fed6d522593811ed58b13ec5d9be464dc47488a
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-02-26 09:41:17 +0000

    upgrade lock

--- terraform/account/region/network.tf
+++ terraform/account/region/network.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source                              = "github.com/ministryofjustice/opg-terraform-aws-firewalled-network?ref=v0.2.15-mlpab2794inp.0"
+  source                              = "github.com/ministryofjustice/opg-terraform-aws-firewalled-network?ref=v0.2.15"
   cidr                                = var.network_cidr_block
   enable_dns_hostnames                = true
   enable_dns_support                  = true
