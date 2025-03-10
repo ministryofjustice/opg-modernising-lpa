2834792f10aad0ce62c16b9f43fce91bf8af15d5
Joshua Hawxwell <m@hawx.me>
2025-03-10 10:09:45 +0000

    Allow donor to resend voucher access code

--- internal/donor/donorpage/resend_voucher_access_code.go
+++ internal/donor/donorpage/resend_voucher_access_code.go
@@ -10,31 +10,23 @@ import (
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/validation"
 )
 
-type checkYourDetailsData struct {
+type resendVoucherAccessCodeData struct {
 	App    appcontext.Data
 	Errors validation.List
-	Donor  *donordata.Provided
 }
 
-func CheckYourDetails(tmpl template.Template, shareCodeSender ShareCodeSender, donorStore DonorStore) Handler {
+func ResendVoucherAccessCode(tmpl template.Template, shareCodeSender ShareCodeSender) Handler {
 	return func(appData appcontext.Data, w http.ResponseWriter, r *http.Request, provided *donordata.Provided) error {
-		if r.Method == http.MethodPost {
-			if provided.Tasks.PayForLpa.IsCompleted() {
-				if err := shareCodeSender.SendVoucherInvite(r.Context(), provided, appData); err != nil {
-					return err
-				}
+		data := &resendVoucherAccessCodeData{
+			App: appData,
+		}
 
-				if err := donorStore.Put(r.Context(), provided); err != nil {
-					return err
-				}
+		if r.Method == http.MethodPost {
+			if err := shareCodeSender.SendVoucherAccessCode(r.Context(), provided, appData); err != nil {
+				return err
 			}
 
 			return donor.PathWeHaveContactedVoucher.Redirect(w, r, appData, provided)
-		}
-
-		data := &checkYourDetailsData{
-			App:   appData,
-			Donor: provided,
 		}
 
 		return tmpl(w, data)
