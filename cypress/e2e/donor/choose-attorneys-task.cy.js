6adc423e9fdeb4038be29eb3e2e2c303c1a62965
Joshua Hawxwell <m@hawx.me>
2025-03-12 08:17:45 +0000

    Updates to fixtures pages
    - Donor correspondent task now sets to No
    - Can select lpa language for attorney and certificate provider
    - Can provide donor email for attorney

--- cypress/e2e/donor/choose-attorneys-task.cy.js
+++ cypress/e2e/donor/choose-attorneys-task.cy.js
@@ -1,143 +1,143 @@
 import { AddressFormAssertions, TestEmail } from "../../support/e2e";
 
 describe('Choose attorneys task', () => {
-  it('is not started when no attorneys are set', () => {
-    cy.visit('/fixtures?redirect=/task-list');
+    it('is not started when no attorneys are set', () => {
+        cy.visit('/fixtures?redirect=/task-list');
 
-    cy.contains('a', 'Choose your attorneys').parent().parent().contains('Not started');
-  });
-
-  it('is in progress if I start adding an attorney', () => {
-    cy.visit('/fixtures?redirect=/task-list');
-    cy.contains('a', 'Choose your attorneys').click();
-    cy.contains('button', 'Continue').click();
-
-    cy.get('#f-first-names').invoke('val', 'John');
-    cy.get('#f-last-name').invoke('val', 'Doe');
-    cy.get('#f-email').invoke('val', TestEmail);
-    cy.get('#f-date-of-birth').invoke('val', '1');
-    cy.get('#f-date-of-birth-month').invoke('val', '2');
-    cy.get('#f-date-of-birth-year').invoke('val', '1990');
-    cy.contains('button', 'Save and continue').click();
-
-    cy.visitLpa('/task-list');
-    cy.contains('a', 'Choose your attorneys').parent().parent().within(() => {
-      cy.contains('In progress');
-      cy.contains('1 added');
+        cy.contains('a', 'Choose your attorneys').parent().parent().contains('Not started');
     });
-  });
 
-  it('is completed if enter an attorneys details using address', () => {
-    cy.visit('/fixtures?redirect=/task-list');
-    cy.contains('a', 'Choose your attorneys').click();
-    cy.contains('button', 'Continue').click();
+    it('is in progress if I start adding an attorney', () => {
+        cy.visit('/fixtures?redirect=/task-list');
+        cy.contains('a', 'Choose your attorneys').click();
+        cy.contains('button', 'Continue').click();
 
-    cy.get('#f-first-names').invoke('val', 'John');
-    cy.get('#f-last-name').invoke('val', 'Doe');
-    cy.get('#f-date-of-birth').invoke('val', '1');
-    cy.get('#f-date-of-birth-month').invoke('val', '2');
-    cy.get('#f-date-of-birth-year').invoke('val', '1990');
-    cy.contains('button', 'Save and continue').click();
+        cy.get('#f-first-names').invoke('val', 'John');
+        cy.get('#f-last-name').invoke('val', 'Doe');
+        cy.get('#f-email').invoke('val', TestEmail);
+        cy.get('#f-date-of-birth').invoke('val', '1');
+        cy.get('#f-date-of-birth-month').invoke('val', '2');
+        cy.get('#f-date-of-birth-year').invoke('val', '1990');
+        cy.contains('button', 'Save and continue').click();
 
-    cy.visitLpa('/task-list');
-    cy.contains('a', 'Choose your attorneys').parent().parent().within(() => {
-      cy.contains('In progress');
-      cy.contains('1 added');
+        cy.visitLpa('/task-list');
+        cy.contains('a', 'Choose your attorneys').parent().parent().within(() => {
+            cy.contains('In progress');
+            cy.contains('1 added');
+        });
     });
-    cy.go('back');
 
-    cy.contains('label', 'Enter a new address').click();
-    cy.contains('button', 'Continue').click();
-    cy.get('#f-lookup-postcode').invoke('val', 'B14 7ED');
-    cy.contains('button', 'Find address').click();
-    cy.get('#f-select-address').select('2 RICHMOND PLACE, BIRMINGHAM, B14 7ED');
-    cy.contains('button', 'Continue').click();
-    cy.contains('button', 'Save and continue').click();
+    it('is completed if enter an attorneys details using address', () => {
+        cy.visit('/fixtures?redirect=/task-list');
+        cy.contains('a', 'Choose your attorneys').click();
+        cy.contains('button', 'Continue').click();
 
-    cy.visitLpa('/task-list');
-    cy.contains('a', 'Choose your attorneys').parent().parent().contains('1 added');
-  });
+        cy.get('#f-first-names').invoke('val', 'John');
+        cy.get('#f-last-name').invoke('val', 'Doe');
+        cy.get('#f-date-of-birth').invoke('val', '1');
+        cy.get('#f-date-of-birth-month').invoke('val', '2');
+        cy.get('#f-date-of-birth-year').invoke('val', '1990');
+        cy.contains('button', 'Save and continue').click();
 
-  it('is in progress if I enter multiple attorneys details', () => {
-    cy.visit('/fixtures?redirect=/task-list&progress=chooseYourAttorneys&attorneys=single');
-    cy.contains('a', 'Choose your attorneys').click();
-    cy.contains('button', 'Continue').click();
+        cy.visitLpa('/task-list');
+        cy.contains('a', 'Choose your attorneys').parent().parent().within(() => {
+            cy.contains('In progress');
+            cy.contains('1 added');
+        });
+        cy.go('back');
 
-    cy.contains('label', 'Yes').click();
-    cy.contains('button', 'Continue').click();
+        cy.contains('label', 'Enter a new address').click();
+        cy.contains('button', 'Continue').click();
+        cy.get('#f-lookup-postcode').invoke('val', 'B14 7ED');
+        cy.contains('button', 'Find address').click();
+        cy.get('#f-select-address').select('2 RICHMOND PLACE, BIRMINGHAM, B14 7ED');
+        cy.contains('button', 'Continue').click();
+        cy.contains('button', 'Save and continue').click();
 
-    cy.get('#f-first-names').invoke('val', 'John');
-    cy.get('#f-last-name').invoke('val', 'Doe');
-    cy.get('#f-email').invoke('val', TestEmail);
-    cy.get('#f-date-of-birth').invoke('val', '1');
-    cy.get('#f-date-of-birth-month').invoke('val', '2');
-    cy.get('#f-date-of-birth-year').invoke('val', '1990');
-    cy.contains('button', 'Save and continue').click();
-
-    cy.visitLpa('/task-list');
-    cy.contains('a', 'Choose your attorneys').parent().parent().within(() => {
-      cy.contains('In progress');
-      cy.contains('2 added');
+        cy.visitLpa('/task-list');
+        cy.contains('a', 'Choose your attorneys').parent().parent().contains('1 added');
     });
-  });
 
-  it('is completed if I enter multiple attorneys details with how they act', () => {
-    cy.visit('/fixtures?redirect=/task-list&progress=chooseYourAttorneys&attorneys=single');
-    cy.contains('a', 'Choose your attorneys').click();
-    cy.contains('button', 'Continue').click();
+    it('is in progress if I enter multiple attorneys details', () => {
+        cy.visit('/fixtures?redirect=/task-list&progress=chooseYourAttorneys&attorneys=single');
+        cy.contains('a', 'Choose your attorneys').click();
+        cy.contains('button', 'Continue').click();
 
-    cy.contains('label', 'Yes').click();
-    cy.contains('button', 'Continue').click();
+        cy.contains('label', 'Yes').click();
+        cy.contains('button', 'Continue').click();
 
-    cy.get('#f-first-names').invoke('val', 'John');
-    cy.get('#f-last-name').invoke('val', 'Doe');
-    cy.get('#f-email').invoke('val', TestEmail);
-    cy.get('#f-date-of-birth').invoke('val', '1');
-    cy.get('#f-date-of-birth-month').invoke('val', '2');
-    cy.get('#f-date-of-birth-year').invoke('val', '1990');
-    cy.contains('button', 'Save and continue').click();
+        cy.get('#f-first-names').invoke('val', 'John');
+        cy.get('#f-last-name').invoke('val', 'Doe');
+        cy.get('#f-email').invoke('val', TestEmail);
+        cy.get('#f-date-of-birth').invoke('val', '1');
+        cy.get('#f-date-of-birth-month').invoke('val', '2');
+        cy.get('#f-date-of-birth-year').invoke('val', '1990');
+        cy.contains('button', 'Save and continue').click();
 
-    cy.contains('label', 'Enter a new address').click();
-    cy.contains('button', 'Continue').click();
-    AddressFormAssertions.assertCanAddAddressManually('Enter address manually', true)
+        cy.visitLpa('/task-list');
+        cy.contains('a', 'Choose your attorneys').parent().parent().within(() => {
+            cy.contains('In progress');
+            cy.contains('2 added');
+        });
+    });
 
-    cy.contains('label', 'No').click();
-    cy.contains('button', 'Continue').click();
+    it('is completed if I enter multiple attorneys details with how they act', () => {
+        cy.visit('/fixtures?redirect=/task-list&progress=chooseYourAttorneys&attorneys=single');
+        cy.contains('a', 'Choose your attorneys').click();
+        cy.contains('button', 'Continue').click();
 
-    cy.get('input[value=jointly-and-severally]').click();
-    cy.contains('button', 'Save and continue').click();
+        cy.contains('label', 'Yes').click();
+        cy.contains('button', 'Continue').click();
 
-    cy.visitLpa('/task-list');
-    cy.contains('a', 'Choose your attorneys').parent().parent().contains('2 added');
-  });
+        cy.get('#f-first-names').invoke('val', 'John');
+        cy.get('#f-last-name').invoke('val', 'Doe');
+        cy.get('#f-email').invoke('val', TestEmail);
+        cy.get('#f-date-of-birth').invoke('val', '1');
+        cy.get('#f-date-of-birth-month').invoke('val', '2');
+        cy.get('#f-date-of-birth-year').invoke('val', '1990');
+        cy.contains('button', 'Save and continue').click();
 
-  it('is completed if I enter multiple attorneys details when jointly', () => {
-    cy.visit('/fixtures?redirect=/task-list&progress=chooseYourAttorneys&attorneys=single');
-    cy.contains('a', 'Choose your attorneys').click();
-    cy.contains('button', 'Continue').click();
+        cy.contains('label', 'Enter a new address').click();
+        cy.contains('button', 'Continue').click();
+        AddressFormAssertions.assertCanAddAddressManually('Enter address manually', true)
 
-    cy.contains('label', 'Yes').click();
-    cy.contains('button', 'Continue').click();
+        cy.contains('label', 'No').click();
+        cy.contains('button', 'Continue').click();
 
-    cy.get('#f-first-names').invoke('val', 'John');
-    cy.get('#f-last-name').invoke('val', 'Doe');
-    cy.get('#f-email').invoke('val', TestEmail);
-    cy.get('#f-date-of-birth').invoke('val', '1');
-    cy.get('#f-date-of-birth-month').invoke('val', '2');
-    cy.get('#f-date-of-birth-year').invoke('val', '1990');
-    cy.contains('button', 'Save and continue').click();
+        cy.get('input[value=jointly-and-severally]').click();
+        cy.contains('button', 'Save and continue').click();
 
-    cy.contains('label', 'Enter a new address').click();
-    cy.contains('button', 'Continue').click();
-    AddressFormAssertions.assertCanAddAddressManually('Enter address manually', true)
+        cy.visitLpa('/task-list');
+        cy.contains('a', 'Choose your attorneys').parent().parent().contains('2 added');
+    });
 
-    cy.contains('label', 'No').click();
-    cy.contains('button', 'Continue').click();
+    it('is completed if I enter multiple attorneys details when jointly', () => {
+        cy.visit('/fixtures?redirect=/task-list&progress=chooseYourAttorneys&attorneys=single');
+        cy.contains('a', 'Choose your attorneys').click();
+        cy.contains('button', 'Continue').click();
 
-    cy.get('input[value=jointly]').click();
-    cy.contains('button', 'Save and continue').click();
+        cy.contains('label', 'Yes').click();
+        cy.contains('button', 'Continue').click();
 
-    cy.visitLpa('/task-list');
-    cy.contains('a', 'Choose your attorneys').parent().parent().contains('2 added');
-  });
+        cy.get('#f-first-names').invoke('val', 'John');
+        cy.get('#f-last-name').invoke('val', 'Doe');
+        cy.get('#f-email').invoke('val', TestEmail);
+        cy.get('#f-date-of-birth').invoke('val', '1');
+        cy.get('#f-date-of-birth-month').invoke('val', '2');
+        cy.get('#f-date-of-birth-year').invoke('val', '1990');
+        cy.contains('button', 'Save and continue').click();
+
+        cy.contains('label', 'Enter a new address').click();
+        cy.contains('button', 'Continue').click();
+        AddressFormAssertions.assertCanAddAddressManually('Enter address manually', true)
+
+        cy.contains('label', 'No').click();
+        cy.contains('button', 'Continue').click();
+
+        cy.get('input[value=jointly]').click();
+        cy.contains('button', 'Save and continue').click();
+
+        cy.visitLpa('/task-list');
+        cy.contains('a', 'Choose your attorneys').parent().parent().contains('2 added');
+    });
 });
