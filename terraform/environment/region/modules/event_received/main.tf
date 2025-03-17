47a963cc6b64eed81fb11db3e89bf92f1917cada
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-03-17 11:47:24 +0000

    cache

--- terraform/environment/region/modules/event_received/main.tf
+++ terraform/environment/region/modules/event_received/main.tf
@@ -50,15 +50,15 @@ resource "aws_sqs_queue" "receive_events_queue" {
     deadLetterTargetArn = aws_sqs_queue.receive_events_deadletter.arn
     maxReceiveCount     = 3
   })
-  # policy   = data.aws_iam_policy_document.receive_events_queue_policy.json
+  policy   = data.aws_iam_policy_document.receive_events_queue_policy.json
   provider = aws.region
 }
 
-resource "aws_sqs_queue_policy" "receive_events_queue_queue_policy" {
-  queue_url = aws_sqs_queue.receive_events_queue.id
-  policy    = data.aws_iam_policy_document.receive_events_queue_policy.json
-  provider  = aws.region
-}
+# resource "aws_sqs_queue_policy" "receive_events_queue_queue_policy" {
+#   queue_url = aws_sqs_queue.receive_events_queue.id
+#   policy    = data.aws_iam_policy_document.receive_events_queue_policy.json
+#   provider  = aws.region
+# }
 
 data "aws_iam_policy_document" "receive_events_queue_policy" {
   statement {
