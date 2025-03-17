9b11d66014ef0d6b2fc883d8cda4f66b525f461c
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-03-17 15:47:17 +0000

    remove in-resource policy

--- terraform/environment/region/modules/event_received/main.tf
+++ terraform/environment/region/modules/event_received/main.tf
@@ -50,7 +50,6 @@ resource "aws_sqs_queue" "receive_events_queue" {
     deadLetterTargetArn = aws_sqs_queue.receive_events_deadletter.arn
     maxReceiveCount     = 3
   })
-  # policy   = data.aws_iam_policy_document.receive_events_queue_policy.json
   provider = aws.region
 }
 
