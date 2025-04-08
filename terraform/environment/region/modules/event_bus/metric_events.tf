335c89a947eee0526fc93990bb3e66dbdbd1e275
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-04-08 13:55:49 +0100

    add target for opg-metrics

--- terraform/environment/region/modules/event_bus/metric_events.tf
+++ terraform/environment/region/modules/event_bus/metric_events.tf
@@ -9,3 +9,22 @@ resource "aws_cloudwatch_event_rule" "metric_events" {
   })
   provider = aws.region
 }
+
+data "aws_ssm_parameter" "opg_metrics_arn" {
+  name     = "opg-metrics-api-destination-arn"
+  provider = aws.region
+}
+
+resource "aws_cloudwatch_event_target" "opg_metrics" {
+  arn            = data.aws_ssm_parameter.opg_metrics_arn.insecure_value
+  event_bus_name = aws_cloudwatch_event_bus.main.name
+  rule           = aws_cloudwatch_event_rule.metric_events.name
+  # role_arn       = "arn:aws:iam::653761790766:role/service-role/Amazon_EventBridge_Invoke_Api_Destination_344039630"
+  role_arn = var.opg_metrics_api_destination_role.arn
+  http_target {
+    header_parameters       = {}
+    path_parameter_values   = []
+    query_string_parameters = {}
+  }
+  provider = aws.region
+}
