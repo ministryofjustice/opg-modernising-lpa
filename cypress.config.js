0117af0791d728a72155933a78223c3f9f4f3206
Alex Saunders <acsauk@gmail.com>
2025-05-02 14:11:37 +0100

    MLPAB-2975 different ways of extracting content

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,6 @@
-const { defineConfig } = require("cypress");
+import {defineConfig} from 'cypress';
 
-module.exports = defineConfig({
+export default defineConfig({
   projectId: "xxbft5",
   e2e: {
     baseUrl: 'http://localhost:5050',
---
+++ playwright/e2e.js
@@ -0,0 +1,41 @@
+import {expect} from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright'
+
+export const
+    TestEmail = 'simulate-delivered@notifications.service.gov.uk',
+    TestEmail2 = 'simulate-delivered-2@notifications.service.gov.uk',
+    TestMobile = '07700900000'
+
+export function randomShareCode() {
+    const characters = 'abcdefghijklmnpqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789'
+    let result = [];
+
+    for (let i = 0; i < 12; i++) {
+        result.push(characters.charAt(Math.floor(Math.random() * characters.length)));
+    }
+
+    return result.join('');
+}
+
+export async function checkA11y(page) {
+    const accessibilityScanResults = await new AxeBuilder({ page })
+        .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
+        .exclude('.govuk-phase-banner')
+        .analyze();
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+}
+
+export async function visitLpa(path, page) {
+    console.log(page.url())
+    await page.goto(page.url().split('/').slice(0, 5).join('/') + path);
+}
+
+export function sanitisedPath(page) {
+    const parts = page.url().split('/')
+    return parts[parts.length - 1].replace(/\//g, ' ')
+}
+
+export async function screenshot(page) {
+    await page.screenshot({ path: `playwright/screenshots/${Date.now()} ${sanitisedPath(page)}.png`, fullPage: true });
+}
