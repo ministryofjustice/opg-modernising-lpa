456b25eb573e172e8cf4a2da2faa02c4b77f8633
Joshua Hawxwell <m@hawx.me>
2025-06-09 11:53:31 +0100

    Force actors to read LPA in language it will be registered in

--- internal/certificateprovider/certificateproviderpage/read_the_lpa.go
+++ internal/certificateprovider/certificateproviderpage/read_the_lpa.go
@@ -2,24 +2,36 @@ package certificateproviderpage
 
 import (
 	"net/http"
+	"net/url"
 
 	"github.com/ministryofjustice/opg-go-common/template"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/appcontext"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/certificateprovider"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/certificateprovider/certificateproviderdata"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/localize"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/lpastore/lpadata"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/task"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/validation"
 )
 
 type readTheLpaData struct {
-	App    appcontext.Data
-	Errors validation.List
-	Lpa    *lpadata.Lpa
+	App       appcontext.Data
+	BannerApp appcontext.Data
+	Errors    validation.List
+	Lpa       *lpadata.Lpa
 }
 
-func ReadTheLpa(tmpl template.Template, certificateProviderStore CertificateProviderStore) Handler {
+func ReadTheLpa(tmpl template.Template, certificateProviderStore CertificateProviderStore, bundle Bundle) Handler {
 	return func(appData appcontext.Data, w http.ResponseWriter, r *http.Request, certificateProvider *certificateproviderdata.Provided, lpa *lpadata.Lpa) error {
+		bannerLanguage, err := localize.ParseLang(r.FormValue("bannerLanguage"))
+		if err != nil || bannerLanguage.Empty() {
+			bannerLanguage = appData.Lang
+			appData.Lang = lpa.Language
+			return certificateprovider.PathReadTheLpa.RedirectQuery(w, r, appData, lpa.LpaID, url.Values{
+				"bannerLanguage": {bannerLanguage.String()},
+			})
+		}
+
 		if r.Method == http.MethodPost {
 			certificateProvider.Tasks.ReadTheLpa = task.StateCompleted
 			if err := certificateProviderStore.Put(r.Context(), certificateProvider); err != nil {
@@ -29,9 +41,14 @@ func ReadTheLpa(tmpl template.Template, certificateProviderStore CertificateProv
 			return certificateprovider.PathWhatHappensNext.Redirect(w, r, appData, lpa.LpaID)
 		}
 
+		bannerAppData := appData
+		bannerAppData.Lang = bannerLanguage
+		bannerAppData.Localizer = bundle.For(bannerAppData.Lang)
+
 		data := &readTheLpaData{
-			App: appData,
-			Lpa: lpa,
+			App:       appData,
+			BannerApp: bannerAppData,
+			Lpa:       lpa,
 		}
 
 		return tmpl(w, data)
