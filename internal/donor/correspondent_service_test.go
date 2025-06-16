b5aee750c7197f674e36d5698b53ae48cf99d37a
Joshua Hawxwell <m@hawx.me>
2025-06-16 08:58:09 +0100

    Extract a CorrespondentService

---
+++ internal/donor/correspondent_service_test.go
@@ -0,0 +1,413 @@
+package donor
+
+import (
+	"testing"
+
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/donor/donordata"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/dynamo"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/event"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/form"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/place"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/task"
+	"github.com/stretchr/testify/assert"
+	mock "github.com/stretchr/testify/mock"
+)
+
+func TestCorrespondentServiceReusable(t *testing.T) {
+	correspondents := []donordata.Correspondent{{}}
+
+	reuseStore := newMockReuseStore(t)
+	reuseStore.EXPECT().
+		Correspondents(ctx).
+		Return(correspondents, nil)
+
+	service := &CorrespondentService{reuseStore: reuseStore}
+	result, err := service.Reusable(ctx)
+
+	assert.Nil(t, err)
+	assert.Equal(t, correspondents, result)
+}
+
+func TestCorrespondentServiceReusableWhenNotFound(t *testing.T) {
+	reuseStore := newMockReuseStore(t)
+	reuseStore.EXPECT().
+		Correspondents(ctx).
+		Return(nil, dynamo.NotFoundError{})
+
+	service := &CorrespondentService{reuseStore: reuseStore}
+	result, err := service.Reusable(ctx)
+
+	assert.Nil(t, err)
+	assert.Empty(t, result)
+}
+
+func TestCorrespondentServiceReusableWhenError(t *testing.T) {
+	reuseStore := newMockReuseStore(t)
+	reuseStore.EXPECT().
+		Correspondents(ctx).
+		Return(nil, expectedError)
+
+	service := &CorrespondentService{reuseStore: reuseStore}
+	_, err := service.Reusable(ctx)
+
+	assert.ErrorIs(t, err, expectedError)
+}
+
+func TestCorrespondentServiceNotWanted(t *testing.T) {
+	donorStore := newMockDonorStore(t)
+	donorStore.EXPECT().
+		Put(ctx, &donordata.Provided{
+			LpaID:            "lpa-id",
+			AddCorrespondent: form.No,
+			Tasks:            donordata.Tasks{AddCorrespondent: task.StateCompleted},
+		}).
+		Return(nil)
+
+	service := &CorrespondentService{donorStore: donorStore}
+	err := service.NotWanted(ctx, &donordata.Provided{LpaID: "lpa-id"})
+
+	assert.Nil(t, err)
+}
+
+func TestCorrespondentServiceNotWantedWhenPreviouslySet(t *testing.T) {
+	correspondent := donordata.Correspondent{FirstNames: "A"}
+
+	reuseStore := newMockReuseStore(t)
+	reuseStore.EXPECT().
+		DeleteCorrespondent(ctx, correspondent).
+		Return(nil)
+
+	eventClient := newMockCorrespondentEventClient(t)
+	eventClient.EXPECT().
+		SendCorrespondentUpdated(ctx, event.CorrespondentUpdated{UID: "lpa-uid"}).
+		Return(nil)
+
+	donorStore := newMockDonorStore(t)
+	donorStore.EXPECT().
+		Put(ctx, &donordata.Provided{
+			LpaUID:           "lpa-uid",
+			AddCorrespondent: form.No,
+			Tasks:            donordata.Tasks{AddCorrespondent: task.StateCompleted},
+		}).
+		Return(nil)
+
+	service := &CorrespondentService{donorStore: donorStore, reuseStore: reuseStore, eventClient: eventClient}
+	err := service.NotWanted(ctx, &donordata.Provided{
+		LpaUID:           "lpa-uid",
+		Correspondent:    correspondent,
+		AddCorrespondent: form.Yes,
+		Tasks:            donordata.Tasks{AddCorrespondent: task.StateInProgress},
+	})
+
+	assert.Nil(t, err)
+}
+
+func TestCorrespondentServiceNotWantedWhenReuseStoreErrors(t *testing.T) {
+	reuseStore := newMockReuseStore(t)
+	reuseStore.EXPECT().
+		DeleteCorrespondent(mock.Anything, mock.Anything).
+		Return(expectedError)
+
+	service := &CorrespondentService{reuseStore: reuseStore}
+	err := service.NotWanted(ctx, &donordata.Provided{
+		LpaUID:           "lpa-uid",
+		Correspondent:    donordata.Correspondent{FirstNames: "A"},
+		AddCorrespondent: form.Yes,
+		Tasks:            donordata.Tasks{AddCorrespondent: task.StateInProgress},
+	})
+
+	assert.ErrorIs(t, err, expectedError)
+}
+
+func TestCorrespondentServiceNotWantedWhenEventClientErrors(t *testing.T) {
+	reuseStore := newMockReuseStore(t)
+	reuseStore.EXPECT().
+		DeleteCorrespondent(mock.Anything, mock.Anything).
+		Return(nil)
+
+	eventClient := newMockCorrespondentEventClient(t)
+	eventClient.EXPECT().
+		SendCorrespondentUpdated(mock.Anything, mock.Anything).
+		Return(expectedError)
+
+	service := &CorrespondentService{reuseStore: reuseStore, eventClient: eventClient}
+	err := service.NotWanted(ctx, &donordata.Provided{
+		LpaUID:           "lpa-uid",
+		Correspondent:    donordata.Correspondent{FirstNames: "A"},
+		AddCorrespondent: form.Yes,
+		Tasks:            donordata.Tasks{AddCorrespondent: task.StateInProgress},
+	})
+
+	assert.ErrorIs(t, err, expectedError)
+}
+
+func TestCorrespondentServiceNotWantedWhenDonorStoreErrors(t *testing.T) {
+	donorStore := newMockDonorStore(t)
+	donorStore.EXPECT().
+		Put(mock.Anything, mock.Anything).
+		Return(expectedError)
+
+	service := &CorrespondentService{donorStore: donorStore}
+	err := service.NotWanted(ctx, &donordata.Provided{LpaID: "lpa-id"})
+
+	assert.ErrorIs(t, err, expectedError)
+}
+
+func TestCorrespondentServicePutWhenDoNotWantAddress(t *testing.T) {
+	correspondent := donordata.Correspondent{
+		UID:         testUID,
+		FirstNames:  "A",
+		WantAddress: form.No,
+	}
+
+	reuseStore := newMockReuseStore(t)
+	reuseStore.EXPECT().
+		PutCorrespondent(ctx, correspondent).
+		Return(nil)
+
+	eventClient := newMockCorrespondentEventClient(t)
+	eventClient.EXPECT().
+		SendCorrespondentUpdated(ctx, event.CorrespondentUpdated{
+			UID:        "lpa-uid",
+			ActorUID:   &testUID,
+			FirstNames: "A",
+		}).
+		Return(nil)
+
+	donorStore := newMockDonorStore(t)
+	donorStore.EXPECT().
+		Put(ctx, &donordata.Provided{
+			LpaUID:        "lpa-uid",
+			Correspondent: correspondent,
+			Tasks:         donordata.Tasks{AddCorrespondent: task.StateCompleted},
+		}).
+		Return(nil)
+
+	service := &CorrespondentService{donorStore: donorStore, reuseStore: reuseStore, eventClient: eventClient, newUID: testUIDFn}
+	err := service.Put(ctx, &donordata.Provided{
+		LpaUID: "lpa-uid",
+		Correspondent: donordata.Correspondent{
+			FirstNames:  "A",
+			WantAddress: form.No,
+			Address:     place.Address{Line1: "A"},
+		},
+	})
+
+	assert.Nil(t, err)
+}
+
+func TestCorrespondentServicePutWhenWantAddressButNotCompleted(t *testing.T) {
+	donorStore := newMockDonorStore(t)
+	donorStore.EXPECT().
+		Put(ctx, &donordata.Provided{
+			LpaUID: "lpa-uid",
+			Correspondent: donordata.Correspondent{
+				UID:         testUID,
+				FirstNames:  "A",
+				WantAddress: form.Yes,
+			},
+			Tasks: donordata.Tasks{AddCorrespondent: task.StateInProgress},
+		}).
+		Return(nil)
+
+	service := &CorrespondentService{donorStore: donorStore, newUID: testUIDFn}
+	err := service.Put(ctx, &donordata.Provided{
+		LpaUID: "lpa-uid",
+		Correspondent: donordata.Correspondent{
+			FirstNames:  "A",
+			WantAddress: form.Yes,
+		},
+	})
+
+	assert.Nil(t, err)
+}
+
+func TestCorrespondentServicePutWhenWantAddressAndCompleted(t *testing.T) {
+	correspondent := donordata.Correspondent{
+		UID:         testUID,
+		FirstNames:  "A",
+		WantAddress: form.Yes,
+		Address:     place.Address{Line1: "B"},
+	}
+
+	reuseStore := newMockReuseStore(t)
+	reuseStore.EXPECT().
+		PutCorrespondent(ctx, correspondent).
+		Return(nil)
+
+	eventClient := newMockCorrespondentEventClient(t)
+	eventClient.EXPECT().
+		SendCorrespondentUpdated(ctx, event.CorrespondentUpdated{
+			UID:        "lpa-uid",
+			ActorUID:   &testUID,
+			FirstNames: "A",
+			Address:    &place.Address{Line1: "B"},
+		}).
+		Return(nil)
+
+	donorStore := newMockDonorStore(t)
+	donorStore.EXPECT().
+		Put(ctx, &donordata.Provided{
+			LpaUID:        "lpa-uid",
+			Correspondent: correspondent,
+			Tasks:         donordata.Tasks{AddCorrespondent: task.StateCompleted},
+		}).
+		Return(nil)
+
+	service := &CorrespondentService{donorStore: donorStore, reuseStore: reuseStore, eventClient: eventClient, newUID: testUIDFn}
+	err := service.Put(ctx, &donordata.Provided{
+		LpaUID: "lpa-uid",
+		Correspondent: donordata.Correspondent{
+			FirstNames:  "A",
+			WantAddress: form.Yes,
+			Address:     place.Address{Line1: "B"},
+		},
+	})
+
+	assert.Nil(t, err)
+}
+
+func TestCorrespondentServicePutWhenReuseStoreErrors(t *testing.T) {
+	reuseStore := newMockReuseStore(t)
+	reuseStore.EXPECT().
+		PutCorrespondent(mock.Anything, mock.Anything).
+		Return(expectedError)
+
+	service := &CorrespondentService{reuseStore: reuseStore, newUID: testUIDFn}
+	err := service.Put(ctx, &donordata.Provided{
+		LpaUID: "lpa-uid",
+		Correspondent: donordata.Correspondent{
+			FirstNames:  "A",
+			WantAddress: form.Yes,
+			Address:     place.Address{Line1: "B"},
+		},
+	})
+
+	assert.ErrorIs(t, err, expectedError)
+}
+
+func TestCorrespondentServicePutWhenEventClientErrors(t *testing.T) {
+	reuseStore := newMockReuseStore(t)
+	reuseStore.EXPECT().
+		PutCorrespondent(mock.Anything, mock.Anything).
+		Return(nil)
+
+	eventClient := newMockCorrespondentEventClient(t)
+	eventClient.EXPECT().
+		SendCorrespondentUpdated(mock.Anything, mock.Anything).
+		Return(expectedError)
+
+	service := &CorrespondentService{reuseStore: reuseStore, eventClient: eventClient, newUID: testUIDFn}
+	err := service.Put(ctx, &donordata.Provided{
+		LpaUID: "lpa-uid",
+		Correspondent: donordata.Correspondent{
+			FirstNames:  "A",
+			WantAddress: form.Yes,
+			Address:     place.Address{Line1: "B"},
+		},
+	})
+
+	assert.ErrorIs(t, err, expectedError)
+}
+
+func TestCorrespondentServicePutWhenDonorStoreErrors(t *testing.T) {
+	donorStore := newMockDonorStore(t)
+	donorStore.EXPECT().
+		Put(mock.Anything, mock.Anything).
+		Return(expectedError)
+
+	service := &CorrespondentService{donorStore: donorStore, newUID: testUIDFn}
+	err := service.Put(ctx, &donordata.Provided{
+		LpaUID: "lpa-uid",
+		Correspondent: donordata.Correspondent{
+			FirstNames:  "A",
+			WantAddress: form.Yes,
+		},
+	})
+
+	assert.ErrorIs(t, err, expectedError)
+}
+
+func TestCorrespondentServiceDelete(t *testing.T) {
+	correspondent := donordata.Correspondent{FirstNames: "A"}
+
+	reuseStore := newMockReuseStore(t)
+	reuseStore.EXPECT().
+		DeleteCorrespondent(ctx, correspondent).
+		Return(nil)
+
+	eventClient := newMockCorrespondentEventClient(t)
+	eventClient.EXPECT().
+		SendCorrespondentUpdated(ctx, event.CorrespondentUpdated{
+			UID: "lpa-uid",
+		}).
+		Return(nil)
+
+	donorStore := newMockDonorStore(t)
+	donorStore.EXPECT().
+		Put(ctx, &donordata.Provided{
+			LpaUID: "lpa-uid",
+		}).
+		Return(nil)
+
+	service := &CorrespondentService{donorStore: donorStore, reuseStore: reuseStore, eventClient: eventClient}
+	err := service.Delete(ctx, &donordata.Provided{
+		LpaUID:           "lpa-uid",
+		AddCorrespondent: form.Yes,
+		Correspondent:    correspondent,
+		Tasks:            donordata.Tasks{AddCorrespondent: task.StateCompleted},
+	})
+
+	assert.Nil(t, err)
+}
+
+func TestCorrespondentServiceDeleteWhenReuseStoreErrors(t *testing.T) {
+	reuseStore := newMockReuseStore(t)
+	reuseStore.EXPECT().
+		DeleteCorrespondent(mock.Anything, mock.Anything).
+		Return(expectedError)
+
+	service := &CorrespondentService{reuseStore: reuseStore}
+	err := service.Delete(ctx, &donordata.Provided{})
+
+	assert.ErrorIs(t, err, expectedError)
+}
+
+func TestCorrespondentServiceDeleteWhenEventClientErrors(t *testing.T) {
+	reuseStore := newMockReuseStore(t)
+	reuseStore.EXPECT().
+		DeleteCorrespondent(mock.Anything, mock.Anything).
+		Return(nil)
+
+	eventClient := newMockCorrespondentEventClient(t)
+	eventClient.EXPECT().
+		SendCorrespondentUpdated(mock.Anything, mock.Anything).
+		Return(expectedError)
+
+	service := &CorrespondentService{reuseStore: reuseStore, eventClient: eventClient}
+	err := service.Delete(ctx, &donordata.Provided{})
+
+	assert.ErrorIs(t, err, expectedError)
+}
+
+func TestCorrespondentServiceDeleteWhenDonorStoreErrors(t *testing.T) {
+	reuseStore := newMockReuseStore(t)
+	reuseStore.EXPECT().
+		DeleteCorrespondent(mock.Anything, mock.Anything).
+		Return(nil)
+
+	eventClient := newMockCorrespondentEventClient(t)
+	eventClient.EXPECT().
+		SendCorrespondentUpdated(mock.Anything, mock.Anything).
+		Return(nil)
+
+	donorStore := newMockDonorStore(t)
+	donorStore.EXPECT().
+		Put(mock.Anything, mock.Anything).
+		Return(expectedError)
+
+	service := &CorrespondentService{donorStore: donorStore, reuseStore: reuseStore, eventClient: eventClient}
+	err := service.Delete(ctx, &donordata.Provided{})
+
+	assert.ErrorIs(t, err, expectedError)
+}
--- internal/donor/mock_ReuseStore_test.go
+++ internal/donor/mock_ReuseStore_test.go
@@ -80,6 +80,64 @@ func (_c *mockReuseStore_CertificateProviders_Call) RunAndReturn(run func(contex
 	return _c
 }
 
+// Correspondents provides a mock function with given fields: ctx
+func (_m *mockReuseStore) Correspondents(ctx context.Context) ([]donordata.Correspondent, error) {
+	ret := _m.Called(ctx)
+
+	if len(ret) == 0 {
+		panic("no return value specified for Correspondents")
+	}
+
+	var r0 []donordata.Correspondent
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context) ([]donordata.Correspondent, error)); ok {
+		return rf(ctx)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context) []donordata.Correspondent); ok {
+		r0 = rf(ctx)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]donordata.Correspondent)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context) error); ok {
+		r1 = rf(ctx)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// mockReuseStore_Correspondents_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'Correspondents'
+type mockReuseStore_Correspondents_Call struct {
+	*mock.Call
+}
+
+// Correspondents is a helper method to define mock.On call
+//   - ctx context.Context
+func (_e *mockReuseStore_Expecter) Correspondents(ctx interface{}) *mockReuseStore_Correspondents_Call {
+	return &mockReuseStore_Correspondents_Call{Call: _e.mock.On("Correspondents", ctx)}
+}
+
+func (_c *mockReuseStore_Correspondents_Call) Run(run func(ctx context.Context)) *mockReuseStore_Correspondents_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(context.Context))
+	})
+	return _c
+}
+
+func (_c *mockReuseStore_Correspondents_Call) Return(_a0 []donordata.Correspondent, _a1 error) *mockReuseStore_Correspondents_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *mockReuseStore_Correspondents_Call) RunAndReturn(run func(context.Context) ([]donordata.Correspondent, error)) *mockReuseStore_Correspondents_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // DeleteCertificateProvider provides a mock function with given fields: ctx, certificateProvider
 func (_m *mockReuseStore) DeleteCertificateProvider(ctx context.Context, certificateProvider donordata.CertificateProvider) error {
 	ret := _m.Called(ctx, certificateProvider)
@@ -123,6 +181,53 @@ func (_c *mockReuseStore_DeleteCertificateProvider_Call) Return(_a0 error) *mock
 }
 
 func (_c *mockReuseStore_DeleteCertificateProvider_Call) RunAndReturn(run func(context.Context, donordata.CertificateProvider) error) *mockReuseStore_DeleteCertificateProvider_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
+// DeleteCorrespondent provides a mock function with given fields: ctx, correspondent
+func (_m *mockReuseStore) DeleteCorrespondent(ctx context.Context, correspondent donordata.Correspondent) error {
+	ret := _m.Called(ctx, correspondent)
+
+	if len(ret) == 0 {
+		panic("no return value specified for DeleteCorrespondent")
+	}
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, donordata.Correspondent) error); ok {
+		r0 = rf(ctx, correspondent)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// mockReuseStore_DeleteCorrespondent_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'DeleteCorrespondent'
+type mockReuseStore_DeleteCorrespondent_Call struct {
+	*mock.Call
+}
+
+// DeleteCorrespondent is a helper method to define mock.On call
+//   - ctx context.Context
+//   - correspondent donordata.Correspondent
+func (_e *mockReuseStore_Expecter) DeleteCorrespondent(ctx interface{}, correspondent interface{}) *mockReuseStore_DeleteCorrespondent_Call {
+	return &mockReuseStore_DeleteCorrespondent_Call{Call: _e.mock.On("DeleteCorrespondent", ctx, correspondent)}
+}
+
+func (_c *mockReuseStore_DeleteCorrespondent_Call) Run(run func(ctx context.Context, correspondent donordata.Correspondent)) *mockReuseStore_DeleteCorrespondent_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(context.Context), args[1].(donordata.Correspondent))
+	})
+	return _c
+}
+
+func (_c *mockReuseStore_DeleteCorrespondent_Call) Return(_a0 error) *mockReuseStore_DeleteCorrespondent_Call {
+	_c.Call.Return(_a0)
+	return _c
+}
+
+func (_c *mockReuseStore_DeleteCorrespondent_Call) RunAndReturn(run func(context.Context, donordata.Correspondent) error) *mockReuseStore_DeleteCorrespondent_Call {
 	_c.Call.Return(run)
 	return _c
 }
@@ -276,6 +381,53 @@ func (_c *mockReuseStore_PutCertificateProvider_Call) Return(_a0 error) *mockReu
 }
 
 func (_c *mockReuseStore_PutCertificateProvider_Call) RunAndReturn(run func(context.Context, donordata.CertificateProvider) error) *mockReuseStore_PutCertificateProvider_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
+// PutCorrespondent provides a mock function with given fields: ctx, correspondent
+func (_m *mockReuseStore) PutCorrespondent(ctx context.Context, correspondent donordata.Correspondent) error {
+	ret := _m.Called(ctx, correspondent)
+
+	if len(ret) == 0 {
+		panic("no return value specified for PutCorrespondent")
+	}
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, donordata.Correspondent) error); ok {
+		r0 = rf(ctx, correspondent)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// mockReuseStore_PutCorrespondent_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'PutCorrespondent'
+type mockReuseStore_PutCorrespondent_Call struct {
+	*mock.Call
+}
+
+// PutCorrespondent is a helper method to define mock.On call
+//   - ctx context.Context
+//   - correspondent donordata.Correspondent
+func (_e *mockReuseStore_Expecter) PutCorrespondent(ctx interface{}, correspondent interface{}) *mockReuseStore_PutCorrespondent_Call {
+	return &mockReuseStore_PutCorrespondent_Call{Call: _e.mock.On("PutCorrespondent", ctx, correspondent)}
+}
+
+func (_c *mockReuseStore_PutCorrespondent_Call) Run(run func(ctx context.Context, correspondent donordata.Correspondent)) *mockReuseStore_PutCorrespondent_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(context.Context), args[1].(donordata.Correspondent))
+	})
+	return _c
+}
+
+func (_c *mockReuseStore_PutCorrespondent_Call) Return(_a0 error) *mockReuseStore_PutCorrespondent_Call {
+	_c.Call.Return(_a0)
+	return _c
+}
+
+func (_c *mockReuseStore_PutCorrespondent_Call) RunAndReturn(run func(context.Context, donordata.Correspondent) error) *mockReuseStore_PutCorrespondent_Call {
 	_c.Call.Return(run)
 	return _c
 }
