20fe98cba4c80aa9959e32126658b4f9dd208b91
Joshua Hawxwell <m@hawx.me>
2025-06-17 08:35:30 +0100

    Donor read lpa warning for language

--- internal/donor/donorpage/read_your_lpa.go
+++ internal/donor/donorpage/read_your_lpa.go
@@ -2,30 +2,42 @@ package donorpage
 
 import (
 	"net/http"
+	"net/url"
 
 	"github.com/ministryofjustice/opg-go-common/template"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/appcontext"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/donor"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/donor/donordata"
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/localize"
 	"github.com/ministryofjustice/opg-modernising-lpa/internal/validation"
 )
 
 type readYourLpaData struct {
-	App            appcontext.Data
-	LpaLanguageApp appcontext.Data
-	Errors         validation.List
-	Donor          *donordata.Provided
+	App       appcontext.Data
+	BannerApp appcontext.Data
+	Errors    validation.List
+	Donor     *donordata.Provided
 }
 
 func ReadYourLpa(tmpl template.Template, bundle Bundle) Handler {
-	return func(appData appcontext.Data, w http.ResponseWriter, r *http.Request, donor *donordata.Provided) error {
-		lpaLanguageApp := appData
-		lpaLanguageApp.Lang = donor.Donor.LpaLanguagePreference
-		lpaLanguageApp.Localizer = bundle.For(donor.Donor.LpaLanguagePreference)
+	return func(appData appcontext.Data, w http.ResponseWriter, r *http.Request, provided *donordata.Provided) error {
+		bannerLanguage, err := localize.ParseLang(r.FormValue("bannerLanguage"))
+		if err != nil || bannerLanguage.Empty() {
+			bannerLanguage = appData.Lang
+			appData.Lang = provided.Donor.LpaLanguagePreference
+			return donor.PathReadYourLpa.RedirectQuery(w, r, appData, provided, url.Values{
+				"bannerLanguage": {bannerLanguage.String()},
+			})
+		}
+
+		bannerAppData := appData
+		bannerAppData.Lang = bannerLanguage
+		bannerAppData.Localizer = bundle.For(bannerAppData.Lang)
 
 		data := &readYourLpaData{
-			App:            appData,
-			LpaLanguageApp: lpaLanguageApp,
-			Donor:          donor,
+			App:       appData,
+			BannerApp: bannerAppData,
+			Donor:     provided,
 		}
 
 		return tmpl(w, data)
