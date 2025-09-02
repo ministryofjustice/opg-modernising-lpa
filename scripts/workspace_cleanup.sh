2ba0100dbf17ce1292e57777c9901cba89bb2121
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-09-02 15:13:56 +0100

    skip deleting log group for container insights

--- scripts/workspace_cleanup.sh
+++ scripts/workspace_cleanup.sh
@@ -31,8 +31,8 @@ do
       if ! terraform destroy -auto-approve; then
         TF_EXIT_CODE=1
       fi
-      echo "deleting containter insights log group..."
-      aws logs delete-log-group --log-group-name /aws/ecs/containerinsights/"$workspace"/performance:*
+      # echo "deleting containter insights log group..."
+      # aws logs delete-log-group --log-group-name /aws/ecs/containerinsights/"$workspace"/performance
       echo "deleting opensearch index..."
       response=$(awscurl \
         "${DEVELOPMENT_OPENSEARCH_COLLECTION_ENDPOINT}/lpas_v2_$workspace" \
