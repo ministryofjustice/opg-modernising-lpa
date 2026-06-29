822007e472bdfc5150c7c1b7b5a1a53f77834810
Joshua Hawxwell <m@hawx.me>
2026-06-29 14:03:02 +0100

    Use new forms package for voucher pages

---
+++ internal/forms/enum.go
@@ -0,0 +1,81 @@
+package forms
+
+import (
+	"net/url"
+	"strings"
+)
+
+type anEnum interface {
+	String() string
+}
+
+type anUnmarshallableEnum[T anEnum] interface {
+	UnmarshalText([]byte) error
+	*T
+}
+
+// Enum is a validatable enum form field.
+type Enum[T anEnum, O any, U anUnmarshallableEnum[T]] struct {
+	Field
+	Value      T
+	Options    O
+	parseError error
+	validators []validator[anEnum]
+}
+
+func NewEnum[T anEnum, O any, U anUnmarshallableEnum[T]](name, label string, values O) *Enum[T, O, U] {
+	f := &Enum[T, O, U]{}
+	f.Name = name
+	f.Label = label
+	f.Options = values
+	return f
+}
+
+func (f *Enum[T, O, U]) Set(e T) {
+	f.Input = e.String()
+}
+
+func (f *Enum[T, O, U]) Parse(values url.Values) {
+	f.Input = strings.TrimSpace(values.Get(f.Name))
+	f.parseError = U(&f.Value).UnmarshalText([]byte(f.Input))
+
+	for _, validator := range f.validators {
+		if error := validator.Validate(f.Value); error != nil {
+			f.Error = error
+			break
+		}
+	}
+}
+
+// WithError overrides the error returned by the previously defined validator.
+func (f *Enum[T, O, U]) WithError(replace Error) *Enum[T, O, U] {
+	if l := len(f.validators); l > 0 {
+		f.validators[l-1] = withError[anEnum]{replace: replace, wrapped: f.validators[l-1]}
+	}
+
+	return f
+}
+
+type enumSelectedValidator struct {
+	label      string
+	parseError bool
+}
+
+func (v enumSelectedValidator) Validate(e anEnum) Error {
+	if v.parseError {
+		return newSelectError(v.label)
+	}
+
+	et, ok := e.(interface{ Empty() bool })
+	if ok && et.Empty() {
+		return newSelectError(v.label)
+	}
+
+	return nil
+}
+
+func (f *Enum[T, O, U]) Selected() *Enum[T, O, U] {
+	f.validators = append(f.validators, enumSelectedValidator{label: f.Field.Label, parseError: f.parseError != nil})
+
+	return f
+}
