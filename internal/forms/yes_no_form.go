a56f4eaecbd81302d7697a8562169a233f3d0e2e
Joshua Hawxwell <m@hawx.me>
2026-06-29 14:03:02 +0100

    Use new forms package for voucher pages

---
+++ internal/forms/yes_no_form.go
@@ -0,0 +1,27 @@
+package forms
+
+import (
+	"net/http"
+
+	"github.com/ministryofjustice/opg-modernising-lpa/internal/form"
+)
+
+var (
+	Yes = form.Yes
+	No  = form.No
+)
+
+type YesNoForm struct {
+	Form
+	YesNo *Enum[form.YesNo, form.YesNoOptions, *form.YesNo]
+}
+
+func NewYesNoForm(label string) *YesNoForm {
+	return &YesNoForm{
+		YesNo: NewEnum[form.YesNo]("yesNo", label, form.YesNoValues).Selected(),
+	}
+}
+
+func (f *YesNoForm) Parse(r *http.Request) bool {
+	return f.ParsePostForm(r, f.YesNo)
+}
